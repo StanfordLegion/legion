20c60ac27d7051840da4d1c778df5e4847087ddb
Mike <mbauer@nvidia.com>
2015-07-01 16:32:50 -0700

    Adding a modified version of the circuit simulation that shows how to use the dependent partitioning API to the examples

---
+++ examples/dependent_circuit/circuit_mapper.h
@@ -0,0 +1,38 @@
+#ifndef __CIRCUIT_MAPPER_H__
+#define __CIRCUIT_MAPPER_H__
+
+#include "legion.h"
+#include "default_mapper.h"
+#include "circuit.h"
+
+using namespace LegionRuntime::HighLevel;
+
+class CircuitMapper : public DefaultMapper {
+public:
+  CircuitMapper(Machine machine, HighLevelRuntime *runtime, Processor local);
+public:
+  virtual void select_task_options(Task *task);
+  virtual void slice_domain(const Task *task, const Domain &domain,
+                            std::vector<DomainSplit> &slices);
+  virtual bool map_task(Task *task);
+  virtual bool map_inline(Inline *inline_operation);
+
+  virtual void notify_mapping_failed(const Mappable *mappable);
+#if 0
+  virtual bool rank_copy_targets(const Mappable *mappble,
+                                 const std::set<Memory> &current_instances,
+                                 bool complete,
+                                 size_t max_blocking_factor,
+                                 std::set<Memory> &to_reuse,
+                                 std::vector<Memory> &to_create,
+                                 bool &create_one,
+                                 size_t &blocking_factor);
+#endif
+protected:
+  bool map_to_gpus;
+  std::vector<Processor> all_cpus;
+  std::vector<Processor> all_gpus;
+  std::map<Processor, Memory> all_sysmems;
+};
+
+#endif // __CIRCUIT_MAPPER_H__
