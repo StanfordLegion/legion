ffacb3fe15a60121c5f20b66c96725d59c8352ba
Noah Watkins <noahwatkins@gmail.com>
2015-08-19 11:02:20 -0600

    test: support 3D version of hdf attach test
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- test/hdf_attach_subregion_parallel/legion_io.cc
+++ test/hdf_attach_subregion_parallel/legion_io.cc
@@ -28,8 +28,8 @@ PersistentRegion::PersistentRegion(HighLevelRuntime * runtime) {
 
 void copy_values_task(const Task *task,
                       const std::vector<PhysicalRegion> &regions,
-                      Context ctx, HighLevelRuntime *runtime) {
-  
+                      Context ctx, HighLevelRuntime *runtime)
+{
   Piece piece = * ((Piece*) task->local_args);
   struct task_args_t task_args = *(struct task_args_t*) task->args;
   
@@ -39,11 +39,13 @@ void copy_values_task(const Task *task,
 
   std::map<FieldID, std::string> field_string_map;
   Realm::Serialization::FixedBufferDeserializer fdb(task_args.field_map_serial,
-                                                    task_args.field_map_size);
+      task_args.field_map_size);
+
   bool ok  = fdb >> field_string_map;
   if(!ok) {
     std::cout << "ERROR in copy_values_task, can't deserialize " << std::endl;
   }
+
 #ifdef IOTESTER_VERBOSE
   std::cout << "field_map_size is : " <<task_args.field_map_size << std::endl;
   std::cout << "field_string_map.size is : " << field_string_map.size() << std::endl;
@@ -61,13 +63,11 @@ void copy_values_task(const Task *task,
   }
   
   Domain dom = runtime->get_index_space_domain(ctx,
-                                               piece.child_lr.get_index_space());
+      piece.child_lr.get_index_space());
 
-  
 #ifdef IOTESTER_VERBOSE
   std::cout << "In write_values_task and found my piece!" << std::endl;
   std::cout<< "In write_value_task "  << std::endl;
-#endif
   
   int x_min = 0, y_min = 0, 
     x_max = 0, y_max = 0;
@@ -76,7 +76,6 @@ void copy_values_task(const Task *task,
   x_max = dom.get_rect<2>().hi.x[0];
   y_max = dom.get_rect<2>().hi.x[1];
   
-#ifdef IOTESTER_VERBOSE
   std::cout << "domain rect is: [[" << x_min << "," << y_min
             << "],[" << x_max  << "," << y_max << "]] writing to file "
             << piece.shard_name << std::endl; 
@@ -85,13 +84,9 @@ void copy_values_task(const Task *task,
   runtime->unmap_region(ctx, regions[0]);
       
   PhysicalRegion pr = runtime->attach_hdf5(ctx, piece.shard_name,
-                                           piece.child_lr, piece.child_lr,
-                                           field_map, task_args.copy_write ? LEGION_FILE_READ_WRITE: LEGION_FILE_READ_ONLY);
-  
-  
-  
-  
-  
+      piece.child_lr, piece.child_lr, field_map,
+      task_args.copy_write ? LEGION_FILE_READ_WRITE: LEGION_FILE_READ_ONLY);
+
   runtime->remap_region(ctx, pr);
   pr.wait_until_valid();
   
@@ -206,17 +201,15 @@ void PersistentRegion::read_persistent_subregions(Context ctx, LogicalRegion src
 
 
 
-void PersistentRegion::create_persistent_subregions(Context ctx, const char *name,
-						    LogicalRegion parent_lr,
-						    LogicalPartition lp,
-						    Domain dom, std::map<FieldID, std::string> &field_map){
-
-
+void PersistentRegion::create_persistent_subregions(Context ctx,
+    const char *name, LogicalRegion parent_lr, LogicalPartition lp,
+    Domain dom, std::map<FieldID, std::string> &field_map)
+{
   hid_t link_file_id, shard_group_id, shard_ds_id, dataspace_id, dtype_id, shard_file_id, attr_ds_id, link_group_id, link_group_2_id;
   herr_t status;
   link_file_id = H5Fcreate(name, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
   dtype_id  = H5Tcopy (H5T_NATIVE_DOUBLE);
-  
+
   this->lp = lp;
   this->parent_lr = parent_lr;
   this->field_map = field_map;
@@ -228,145 +221,166 @@ void PersistentRegion::create_persistent_subregions(Context ctx, const char *nam
   dbs << field_map;
   this->field_map_size = dbs.bytes_used();
   this->field_map_serial = dbs.detach_buffer();
-//  Realm::Serialization::FixedBufferDeserializer fbd(this->field_map_serial, this->field_map_size);
-//  bool ok = fbd >> field_map_des;
-//  if(ok) { 
-//    std::cout << " In create_persistent_subregions and I deserialized!: " <<
-//      ok << std::endl;
-//  }
+  //  Realm::Serialization::FixedBufferDeserializer fbd(this->field_map_serial, this->field_map_size);
+  //  bool ok = fbd >> field_map_des;
+  //  if(ok) { 
+  //    std::cout << " In create_persistent_subregions and I deserialized!: " <<
+  //      ok << std::endl;
+  //  }
 
-  
-    int i = 0;
-    for(LegionRuntime::LowLevel::Domain::DomainPointIterator itr(dom); itr; itr++) {
-      pieces.push_back(Piece());
-      LegionRuntime::LowLevel::DomainPoint dp = itr.p;
-      pieces[i].dp = dp;
-      pieces[i].child_lr = runtime->get_logical_subregion_by_color(ctx, lp, dp);
-      pieces[i].parent_lr = parent_lr;
-      IndexSpace is = runtime->get_index_subspace(ctx, lp.get_index_partition(), dp); 
-      FieldSpace fs = pieces[i].child_lr.get_field_space();
-      Domain d = runtime->get_index_space_domain(ctx, is);
-      int dim = d.get_dim();
+  int i = 0;
+  for (LegionRuntime::LowLevel::Domain::DomainPointIterator itr(dom); itr; itr++) {
+    pieces.push_back(Piece());
+    LegionRuntime::LowLevel::DomainPoint dp = itr.p;
+    pieces[i].dp = dp;
+    pieces[i].child_lr = runtime->get_logical_subregion_by_color(ctx, lp, dp);
+    pieces[i].parent_lr = parent_lr;
+    IndexSpace is = runtime->get_index_subspace(ctx, lp.get_index_partition(), dp); 
+    FieldSpace fs = pieces[i].child_lr.get_field_space();
+    Domain d = runtime->get_index_space_domain(ctx, is);
+    int dim = d.get_dim();
 
 #ifdef IOTESTER_VERBOSE
-      std::cout << "Found a logical region:  Dimension " << dim <<  std::endl;
+    std::cout << "Found a logical region:  Dimension " << dim <<  std::endl;
 #endif
-      int x_min = 0, y_min = 0, z_min = 0,
+    int x_min = 0, y_min = 0, z_min = 0,
         x_max = 0, y_max = 0, z_max = 0;
-      
-      sprintf(pieces[i].shard_name, "%d-%d-%s", pieces[i].dp[0], pieces[i].dp[1], name); 
-      shard_file_id = H5Fcreate(pieces[i].shard_name, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-      int *shard_dims;
-      
-      
-      switch(dim){
-      case 1: {
-        x_min = d.get_rect<1>().lo.x[0];
-        x_max = d.get_rect<1>().hi.x[0];
-        hsize_t dims[1];
-        dims[0]=x_max;
-        dataspace_id = H5Screate_simple(1, dims, NULL);
-      }
-        break;
-      case 2: {
-        x_min = d.get_rect<2>().lo.x[0];
-        y_min = d.get_rect<2>().lo.x[1];
-        x_max = d.get_rect<2>().hi.x[0];
-        y_max = d.get_rect<2>().hi.x[1];
-        
-#ifdef IO_TESTER_VERBOSE 
-        std::cout << "domain rect is: [[" << x_min << "," << y_min
-                  << "],[" << x_max  << "," << y_max << "]]" << std::endl; 
-#endif
-        
-        hsize_t dims[2];
-        dims[0] = x_max-x_min+1;
-        dims[1] = y_max-y_min+1;
-        dataspace_id = H5Screate_simple(2, dims, NULL);
-        dims[0] = 2; dims[1] = 2; 
-        attr_ds_id = H5Screate_simple(2, dims, NULL);
-        shard_dims = (int*) malloc(4*sizeof(int)); 
-        shard_dims[0] = x_min;
-        shard_dims[1] = y_min;
-        shard_dims[2] = x_max;
-        shard_dims[3] = y_max;
-        
 
-      }
+    int *shard_dims;
+    std::ostringstream ds_name_stream;
+
+
+    switch(dim) {
+      case 2:
+        {
+          ds_name_stream <<  pieces[i].dp[0] << "-" << pieces[i].dp[1];
+          sprintf(pieces[i].shard_name, "%d-%d-%s",
+              pieces[i].dp[0], pieces[i].dp[1], name); 
+
+          x_min = d.get_rect<2>().lo.x[0];
+          y_min = d.get_rect<2>().lo.x[1];
+          x_max = d.get_rect<2>().hi.x[0];
+          y_max = d.get_rect<2>().hi.x[1];
+
+#ifdef IO_TESTER_VERBOSE 
+          std::cout << "domain rect is: [[" << x_min << "," << y_min
+            << "],[" << x_max  << "," << y_max << "]]" << std::endl; 
+#endif
+
+          hsize_t dims[2];
+          dims[0] = x_max-x_min+1;
+          dims[1] = y_max-y_min+1;
+          dataspace_id = H5Screate_simple(2, dims, NULL);
+
+          dims[0] = 2;
+          dims[1] = 2; 
+          attr_ds_id = H5Screate_simple(2, dims, NULL);
+
+          shard_dims = (int*) malloc(4*sizeof(int)); 
+          shard_dims[0] = x_min;
+          shard_dims[1] = y_min;
+          shard_dims[2] = x_max;
+          shard_dims[3] = y_max;
+        }
         break;
-      case 3: {
-        x_min = d.get_rect<3>().lo.x[0];
-        x_max = d.get_rect<3>().hi.x[0];
-        y_min = d.get_rect<3>().lo.x[1];
-        y_max = d.get_rect<3>().hi.x[1];
-        z_min = d.get_rect<3>().lo.x[2];
-        z_max = d.get_rect<3>().hi.x[2];
-        hsize_t dims[3];
-        dims[0] = x_max;
-        dims[1] = y_max;
-        dims[2] = z_max;
-        
-    }
+
+      case 3:
+        {
+          ds_name_stream <<  pieces[i].dp[0] << "-" <<
+            pieces[i].dp[1] << "-" << pieces[i].dp[2];
+          sprintf(pieces[i].shard_name, "%d-%d-%d-%s",
+              pieces[i].dp[0], pieces[i].dp[1],
+              pieces[i].dp[2], name);
+
+          x_min = d.get_rect<3>().lo.x[0];
+          y_min = d.get_rect<3>().lo.x[1];
+          z_min = d.get_rect<3>().lo.x[2];
+          x_max = d.get_rect<3>().hi.x[0];
+          y_max = d.get_rect<3>().hi.x[1];
+          z_max = d.get_rect<3>().hi.x[2];
+
+          hsize_t dims[3];
+          dims[0] = x_max-x_min+1;
+          dims[1] = y_max-y_min+1;
+          dims[2] = z_max-z_min+1;
+          dataspace_id = H5Screate_simple(3, dims, NULL);
+
+          dims[0] = 2;
+          dims[1] = 2; 
+          dims[2] = 2; 
+          attr_ds_id = H5Screate_simple(3, dims, NULL);
+
+          shard_dims = (int*) malloc(6*sizeof(int)); 
+          shard_dims[0] = x_min;
+          shard_dims[1] = y_min;
+          shard_dims[2] = z_min;
+          shard_dims[3] = x_max;
+          shard_dims[4] = y_max;
+          shard_dims[5] = z_max;
+        }
         break;
-    default:
+
+      default:
         assert(false);
     }
-      typedef std::map<FieldID, std::string>::iterator it_type;
-      for(it_type iterator = field_map.begin(); iterator != field_map.end(); iterator++) {
+
+    shard_file_id = H5Fcreate(pieces[i].shard_name, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+    for (std::map<FieldID, std::string>::const_iterator iterator = field_map.begin();
+         iterator != field_map.end(); iterator++) {
       FieldID fid = iterator->first;
+
       char* ds;
       char* gp;
       split_path_file(&gp, &ds, iterator->second.c_str());
-        size_t field_size = runtime->get_field_size(ctx, fs, fid);
-        status = H5Tset_size(dtype_id, field_size);
-        if(H5Lexists(shard_file_id, gp, H5P_DEFAULT)) { 
-          shard_group_id = H5Gopen2(shard_file_id, gp, H5P_DEFAULT);
-        } else { 
-          shard_group_id = H5Gcreate2(shard_file_id, gp, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-        } 
-        if(H5Lexists(shard_group_id, ds, H5P_DEFAULT)) { 
-          shard_ds_id = H5Dopen2(shard_group_id, ds, H5P_DEFAULT); 
-        } else { 
-          shard_ds_id = H5Dcreate2(shard_group_id, ds, H5T_NATIVE_DOUBLE,
-                                   dataspace_id, H5P_DEFAULT, H5P_DEFAULT,
-                                   H5P_DEFAULT);
-        }
-        
-        if(H5Lexists(link_file_id, gp, H5P_DEFAULT) && H5Lexists(link_file_id, iterator->second.c_str(), H5P_DEFAULT)) {
-          link_group_2_id = H5Gopen2(link_file_id, iterator->second.c_str(), H5P_DEFAULT);
-        } else {
-          link_group_id = H5Gcreate2(link_file_id, gp, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-          link_group_2_id = H5Gcreate2(link_group_id, ds, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-          H5Gclose(link_group_id);
-        }
 
-        std::ostringstream ds_name_stream;
-        
-        ds_name_stream <<  pieces[i].dp[0] << "-" << pieces[i].dp[1];
-        
-        hid_t attr_id = H5Acreate2(shard_ds_id, "dims", H5T_NATIVE_INT, attr_ds_id,
-                            H5P_DEFAULT, H5P_DEFAULT);
-        
-        status = H5Awrite(attr_id, H5T_NATIVE_INT, shard_dims);
-        H5Aclose(attr_id);
-        
-        H5Dclose(shard_ds_id);
-        H5Gclose(shard_group_id);
-        H5Fclose(shard_file_id);
+      size_t field_size = runtime->get_field_size(ctx, fs, fid);
+      status = H5Tset_size(dtype_id, field_size);
 
-        
-        status = H5Lcreate_external(pieces[i].shard_name, iterator->second.c_str(),
-                                    link_group_2_id, ds_name_stream.str().c_str(),
-                                    H5P_DEFAULT, H5P_DEFAULT);
-        
-        shard_file_id = H5Fopen(pieces[i].shard_name, H5F_ACC_RDWR, H5P_DEFAULT);      
-        H5Gclose(link_group_2_id);
-    }
+      if(H5Lexists(shard_file_id, gp, H5P_DEFAULT)) { 
+        shard_group_id = H5Gopen2(shard_file_id, gp, H5P_DEFAULT);
+      } else { 
+        shard_group_id = H5Gcreate2(shard_file_id, gp, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+      } 
+
+      if(H5Lexists(shard_group_id, ds, H5P_DEFAULT)) { 
+        shard_ds_id = H5Dopen2(shard_group_id, ds, H5P_DEFAULT); 
+      } else { 
+        shard_ds_id = H5Dcreate2(shard_group_id, ds, H5T_NATIVE_DOUBLE,
+            dataspace_id, H5P_DEFAULT, H5P_DEFAULT,
+            H5P_DEFAULT);
+      }
+
+      if(H5Lexists(link_file_id, gp, H5P_DEFAULT) && H5Lexists(link_file_id, iterator->second.c_str(), H5P_DEFAULT)) {
+        link_group_2_id = H5Gopen2(link_file_id, iterator->second.c_str(), H5P_DEFAULT);
+      } else {
+        link_group_id = H5Gcreate2(link_file_id, gp, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        link_group_2_id = H5Gcreate2(link_group_id, ds, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        H5Gclose(link_group_id);
+      }
+
+      hid_t attr_id = H5Acreate2(shard_ds_id, "dims", H5T_NATIVE_INT, attr_ds_id,
+          H5P_DEFAULT, H5P_DEFAULT);
+
+      status = H5Awrite(attr_id, H5T_NATIVE_INT, shard_dims);
+      H5Aclose(attr_id);
+
+      H5Dclose(shard_ds_id);
+      H5Gclose(shard_group_id);
       H5Fclose(shard_file_id);
-      
-      i++;
+
+      status = H5Lcreate_external(pieces[i].shard_name, iterator->second.c_str(),
+          link_group_2_id, ds_name_stream.str().c_str(),
+          H5P_DEFAULT, H5P_DEFAULT);
+
+      shard_file_id = H5Fopen(pieces[i].shard_name, H5F_ACC_RDWR, H5P_DEFAULT);      
+      H5Gclose(link_group_2_id);
     }
-      H5Fclose(link_file_id);
+    H5Fclose(shard_file_id);
+
+    i++;
+  }
+  H5Fclose(link_file_id);
 }
 
 
