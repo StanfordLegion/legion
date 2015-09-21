eace5045dc4598588b2ac2f5a7db81ea42a2e0c4
Galen Shipman <gshipman@lanl.gov>
2015-09-21 08:57:01 -0700

    revert runtime.mk changes..

--- runtime/runtime.mk
+++ runtime/runtime.mk
@@ -252,7 +252,7 @@ endif
 CC_FLAGS	+= -DCOMPILE_TIME_MIN_LEVEL=$(OUTPUT_LEVEL)
 
 # demand warning-free compilation
-CC_FLAGS        += -Wall -Werror -Wno-format  -Wno-sign-compare
+CC_FLAGS        += -Wall -Werror
 
 #CC_FLAGS += -DUSE_MASKED_COPIES
 
