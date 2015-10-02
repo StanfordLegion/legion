e8775598efe5210d552f0934659ae4b5801cb2bc
Sean Treichler <sjt@cs.stanford.edu>
2015-10-02 15:47:40 -0700

    runtime: monkey with realm link order

--- runtime/runtime.mk
+++ runtime/runtime.mk
@@ -257,14 +257,6 @@ ASM_SRC		?=
 
 # Set the source files
 ifeq ($(strip $(SHARED_LOWLEVEL)),0)
-LOW_RUNTIME_SRC	+= $(LG_RT_DIR)/lowlevel.cc $(LG_RT_DIR)/lowlevel_disk.cc
-ifeq ($(strip $(USE_CUDA)),1)
-LOW_RUNTIME_SRC += $(LG_RT_DIR)/realm/cuda/cuda_module.cc \
-		   $(LG_RT_DIR)/realm/cuda/cudart_hijack.cc
-endif
-ifeq ($(strip $(USE_GASNET)),1)
-LOW_RUNTIME_SRC += $(LG_RT_DIR)/activemsg.cc
-endif
 LOW_RUNTIME_SRC += $(LG_RT_DIR)/realm/runtime_impl.cc \
 	           $(LG_RT_DIR)/lowlevel_dma.cc \
 	           $(LG_RT_DIR)/realm/cmdline.cc \
@@ -279,7 +271,16 @@ LOW_RUNTIME_SRC += $(LG_RT_DIR)/realm/runtime_impl.cc \
 		   $(LG_RT_DIR)/realm/mem_impl.cc \
 		   $(LG_RT_DIR)/realm/inst_impl.cc \
 		   $(LG_RT_DIR)/realm/idx_impl.cc \
-		   $(LG_RT_DIR)/realm/machine_impl.cc
+		   $(LG_RT_DIR)/realm/machine_impl.cc \
+                   $(LG_RT_DIR)/lowlevel.cc \
+                   $(LG_RT_DIR)/lowlevel_disk.cc
+ifeq ($(strip $(USE_CUDA)),1)
+LOW_RUNTIME_SRC += $(LG_RT_DIR)/realm/cuda/cuda_module.cc \
+		   $(LG_RT_DIR)/realm/cuda/cudart_hijack.cc
+endif
+ifeq ($(strip $(USE_GASNET)),1)
+LOW_RUNTIME_SRC += $(LG_RT_DIR)/activemsg.cc
+endif
 GPU_RUNTIME_SRC +=
 else
 CC_FLAGS	+= -DSHARED_LOWLEVEL
