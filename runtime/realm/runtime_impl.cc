ca7603d5a2e0426daa3a6f86bc8f41c9f5487f4d
Sean Treichler <sjt@cs.stanford.edu>
2015-10-04 23:09:46 -0700

    realm: protect against crazy apps that set argc==0

--- runtime/realm/runtime_impl.cc
+++ runtime/realm/runtime_impl.cc
@@ -436,9 +436,12 @@ namespace Realm {
 
       // new command-line parsers will work from a vector<string> representation of the
       //  command line
-      std::vector<std::string> cmdline(*argc - 1);
-      for(int i = 1; i < *argc; i++)
-	cmdline[i - 1] = (*argv)[i];
+      std::vector<std::string> cmdline;
+      if(*argc > 1) {
+	cmdline.resize(*argc - 1);
+	for(int i = 1; i < *argc; i++)
+	  cmdline[i - 1] = (*argv)[i];
+      }
 
       // very first thing - let the logger initialization happen
       Logger::configure_from_cmdline(cmdline);
