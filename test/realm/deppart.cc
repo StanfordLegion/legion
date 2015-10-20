a1543c4f74e2f3e270efa899f6f2de03ac903a2a
Sean Treichler <sjt@cs.stanford.edu>
2015-10-20 16:38:32 -0700

    deppart: WIP to make test support different app configs

--- test/realm/deppart.cc
+++ test/realm/deppart.cc
@@ -15,7 +15,7 @@ using namespace Realm;
 // Task IDs, some IDs are reserved so start at first available number
 enum {
   TOP_LEVEL_TASK = Processor::TASK_ID_FIRST_AVAILABLE+0,
-  INIT_DATA_TASK,
+  INIT_CIRCUIT_DATA_TASK,
 };
 
 // we're going to use alarm() as a watchdog to detect deadlocks
@@ -25,99 +25,251 @@ void sigalrm_handler(int sig)
   exit(1);
 }
 
-static int num_nodes = 100;
-static int num_edges = 10;
-static int num_pieces = 2;
-static int pct_wire_in_piece = 50;
-static int random_seed = 12345;
-static bool random_colors = false;
-static bool show_graph = true;
-static bool wait_on_events = false;
-
-struct InitDataArgs {
-  int index;
-  RegionInstance ri_nodes, ri_edges;
+template <typename T, T DEFVAL>
+class WithDefault {
+public:
+  WithDefault(void) : val(DEFVAL) {}
+  WithDefault(T _val) : val(_val) {}
+  WithDefault<T,DEFVAL>& operator=(T _val) { val = _val; return *this; }
+  operator T(void) const { return val; }
+protected:
+  T val;
 };
 
 Logger log_app("app");
 
-static ZPoint<1> random_node(const ZIndexSpace<1>& is_nodes, unsigned short *rngstate, bool in_piece)
-{
-  if(in_piece)
-    return (is_nodes.bounds.lo + (nrand48(rngstate) % (is_nodes.bounds.hi.x - is_nodes.bounds.lo.x + 1)));
-  else
-    return (nrand48(rngstate) % num_nodes);
-}
+class TestInterface {
+public:
+  virtual ~TestInterface(void) {}
 
-void init_data_task(const void *args, size_t arglen, Processor p)
-{
-  const InitDataArgs& i_args = *(const InitDataArgs *)args;
+  virtual void print_info(void) = 0;
 
-  log_app.print() << "init task #" << i_args.index << " (ri_nodes=" << i_args.ri_nodes << ", ri_edges=" << i_args.ri_edges << ")";
+  virtual Event initialize_data(const std::vector<Memory>& memories,
+				const std::vector<Processor>& procs) = 0;
 
-  ZIndexSpace<1> is_nodes = i_args.ri_nodes.get_indexspace<1>();
-  ZIndexSpace<1> is_edges = i_args.ri_edges.get_indexspace<1>();
+  virtual Event perform_partitioning(void) = 0;
+};
 
-  log_app.print() << "N: " << is_nodes;
-  log_app.print() << "E: " << is_edges;
+// generic configuration settings
+namespace {
+  int random_seed = 12345;
+  bool random_colors = false;
+  //bool wait_on_events = false;
+  bool show_graph = true;
+  TestInterface *testcfg = 0;
+};
 
-  unsigned short rngstate[3];
-  rngstate[0] = random_seed;
-  rngstate[1] = is_nodes.bounds.lo;
-  rngstate[2] = 0;
-  for(int i = 0; i < 20; i++) nrand48(rngstate);
+class CircuitTest : public TestInterface {
+public:
+  // graph config parameters
+  WithDefault<int, 100> num_nodes;
+  WithDefault<int,  10> num_edges;
+  WithDefault<int,   2> num_pieces;
+  WithDefault<int,  50> pct_wire_in_piece;
 
+  CircuitTest(int argc, const char *argv[])
   {
-    AffineAccessor<int,1> a_subckt_id(i_args.ri_nodes, 0 /* offset */);
+    for(int i = 1; i < argc; i++) {
+      if(!strcmp(argv[i], "-n")) {
+	num_nodes = atoi(argv[++i]);
+	continue;
+      }
 
-    for(int i = is_nodes.bounds.lo; i <= is_nodes.bounds.hi; i++) {
-      int color;
-      if(random_colors)
-	color = nrand48(rngstate) % num_pieces;
-      else
-	color = i_args.index;
-      a_subckt_id.write(i, color);
+      if(!strcmp(argv[i], "-e")) {
+	num_edges = atoi(argv[++i]);
+	continue;
+      }
+
+      if(!strcmp(argv[i], "-p")) {
+	num_pieces = atoi(argv[++i]);
+	continue;
+      }
     }
   }
 
-  {
-    AffineAccessor<ZPoint<1>,1> a_in_node(i_args.ri_edges, 0 * sizeof(ZPoint<1>) /* offset */);
-    AffineAccessor<ZPoint<1>,1> a_out_node(i_args.ri_edges, 1 * sizeof(ZPoint<1>) /* offset */);
+  struct InitDataArgs {
+    int index;
+    RegionInstance ri_nodes, ri_edges;
+  };
 
-    for(int i = is_edges.bounds.lo; i <= is_edges.bounds.hi; i++) {
-      int in_node = random_node(is_nodes, rngstate,
-				!random_colors);
-      int out_node = random_node(is_nodes, rngstate,
-				 !random_colors && ((nrand48(rngstate) % 100) < pct_wire_in_piece));
-      a_in_node.write(i, in_node);
-      a_out_node.write(i, out_node);
+  ZPoint<1> random_node(const ZIndexSpace<1>& is_nodes, unsigned short *rngstate, bool in_piece)
+  {
+    if(in_piece)
+      return (is_nodes.bounds.lo + (nrand48(rngstate) % (is_nodes.bounds.hi.x - is_nodes.bounds.lo.x + 1)));
+    else
+      return (nrand48(rngstate) % num_nodes);
+  }
+
+  static void init_data_task_wrapper(const void *args, size_t arglen, Processor p)
+  {
+    CircuitTest *me = (CircuitTest *)testcfg;
+    me->init_data_task(args, arglen, p);
+  }
+
+  void init_data_task(const void *args, size_t arglen, Processor p)
+  {
+    const InitDataArgs& i_args = *(const InitDataArgs *)args;
+
+    log_app.print() << "init task #" << i_args.index << " (ri_nodes=" << i_args.ri_nodes << ", ri_edges=" << i_args.ri_edges << ")";
+
+    ZIndexSpace<1> is_nodes = i_args.ri_nodes.get_indexspace<1>();
+    ZIndexSpace<1> is_edges = i_args.ri_edges.get_indexspace<1>();
+
+    log_app.print() << "N: " << is_nodes;
+    log_app.print() << "E: " << is_edges;
+    
+    unsigned short rngstate[3];
+    rngstate[0] = random_seed;
+    rngstate[1] = is_nodes.bounds.lo;
+    rngstate[2] = 0;
+    for(int i = 0; i < 20; i++) nrand48(rngstate);
+
+    {
+      AffineAccessor<int,1> a_subckt_id(i_args.ri_nodes, 0 /* offset */);
+      
+      for(int i = is_nodes.bounds.lo; i <= is_nodes.bounds.hi; i++) {
+	int color;
+	if(random_colors)
+	  color = nrand48(rngstate) % num_pieces;
+	else
+	  color = i_args.index;
+	a_subckt_id.write(i, color);
+      }
+    }
+    
+    {
+      AffineAccessor<ZPoint<1>,1> a_in_node(i_args.ri_edges, 0 * sizeof(ZPoint<1>) /* offset */);
+      AffineAccessor<ZPoint<1>,1> a_out_node(i_args.ri_edges, 1 * sizeof(ZPoint<1>) /* offset */);
+      
+      for(int i = is_edges.bounds.lo; i <= is_edges.bounds.hi; i++) {
+	int in_node = random_node(is_nodes, rngstate,
+				  !random_colors);
+	int out_node = random_node(is_nodes, rngstate,
+				   !random_colors && ((nrand48(rngstate) % 100) < pct_wire_in_piece));
+	a_in_node.write(i, in_node);
+	a_out_node.write(i, out_node);
+      }
+    }
+
+    if(show_graph) {
+      AffineAccessor<int,1> a_subckt_id(i_args.ri_nodes, 0 /* offset */);
+
+      for(int i = is_nodes.bounds.lo; i <= is_nodes.bounds.hi; i++)
+	std::cout << "subckt_id[" << i << "] = " << a_subckt_id.read(i) << std::endl;
+
+      AffineAccessor<ZPoint<1>,1> a_in_node(i_args.ri_edges, 0 * sizeof(ZPoint<1>) /* offset */);
+
+      for(int i = is_edges.bounds.lo; i <= is_edges.bounds.hi; i++)
+	std::cout << "in_node[" << i << "] = " << a_in_node.read(i) << std::endl;
+
+      AffineAccessor<ZPoint<1>,1> a_out_node(i_args.ri_edges, 1 * sizeof(ZPoint<1>) /* offset */);
+
+      for(int i = is_edges.bounds.lo; i <= is_edges.bounds.hi; i++)
+	std::cout << "out_node[" << i << "] = " << a_out_node.read(i) << std::endl;
     }
   }
 
-  if(show_graph) {
-    AffineAccessor<int,1> a_subckt_id(i_args.ri_nodes, 0 /* offset */);
+  ZIndexSpace<1> is_nodes, is_edges;
+  std::vector<RegionInstance> ri_nodes;
+  std::vector<FieldDataDescriptor<ZIndexSpace<1>, int> > subckt_field_data;
+  std::vector<RegionInstance> ri_edges;
+  std::vector<FieldDataDescriptor<ZIndexSpace<1>, ZPoint<1> > > in_node_field_data;
+  std::vector<FieldDataDescriptor<ZIndexSpace<1>, ZPoint<1> > > out_node_field_data;
 
-    for(int i = is_nodes.bounds.lo; i <= is_nodes.bounds.hi; i++)
-      std::cout << "subckt_id[" << i << "] = " << a_subckt_id.read(i) << std::endl;
-
-    AffineAccessor<ZPoint<1>,1> a_in_node(i_args.ri_edges, 0 * sizeof(ZPoint<1>) /* offset */);
-
-    for(int i = is_edges.bounds.lo; i <= is_edges.bounds.hi; i++)
-      std::cout << "in_node[" << i << "] = " << a_in_node.read(i) << std::endl;
-
-    AffineAccessor<ZPoint<1>,1> a_out_node(i_args.ri_edges, 1 * sizeof(ZPoint<1>) /* offset */);
-
-    for(int i = is_edges.bounds.lo; i <= is_edges.bounds.hi; i++)
-      std::cout << "out_node[" << i << "] = " << a_out_node.read(i) << std::endl;
+  virtual void print_info(void)
+  {
+    printf("Realm dependent partitioning test - circuit: %d nodes, %d edges, %d pieces\n",
+	   (int)num_nodes, (int)num_edges, (int)num_pieces);
   }
-}
+
+  virtual Event initialize_data(const std::vector<Memory>& memories,
+				const std::vector<Processor>& procs)
+  {
+    // now create index spaces for nodes and edges
+    is_nodes = ZRect<1>(0, num_nodes - 1);
+    is_edges = ZRect<1>(0, num_edges - 1);
+
+    // equal partition is used to do initial population of edges and nodes
+    std::vector<ZIndexSpace<1> > ss_nodes_eq;
+    std::vector<ZIndexSpace<1> > ss_edges_eq;
+
+    is_nodes.create_equal_subspaces(num_pieces, 1, ss_nodes_eq, Realm::ProfilingRequestSet()).wait();
+    is_edges.create_equal_subspaces(num_pieces, 1, ss_edges_eq, Realm::ProfilingRequestSet()).wait();
+
+    std::cout << "Initial partitions:" << std::endl;
+    for(size_t i = 0; i < ss_nodes_eq.size(); i++)
+      std::cout << " Nodes #" << i << ": " << ss_nodes_eq[i] << std::endl;
+    for(size_t i = 0; i < ss_edges_eq.size(); i++)
+      std::cout << " Edges #" << i << ": " << ss_edges_eq[i] << std::endl;
+
+    // create instances for each of these subspaces
+    std::vector<size_t> node_fields, edge_fields;
+    node_fields.push_back(sizeof(int));  // subckt_id
+    assert(sizeof(int) == sizeof(ZPoint<1>));
+    edge_fields.push_back(sizeof(ZPoint<1>));  // in_node
+    edge_fields.push_back(sizeof(ZPoint<1>));  // out_node
+
+    ri_nodes.resize(num_pieces);
+    subckt_field_data.resize(num_pieces);
+
+    for(size_t i = 0; i < ss_nodes_eq.size(); i++) {
+      RegionInstance ri = ss_nodes_eq[i].create_instance(memories[i % memories.size()],
+							 node_fields,
+							 1,
+							 Realm::ProfilingRequestSet());
+      ri_nodes[i] = ri;
+    
+      subckt_field_data[i].index_space = ss_nodes_eq[i];
+      subckt_field_data[i].inst = ri_nodes[i];
+      subckt_field_data[i].field_offset = 0;
+    }
+
+    ri_edges.resize(num_pieces);
+    in_node_field_data.resize(num_pieces);
+    out_node_field_data.resize(num_pieces);
+
+    for(size_t i = 0; i < ss_edges_eq.size(); i++) {
+      RegionInstance ri = ss_edges_eq[i].create_instance(memories[i % memories.size()],
+							 edge_fields,
+							 1,
+							 Realm::ProfilingRequestSet());
+      ri_edges[i] = ri;
+
+      in_node_field_data[i].index_space = ss_edges_eq[i];
+      in_node_field_data[i].inst = ri_edges[i];
+      in_node_field_data[i].field_offset = 0 * sizeof(ZPoint<1>);
+      
+      out_node_field_data[i].index_space = ss_edges_eq[i];
+      out_node_field_data[i].inst = ri_edges[i];
+      out_node_field_data[i].field_offset = 1 * sizeof(ZPoint<1>);
+    }
+
+    // fire off tasks to initialize data
+    std::set<Event> events;
+    for(int i = 0; i < num_pieces; i++) {
+      Processor p = procs[i % memories.size()];
+      InitDataArgs args;
+      args.index = i;
+      args.ri_nodes = ri_nodes[i];
+      args.ri_edges = ri_edges[i];
+      Event e = p.spawn(INIT_CIRCUIT_DATA_TASK, &args, sizeof(args));
+      events.insert(e);
+    }
+
+    return Event::merge_events(events);
+  }
+
+  virtual Event perform_partitioning(void)
+  {
+    return Event::NO_EVENT;
+  }
+};
 
 void top_level_task(const void *args, size_t arglen, Processor p)
 {
   int errors = 0;
 
-  printf("Realm dependent partitioning test - %d nodes, %d edges, %d pieces\n",
-	 num_nodes, num_edges, num_pieces);
+  testcfg->print_info();
 
   // find all the system memories - we'll stride our data across them
   // for each memory, we'll need one CPU that can do the initialization of the data
@@ -152,79 +304,11 @@ void top_level_task(const void *args, size_t arglen, Processor p)
   }
   assert(sysmems.size() > 0);
 
-  // now create index spaces for nodes and edges
-  ZIndexSpace<1> is_nodes(ZRect<1>(0, num_nodes - 1));
-  ZIndexSpace<1> is_edges(ZRect<1>(0, num_edges - 1));
+  Event e = testcfg->initialize_data(sysmems, procs);
+  // wait for all initialization to be done
+  e.wait();
 
-  // equal partition is used to do initial population of edges and nodes
-  std::vector<ZIndexSpace<1> > ss_nodes_eq;
-  std::vector<ZIndexSpace<1> > ss_edges_eq;
-
-  is_nodes.create_equal_subspaces(num_pieces, 1, ss_nodes_eq, Realm::ProfilingRequestSet()).wait();
-  is_edges.create_equal_subspaces(num_pieces, 1, ss_edges_eq, Realm::ProfilingRequestSet()).wait();
-
-  std::cout << "Initial partitions:" << std::endl;
-  for(size_t i = 0; i < ss_nodes_eq.size(); i++)
-    std::cout << " Nodes #" << i << ": " << ss_nodes_eq[i] << std::endl;
-  for(size_t i = 0; i < ss_edges_eq.size(); i++)
-    std::cout << " Edges #" << i << ": " << ss_edges_eq[i] << std::endl;
-
-  // create instances for each of these subspaces
-  std::vector<size_t> node_fields, edge_fields;
-  node_fields.push_back(sizeof(int));  // subckt_id
-  assert(sizeof(int) == sizeof(ZPoint<1>));
-  edge_fields.push_back(sizeof(ZPoint<1>));  // in_node
-  edge_fields.push_back(sizeof(ZPoint<1>));  // out_node
-
-  std::vector<RegionInstance> ri_nodes(num_pieces);
-  std::vector<FieldDataDescriptor<ZIndexSpace<1>, int> > subckt_field_data(num_pieces);
-
-  for(size_t i = 0; i < ss_nodes_eq.size(); i++) {
-    RegionInstance ri = ss_nodes_eq[i].create_instance(sysmems[i % sysmems.size()],
-						       node_fields,
-						       1,
-						       Realm::ProfilingRequestSet());
-    ri_nodes[i] = ri;
-
-    subckt_field_data[i].index_space = ss_nodes_eq[i];
-    subckt_field_data[i].inst = ri_nodes[i];
-    subckt_field_data[i].field_offset = 0;
-  }
-
-  std::vector<RegionInstance> ri_edges(num_pieces);
-  std::vector<FieldDataDescriptor<ZIndexSpace<1>, ZPoint<1> > > in_node_field_data(num_pieces);
-  std::vector<FieldDataDescriptor<ZIndexSpace<1>, ZPoint<1> > > out_node_field_data(num_pieces);
-
-  for(size_t i = 0; i < ss_edges_eq.size(); i++) {
-    RegionInstance ri = ss_edges_eq[i].create_instance(sysmems[i % sysmems.size()],
-						       edge_fields,
-						       1,
-						       Realm::ProfilingRequestSet());
-    ri_edges[i] = ri;
-
-    in_node_field_data[i].index_space = ss_edges_eq[i];
-    in_node_field_data[i].inst = ri_edges[i];
-    in_node_field_data[i].field_offset = 0 * sizeof(ZPoint<1>);
-      
-    out_node_field_data[i].index_space = ss_edges_eq[i];
-    out_node_field_data[i].inst = ri_edges[i];
-    out_node_field_data[i].field_offset = 1 * sizeof(ZPoint<1>);
-  }
-
-  // fire off tasks to initialize data
-  std::set<Event> events;
-  for(int i = 0; i < num_pieces; i++) {
-    Processor p = procs[i % sysmems.size()];
-    InitDataArgs args;
-    args.index = i;
-    args.ri_nodes = ri_nodes[i];
-    args.ri_edges = ri_edges[i];
-    Event e = p.spawn(INIT_DATA_TASK, &args, sizeof(args));
-    events.insert(e);
-  }
-  // we're going to time the next stuff, so always wait here
-  Event::merge_events(events).wait();
-
+#if 0
   // the outputs of our partitioning will be:
   //  is_private, is_shared - subsets of is_nodes based on private/shared
   //  p_pvt, p_shr, p_ghost - subsets of the above split by subckt
@@ -308,7 +392,7 @@ void top_level_task(const void *args, size_t arglen, Processor p)
     e7.wait();
     e8.wait();
   }
-
+#endif
   if(errors > 0) {
     printf("Exiting with errors\n");
     exit(1);
@@ -326,25 +410,10 @@ int main(int argc, char **argv)
 
   rt.init(&argc, &argv);
 
-  for(int i = 1; i < argc; i++) {
-    if(!strcmp(argv[i], "-n")) {
-      num_nodes = atoi(argv[++i]);
-      continue;
-    }
-
-    if(!strcmp(argv[i], "-e")) {
-      num_edges = atoi(argv[++i]);
-      continue;
-    }
-
-    if(!strcmp(argv[i], "-p")) {
-      num_pieces = atoi(argv[++i]);
-      continue;
-    }
-  }
+  testcfg = new CircuitTest(argc, (const char **)argv);
 
   rt.register_task(TOP_LEVEL_TASK, top_level_task);
-  rt.register_task(INIT_DATA_TASK, init_data_task);
+  rt.register_task(INIT_CIRCUIT_DATA_TASK, CircuitTest::init_data_task_wrapper);
 
   signal(SIGALRM, sigalrm_handler);
 
