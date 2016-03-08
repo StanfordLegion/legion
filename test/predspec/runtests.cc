c00fcf208a365cfb3250ce55b3e06ecd560bf6e9
Sean Treichler <sjt@cs.stanford.edu>
2016-03-07 16:00:44 -0800

    tests: split into multiple files - keep tests separate

--- test/predspec/runtests.cc
+++ test/predspec/runtests.cc
@@ -18,12 +18,10 @@
 #include <cstdlib>
 #include <math.h>
 #include <queue>
-#include "legion.h"
-#include "default_mapper.h"
 
-using namespace LegionRuntime::HighLevel;
-using namespace LegionRuntime::Accessor;
-using namespace LegionRuntime::Arrays;
+#include "runtests.h"
+#include "testtasks.h"
+#include "testmapper.h"
 
 LegionRuntime::Logger::Category log_app("app");
 
@@ -31,14 +29,6 @@ enum TaskIDs {
   TOP_LEVEL_TASK_ID,
   RUN_TEST_TASK_ID,
 };
-
-enum FieldIDs {
-  FID_X = 10000,
-  FID_Y,
-  FID_Z,
-};
-
-#define FID(n) (FID_X + (n))
 
 enum {
   REDOPID_BOOL_OR = 77,
@@ -75,178 +65,34 @@ struct BinaryReductionOp {
 
 typedef BinaryReductionOp<bool, logical_or<bool>, REDOPID_BOOL_OR> BooleanOrRedop;
 
-class SetEntry {
-public:
-  struct Args {
-    int idx;
-    int newval;
-  };
 
-  class Launcher : public TaskLauncher {
-  public:
-    Launcher(LogicalRegion region, int idx, FieldID fid, int newval,
-	     Predicate pred = Predicate::TRUE_PRED);
-  };
+////////////////////////////////////////////////////////////////////////
+//
+// class TestInformation
+//
 
-  static TaskID taskid;
-
-  static Future run(Runtime *runtime, Context ctx,
-		    LogicalRegion region, int idx, FieldID fid, int newval,
-		    Predicate pred = Predicate::TRUE_PRED);
-
-  static void preregister_tasks(void);
-
-  static void cpu_task(const Task *task,
-		       const std::vector<PhysicalRegion> &regions,
-		       Context ctx, HighLevelRuntime *runtime);
+namespace {
+  TestInformation *test_list_head = 0;
 };
 
-SetEntry::Launcher::Launcher(LogicalRegion region, int idx, FieldID fid, int newval,
-			     Predicate pred /*= Predicate::TRUE_PRED*/)
+TestInformation::TestInformation(const char *_name, TestEntryFn _entry_fn,
+				 int _num_regions, int _num_elements, int _num_fields)
+  : entry_fn(_entry_fn)
+  , num_regions(_num_regions)
+  , num_elements(_num_elements)
+  , num_fields(_num_fields)
 {
-  this->task_id = taskid;
-  Args *args = new Args; // this is a memory leak for now
-  args->idx = idx;
-  args->newval = newval;
-  this->argument = TaskArgument(args, sizeof(Args));
-  this->predicate = pred;
-
-  this->add_region_requirement(RegionRequirement(region, READ_WRITE, EXCLUSIVE, region)
-			       .add_field(fid));
+  strcpy(name, _name);
+  do {
+    next = test_list_head;
+  } while(!__sync_bool_compare_and_swap(&test_list_head, next, this));
 }
 
-Future SetEntry::run(Runtime *runtime, Context ctx,
-		     LogicalRegion region, int idx, FieldID fid, int newval,
-		     Predicate pred /*= Predicate::TRUE_PRED*/)
-{
-  Launcher l(region, idx, fid, newval, pred);
-  return runtime->execute_task(ctx, l);
-}
 
-/*static*/ TaskID SetEntry::taskid = -1;
-void SetEntry::preregister_tasks(void)
-{
-  taskid = Runtime::generate_static_task_id();
-  TaskVariantRegistrar tvr(taskid, "set_entry");
-  tvr.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
-  tvr.set_leaf(true);
-  Runtime::preregister_task_variant<cpu_task>(tvr, "set_entry");
-}
-
-void SetEntry::cpu_task(const Task *task,
-			const std::vector<PhysicalRegion> &regions,
-			Context ctx, HighLevelRuntime *runtime)
-{
-  const Args& args = *(const Args *)(task->args);
-  RegionAccessor<AccessorType::Affine<1>, int> ra = regions[0].get_field_accessor(task->regions[0].instance_fields[0]).typeify<int>().convert<AccessorType::Affine<1> >();
-
-  log_app.debug() << task->regions[0].region << "." << task->regions[0].instance_fields[0] << "[" << args.idx << "] = " << args.newval;
-  ra[args.idx] = args.newval;
-}
-  
-class CheckEntry {
-public:
-  struct Args {
-    int idx;
-    int checkval;
-  };
-
-  class Launcher : public TaskLauncher {
-  public:
-    Launcher(LogicalRegion region, int idx, FieldID fid, int checkval,
-	     Predicate pred = Predicate::TRUE_PRED);
-  };
-
-  static TaskID taskid;
-
-  static Future run(Runtime *runtime, Context ctx,
-		    LogicalRegion region, int idx, FieldID fid, int checkval,
-		    Predicate pred = Predicate::TRUE_PRED);
-
-  static void preregister_tasks(void);
-
-  static bool cpu_task(const Task *task,
-		       const std::vector<PhysicalRegion> &regions,
-		       Context ctx, HighLevelRuntime *runtime);
-};
-
-CheckEntry::Launcher::Launcher(LogicalRegion region, int idx, FieldID fid, int checkval,
-			     Predicate pred /*= Predicate::TRUE_PRED*/)
-{
-  this->task_id = taskid;
-  Args *args = new Args; // this is a memory leak for now
-  args->idx = idx;
-  args->checkval = checkval;
-  this->argument = TaskArgument(args, sizeof(Args));
-  this->predicate = pred;
-
-  this->add_region_requirement(RegionRequirement(region, READ_WRITE, EXCLUSIVE, region)
-			       .add_field(fid));
-}
-
-Future CheckEntry::run(Runtime *runtime, Context ctx,
-		     LogicalRegion region, int idx, FieldID fid, int checkval,
-		     Predicate pred /*= Predicate::TRUE_PRED*/)
-{
-  Launcher l(region, idx, fid, checkval, pred);
-  return runtime->execute_task(ctx, l);
-}
-
-/*static*/ TaskID CheckEntry::taskid = -1;
-void CheckEntry::preregister_tasks(void)
-{
-  taskid = Runtime::generate_static_task_id();
-  TaskVariantRegistrar tvr(taskid, "check_entry");
-  tvr.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
-  tvr.set_leaf(true);
-  Runtime::preregister_task_variant<bool, cpu_task>(tvr, "check_entry");
-}
-
-bool CheckEntry::cpu_task(const Task *task,
-			  const std::vector<PhysicalRegion> &regions,
-			  Context ctx, HighLevelRuntime *runtime)
-{
-  const Args& args = *(const Args *)(task->args);
-  RegionAccessor<AccessorType::Affine<1>, int> ra = regions[0].get_field_accessor(task->regions[0].instance_fields[0]).typeify<int>().convert<AccessorType::Affine<1> >();
-
-  int actval = ra[args.idx];
-  if(actval == args.checkval) {
-    log_app.debug() << task->regions[0].region << "." << task->regions[0].instance_fields[0] << "[" << args.idx << "] == " << args.checkval;
-    return true;
-  } else {
-    log_app.error() << task->regions[0].region << "." << task->regions[0].instance_fields[0] << "[" << args.idx << "] == " << actval << " (exp: " << args.checkval << ")";
-    return false;
-  }
-}
-  
-
-struct Globals {
-  IndexSpace ispace;
-  FieldSpace fspace;
-  std::vector<LogicalRegion> regions;
-};
-
-enum TestResult {
-  RESULT_UNKNOWN,
-  RESULT_PASS,
-  RESULT_FAIL,
-  RESULT_SKIP,
-};
-
-typedef TestResult (*TestEntryFn)(Runtime *runtime, Context ctx, const Globals& g);  
-
-class DelayedPredicate {
-public:
-  DelayedPredicate(Runtime *_runtime, Context _ctx);
-  operator Predicate(void) const;
-  DelayedPredicate& operator=(bool newval);
-  DelayedPredicate& operator=(Future f);
-protected:
-  Runtime *runtime;
-  Context ctx;
-  DynamicCollective dc;
-  Predicate p;
-};
+////////////////////////////////////////////////////////////////////////
+//
+// class DelayedPredicate
+//
 
 DelayedPredicate::DelayedPredicate(Runtime *_runtime, Context _ctx)
   : runtime(_runtime), ctx(_ctx)
@@ -275,47 +121,17 @@ DelayedPredicate& DelayedPredicate::operator=(Future f)
   return *this;
 }
 
-template <bool PREDVAL>
-TestResult test_pred_const(Runtime *runtime, Context ctx, const Globals& g)
-{
-  SetEntry::run(runtime, ctx, g.regions[0], 0, FID(0), 45);
-  SetEntry::run(runtime, ctx, g.regions[0], 0, FID(0), 46,
-		(PREDVAL ? Predicate::TRUE_PRED : Predicate::FALSE_PRED));
-  Future f = CheckEntry::run(runtime, ctx, g.regions[0], 0, FID(0), 
-			     (PREDVAL ? 46 : 45));
-  bool b = f.get_result<bool>();
-  return b ? RESULT_PASS : RESULT_FAIL;
-}
 
-template <bool PREDVAL, bool EARLY>
-TestResult test_pred_simple(Runtime *runtime, Context ctx, const Globals& g)
-{
-  SetEntry::run(runtime, ctx, g.regions[0], 0, FID(0), 45);
-  DelayedPredicate dp(runtime, ctx);
-  if(EARLY)
-    dp = PREDVAL;
-  SetEntry::run(runtime, ctx, g.regions[0], 0, FID(0), 46, dp);
-  Future f = CheckEntry::run(runtime, ctx, g.regions[0], 0, FID(0), 
-			     (PREDVAL ? 46 : 45));
-  if(!EARLY)
-    dp = PREDVAL;
-  bool b = f.get_result<bool>();
-  return b ? RESULT_PASS : RESULT_FAIL;
-}
-
-struct RunSingleTestArgs {
-  char name[40];
-  TestEntryFn entry_fn;
-  int num_regions;
-  int num_elements;
-  int num_fields;
-};
+////////////////////////////////////////////////////////////////////////
+//
+// task definitions
+//
 
 void run_test_task(const Task *task,
 		   const std::vector<PhysicalRegion> &regions,
 		   Context ctx, HighLevelRuntime *runtime)
 {
-  const RunSingleTestArgs& args = *(const RunSingleTestArgs *)(task->args);
+  const TestInformation& args = *(const TestInformation *)(task->args);
   Globals g;
 
   g.ispace = runtime->create_index_space(ctx, Domain::from_rect<1>(Rect<1>(0, args.num_elements-1)));
@@ -363,48 +179,13 @@ void top_level_task(const Task *task,
                     const std::vector<PhysicalRegion> &regions,
                     Context ctx, HighLevelRuntime *runtime)
 {
-  RunSingleTestArgs cargs;
-  //strcpy(cargs.name, "foo");
-  //cargs.entry_fn = test_foo;
-  cargs.num_elements = 1;
-  cargs.num_fields = 2;
-  cargs.num_regions = 1;
-  TaskLauncher launcher(RUN_TEST_TASK_ID, TaskArgument(&cargs, sizeof(cargs)));
-  {
-    strcpy(cargs.name, "pred_const_false");
-    cargs.entry_fn = test_pred_const<false>;
+  const TestInformation *test_info = test_list_head;
+  while(test_info) {
+    TaskLauncher launcher(RUN_TEST_TASK_ID, TaskArgument(test_info, sizeof(TestInformation)));
     Future f = runtime->execute_task(ctx, launcher);
     f.get_void_result();
-  }
-  {
-    strcpy(cargs.name, "pred_const_true");
-    cargs.entry_fn = test_pred_const<true>;
-    Future f = runtime->execute_task(ctx, launcher);
-    f.get_void_result();
-  }
-  {
-    strcpy(cargs.name, "pred_simple_false");
-    cargs.entry_fn = test_pred_simple<false, false>;
-    Future f = runtime->execute_task(ctx, launcher);
-    f.get_void_result();
-  }
-  {
-    strcpy(cargs.name, "pred_simple_true");
-    cargs.entry_fn = test_pred_simple<true, false>;
-    Future f = runtime->execute_task(ctx, launcher);
-    f.get_void_result();
-  }
-  {
-    strcpy(cargs.name, "pred_simple_false_early");
-    cargs.entry_fn = test_pred_simple<false, true>;
-    Future f = runtime->execute_task(ctx, launcher);
-    f.get_void_result();
-  }
-  {
-    strcpy(cargs.name, "pred_simple_true_early");
-    cargs.entry_fn = test_pred_simple<true, true>;
-    Future f = runtime->execute_task(ctx, launcher);
-    f.get_void_result();
+
+    test_info = test_info->next;
   }
 }
 
@@ -414,7 +195,7 @@ static void update_mappers(Machine machine, HighLevelRuntime *rt,
   for (std::set<Processor>::const_iterator it = local_procs.begin();
         it != local_procs.end(); it++)
   {
-    //rt->replace_default_mapper(new GCMapper(machine, rt, *it), *it);
+    rt->replace_default_mapper(new TestMapper(machine, rt, *it), *it);
   }
 }
 
---
+++ test/predspec/testtasks.h
@@ -0,0 +1,63 @@
+// helper tasks for tests
+
+#ifndef TESTTASKS_H
+#define TESTTASKS_H
+
+#include "runtests.h"
+
+// sets one field of one entry to the specified value
+class SetEntry {
+public:
+  struct Args {
+    int idx;
+    int newval;
+  };
+
+  class Launcher : public TaskLauncher {
+  public:
+    Launcher(LogicalRegion region, int idx, FieldID fid, int newval,
+	     Predicate pred = Predicate::TRUE_PRED);
+  };
+
+  static TaskID taskid;
+
+  static Future run(Runtime *runtime, Context ctx,
+		    LogicalRegion region, int idx, FieldID fid, int newval,
+		    Predicate pred = Predicate::TRUE_PRED);
+
+  static void preregister_tasks(void);
+
+  static void cpu_task(const Task *task,
+		       const std::vector<PhysicalRegion> &regions,
+		       Context ctx, HighLevelRuntime *runtime);
+};
+
+// checks that one field of one entry has the desired value
+class CheckEntry {
+public:
+  struct Args {
+    int idx;
+    int checkval;
+  };
+
+  class Launcher : public TaskLauncher {
+  public:
+    Launcher(LogicalRegion region, int idx, FieldID fid, int checkval,
+	     Predicate pred = Predicate::TRUE_PRED);
+  };
+
+  static TaskID taskid;
+
+  static Future run(Runtime *runtime, Context ctx,
+		    LogicalRegion region, int idx, FieldID fid, int checkval,
+		    Predicate pred = Predicate::TRUE_PRED);
+
+  static void preregister_tasks(void);
+
+  static bool cpu_task(const Task *task,
+		       const std::vector<PhysicalRegion> &regions,
+		       Context ctx, HighLevelRuntime *runtime);
+};
+
+
+#endif
