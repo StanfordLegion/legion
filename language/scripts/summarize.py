285f9aa520278f147f5602f1fefbc06e9223c32e
Elliott Slaughter <slaughter@cs.stanford.edu>
2016-07-25 16:32:40 -0700

    Revert "regent: Update timing script for individual processor times."
    This reverts commit dbdd970595993b4b75f0f11b3610b293d3677605.

--- language/scripts/summarize.py
+++ language/scripts/summarize.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import collections
 import sys
 
 if __name__ == '__main__':
     for path in sys.argv[1:]:
         with open(path, 'r') as f:
-            t = collections.defaultdict(list)
-            for line in f:
-                if line.startswith('t: '):
-                    proc, time = map(int, line.replace('t: ', '').split())
-                    t[proc].append(time)
+            t = [int(x.replace('t: ', '')) for x in f.read().split('\n') if x.startswith('t: ')]
         if len(t) == 0:
-            print("%s: ERROR" % path)
+            print("%s: ERROR ERROR ERROR" % path)
         else:
-            total = max(*(max(*x) - min(*x) for x in t.values()))
-            print("%s: %s" % (path, total/1e6))
+            first = min(*t)
+            last = max(*t)
+            print("%s: %s %s %s" % (path, first/1e6, last/1e6, (last-first)/1e6))
