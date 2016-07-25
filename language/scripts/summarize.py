dbdd970595993b4b75f0f11b3610b293d3677605
Elliott Slaughter <slaughter@cs.stanford.edu>
2016-07-25 12:39:17 -0700

    regent: Update timing script for individual processor times.

--- language/scripts/summarize.py
+++ language/scripts/summarize.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+import collections
 import sys
 
 if __name__ == '__main__':
     for path in sys.argv[1:]:
         with open(path, 'r') as f:
-            t = [int(x.replace('t: ', '')) for x in f.read().split('\n') if x.startswith('t: ')]
+            t = collections.defaultdict(list)
+            for line in f:
+                if line.startswith('t: '):
+                    proc, time = map(int, line.replace('t: ', '').split())
+                    t[proc].append(time)
         if len(t) == 0:
-            print("%s: ERROR ERROR ERROR" % path)
+            print("%s: ERROR" % path)
         else:
-            first = min(*t)
-            last = max(*t)
-            print("%s: %s %s %s" % (path, first/1e6, last/1e6, (last-first)/1e6))
+            total = max(*(max(*x) - min(*x) for x in t.values()))
+            print("%s: %s" % (path, total/1e6))
