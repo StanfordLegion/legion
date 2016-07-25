1177b85fa02cce9f59c3044ac8caa4b646058d25
Elliott Slaughter <slaughter@cs.stanford.edu>
2016-07-25 13:25:18 -0700

    regent: Update mapper.

--- language/examples/stencil_mapper.cc
+++ language/examples/stencil_mapper.cc
@@ -85,7 +85,8 @@ void StencilMapper::select_task_options(Task *task)
   task->map_locally = true;
   task->profile_task = false;
   const char* task_name = task->get_task_name();
-  if ((strcmp(task_name, "stencil") == 0 ||
+  if ((strcmp(task_name, "fill_") == 0 ||
+       strcmp(task_name, "stencil") == 0 ||
        strcmp(task_name, "increment") == 0 ||
        strcmp(task_name, "check") == 0) &&
       !task->is_index_space)
