45be48e171f7c2541051cf6f0e0dda2ef28541e9
Elliott Slaughter <elliottslaughter@gmail.com>
2017-01-08 15:24:01 -0800

    Bump copyright year.

--- tools/legion_spy.py
+++ tools/legion_spy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2016 Stanford University, NVIDIA Corporation
+# Copyright 2017 Stanford University, NVIDIA Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
