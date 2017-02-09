0f6b66be41f685343c72b5b71a1ac4837bc4193c
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-02-09 11:53:56 -0800

    test: Update list of broken tests.

--- test.py
+++ test.py
@@ -48,6 +48,7 @@ runtime_tests = [
     #['test/matrix_multiply/matrix_multiply', []], # FIXME: Broken: https://github.com/StanfordLegion/legion/issues/222
     #['test/predspec/predspec', []], # FIXME: Broken: https://github.com/StanfordLegion/legion/issues/223
     #['test/read_write/read_write', []], # FIXME: Broken: https://github.com/StanfordLegion/legion/issues/224
+    #['test/rendering/rendering', []], # FIXME: Broken: https://github.com/StanfordLegion/legion/issues/225
 ]
 
 def cmd(command, env=None, cwd=None):
