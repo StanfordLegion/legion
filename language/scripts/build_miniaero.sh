32b3e4ff5f68d9f179d545f84f2c937d6b631c72
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-03-13 07:51:48 +0100

    regent: Update MiniAero build script.

--- language/scripts/build_miniaero.sh
+++ language/scripts/build_miniaero.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
 mkdir "$1"
 cd "$1"
 
@@ -9,19 +11,19 @@ cd "$1"
 
 # DETAILED_MESSAGE_TIMING
 
-SAVEOBJ=1 ../regent.py ../miniaero/rdir_1ghost.rg -fflow 0
-mv ../miniaero/miniaero_rdir_1ghost miniaero.none
+SAVEOBJ=1 $root_dir/../regent.py $root_dir/../miniaero/rdir_1ghost.rg -fflow 0
+mv $root_dir/../miniaero/miniaero_rdir_1ghost miniaero.none
 
-SAVEOBJ=1 ../regent.py ../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 1
-mv ../miniaero/miniaero_rdir_1ghost miniaero.spmd1
-SAVEOBJ=1 ../regent.py ../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 4
-mv ../miniaero/miniaero_rdir_1ghost miniaero.spmd4
-SAVEOBJ=1 ../regent.py ../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 8
-mv ../miniaero/miniaero_rdir_1ghost miniaero.spmd8
-SAVEOBJ=1 ../regent.py ../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 16
-mv ../miniaero/miniaero_rdir_1ghost miniaero.spmd16
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 1
+# mv $root_dir/../miniaero/miniaero_rdir_1ghost miniaero.spmd1
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 4
+# mv $root_dir/../miniaero/miniaero_rdir_1ghost miniaero.spmd4
+SAVEOBJ=1 $root_dir/../regent.py $root_dir/../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 8
+mv $root_dir/../miniaero/miniaero_rdir_1ghost miniaero.spmd8
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 16
+# mv $root_dir/../miniaero/miniaero_rdir_1ghost miniaero.spmd16
 
-cp ../../bindings/terra/liblegion_terra.so .
-cp ../miniaero/librdir_1ghost.so .
+cp $root_dir/../../bindings/terra/liblegion_terra.so .
+cp $root_dir/../miniaero/librdir_1ghost.so .
 
-cp ../scripts/*_miniaero.sh .
+cp $root_dir/../scripts/*_miniaero.sh .
