20efcd4b3289a682d4311bc3a39bddea517c1934
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-03-13 02:33:08 +0100

    regent: Update stencil build script.

--- language/scripts/build_stencil.sh
+++ language/scripts/build_stencil.sh
@@ -2,25 +2,27 @@
 
 set -e
 
+root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
 mkdir "$1"
 cd "$1"
 
-SAVEOBJ=1 ../regent.py ../examples/stencil_fast.rg -fflow 0
+SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 0
 mv stencil stencil.none
 
-# SAVEOBJ=1 ../regent.py ../examples/stencil_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 1
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 1
 # mv stencil stencil.spmd1
-# SAVEOBJ=1 ../regent.py ../examples/stencil_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 10
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 10
 # mv stencil stencil.spmd10
-# SAVEOBJ=1 ../regent.py ../examples/stencil_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 12
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 12
 # mv stencil stencil.spmd12
-SAVEOBJ=1 ../regent.py ../examples/stencil_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 14
-mv stencil stencil.spmd14
-# SAVEOBJ=1 ../regent.py ../examples/stencil_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 16
+SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 10
+mv stencil stencil.spmd10
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 16
 # mv stencil stencil.spmd16
 
-cp ../../bindings/terra/liblegion_terra.so .
-cp ../examples/libstencil.so .
-cp ../examples/libstencil_mapper.so .
+cp $root_dir/../../bindings/terra/liblegion_terra.so .
+cp $root_dir/../examples/libstencil.so .
+cp $root_dir/../examples/libstencil_mapper.so .
 
-cp ../scripts/run_stencil.sh .
+cp $root_dir/../scripts/*_stencil.sh .
