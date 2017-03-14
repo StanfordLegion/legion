942e50d18f0d695b4538916e22a6767f3c0c077d
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-03-14 04:18:09 +0100

    regent: Update build scripts.

--- language/scripts/build_circuit.sh
+++ language/scripts/build_circuit.sh
@@ -2,26 +2,28 @@
 
 set -e
 
+root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
 mkdir "$1"
 cd "$1"
 
-SAVEOBJ=1 ../regent.py ../examples/circuit.rg -fflow 0
+SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 0
 mv circuit circuit.none
 
-# SAVEOBJ=1 ../regent.py ../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 1
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 1
 # mv circuit circuit.spmd1
-# SAVEOBJ=1 ../regent.py ../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 10
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 10
 # mv circuit circuit.spmd10
-SAVEOBJ=1 ../regent.py ../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 14
-mv circuit circuit.spmd14
-# SAVEOBJ=1 ../regent.py ../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 12
+SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 10
+mv circuit circuit.spmd10
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 12
 # mv circuit circuit.spmd12
-# SAVEOBJ=1 ../regent.py ../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 14
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 14
 # mv circuit circuit.spmd14
-# SAVEOBJ=1 ../regent.py ../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 16
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 16
 # mv circuit circuit.spmd16
 
-cp ../../bindings/terra/liblegion_terra.so .
-cp ../examples/libcircuit.so .
+cp $root_dir/../../bindings/terra/liblegion_terra.so .
+cp $root_dir/../examples/libcircuit.so .
 
-cp ../scripts/*_circuit.sh .
+cp $root_dir/../scripts/*_circuit.sh .
--- language/scripts/build_pennant.sh
+++ language/scripts/build_pennant.sh
@@ -2,18 +2,22 @@
 
 set -e
 
+root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
 mkdir "$1"
 cd "$1"
 
-SAVEOBJ=1 ../regent.py ../examples/pennant_fast.rg -fflow 0
+SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/pennant_fast.rg -fflow 0
 mv ./pennant pennant.none
 
-SAVEOBJ=1 ../regent.py ../examples/pennant_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 14
-mv ./pennant pennant.spmd14
+# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/pennant_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 4
+# mv ./pennant pennant.spmd4
+SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/pennant_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 10
+mv ./pennant pennant.spmd10
 
-cp ../../bindings/terra/liblegion_terra.so .
-cp ../examples/libpennant.so .
+cp $root_dir/../../bindings/terra/liblegion_terra.so .
+cp $root_dir/../examples/libpennant.so .
 
-cp -r ../examples/pennant.tests .
+cp -r $root_dir/../examples/pennant.tests .
 
-cp ../scripts/*_pennant.sh .
+cp $root_dir/../scripts/*_pennant.sh .
