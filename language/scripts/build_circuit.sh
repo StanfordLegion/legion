59603faa16c371023181781c0842488683e6ddae
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-03-20 15:40:53 -0700

    regent: Update build scripts.

--- language/scripts/build_circuit.sh
+++ language/scripts/build_circuit.sh
@@ -10,18 +10,10 @@ cd "$1"
 SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 0
 mv circuit circuit.none
 
-# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 1
-# mv circuit circuit.spmd1
-# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 10
-# mv circuit circuit.spmd10
-SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 10
-mv circuit circuit.spmd10
-# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 12
-# mv circuit circuit.spmd12
-# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 14
-# mv circuit circuit.spmd14
-# SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 16
-# mv circuit circuit.spmd16
+for c in 10; do
+    SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/circuit.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize "$c"
+    mv circuit circuit.spmd"$c"
+done
 
 cp $root_dir/../../bindings/terra/liblegion_terra.so .
 cp $root_dir/../examples/libcircuit.so .
