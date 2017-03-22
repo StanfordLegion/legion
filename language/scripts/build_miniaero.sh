7d987e2140026663511cf95173cbb2fe865de0ff
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-03-21 17:25:01 -0700

    regent: Update build scripts.

--- language/scripts/build_miniaero.sh
+++ language/scripts/build_miniaero.sh
@@ -2,26 +2,21 @@
 
 set -e
 
+root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
 mkdir "$1"
 cd "$1"
 
-# CC_FLAGS='-DDEFER_ARRIVALS_LOCALLY' ./setup_env.py
+SAVEOBJ=1 $root_dir/../regent.py $root_dir/../miniaero/rdir_1ghost.rg -fflow 0
+mv $root_dir/../miniaero/miniaero_rdir_1ghost miniaero.none
 
-# DETAILED_MESSAGE_TIMING
+for c in 8; do
+    SAVEOBJ=1 $root_dir/../regent.py $root_dir/../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize "$c"
+    mv $root_dir/../miniaero/miniaero_rdir_1ghost miniaero.spmd"$c"
+done
 
-SAVEOBJ=1 ../regent.py ../miniaero/rdir_1ghost.rg -fflow 0
-mv ../miniaero/miniaero_rdir_1ghost miniaero.none
+cp $root_dir/../../bindings/terra/liblegion_terra.so .
+cp $root_dir/../miniaero/librdir_1ghost.so .
 
-SAVEOBJ=1 ../regent.py ../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 1
-mv ../miniaero/miniaero_rdir_1ghost miniaero.spmd1
-SAVEOBJ=1 ../regent.py ../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 4
-mv ../miniaero/miniaero_rdir_1ghost miniaero.spmd4
-SAVEOBJ=1 ../regent.py ../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 8
-mv ../miniaero/miniaero_rdir_1ghost miniaero.spmd8
-SAVEOBJ=1 ../regent.py ../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 16
-mv ../miniaero/miniaero_rdir_1ghost miniaero.spmd16
-
-cp ../../bindings/terra/liblegion_terra.so .
-cp ../miniaero/librdir_1ghost.so .
-
-cp ../scripts/*_miniaero.sh .
+cp $root_dir/../scripts/*_miniaero.sh .
+cp $root_dir/../scripts/summarize.py .
--- language/scripts/build_pennant.sh
+++ language/scripts/build_pennant.sh
@@ -2,18 +2,23 @@
 
 set -e
 
+root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
 mkdir "$1"
 cd "$1"
 
-SAVEOBJ=1 ../regent.py ../examples/pennant_fast.rg -fflow 0
+SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/pennant_fast_vectorized.rg -fflow 0 -fvectorize-unsafe 1
 mv ./pennant pennant.none
 
-SAVEOBJ=1 ../regent.py ../examples/pennant_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize 12
-mv ./pennant pennant.spmd12
+for c in 10; do
+    SAVEOBJ=1 $root_dir/../regent.py $root_dir/../examples/pennant_fast_vectorized.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize "$c" -fvectorize-unsafe 1
+    mv ./pennant pennant.spmd"$c"
+done
 
-cp ../../bindings/terra/liblegion_terra.so .
-cp ../examples/libpennant.so .
+cp $root_dir/../../bindings/terra/liblegion_terra.so .
+cp $root_dir/../examples/libpennant.so .
 
-cp -r ../examples/pennant.tests .
+cp -r $root_dir/../examples/pennant.tests .
 
-cp ../scripts/*_pennant.sh .
+cp $root_dir/../scripts/*_pennant.sh .
+cp $root_dir/../scripts/summarize.py .
