8cc3b097b01813cb9d0c4d64c1570ba8f38f230c
Mike <mebauer@cs.stanford.edu>
2017-03-29 17:39:42 -0700

    legion: more dumbness on my part

--- runtime/legion/runtime.h
+++ runtime/legion/runtime.h
@@ -446,7 +446,7 @@ namespace Legion {
       void perform_rank_exchange(void);
       void handle_mpi_rank_exchange(Deserializer &derez);
     protected:
-      void send_stages(int start_stage);
+      bool send_stages(int start_stage);
       bool unpack_exchange(int stage, Deserializer &derez);
     public:
       Runtime *const runtime;
