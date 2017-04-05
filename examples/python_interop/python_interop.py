118c75f2481c2da2213b8532c38dd76b257d91a0
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-04-05 14:21:33 -0700

    examples: Update Python interop.

--- examples/python_interop/python_interop.py
+++ examples/python_interop/python_interop.py
@@ -32,10 +32,11 @@ int dlclose(void *handle);
 """)
 c = ffi.dlopen(None)
 
-handle = c.dlopen(ffi.NULL, 0)
+c.dlerror() # Clear any previous error
+handle = c.dlopen(ffi.NULL, ffi.RTLD_NOW)
+print("handle", handle)
 if not handle:
-    print(c.dlerror())
-print(handle)
+    print("error", ffi.string(c.dlerror()))
 
 ffi.cdef(r"""
   typedef unsigned long long legion_lowlevel_id_t;
@@ -52,12 +53,17 @@ ffi.cdef(r"""
                                          legion_context_t ctx);
 """)
 
+c.dlerror()
+legion_runtime_get_executing_processor = c.dlsym(handle, "legion_runtime_get_executing_processor")
+print("dlsym returned", legion_runtime_get_executing_processor)
+if not legion_runtime_get_executing_processor:
+    print("error", ffi.string(c.dlerror()))
+
 legion_runtime_get_executing_processor = ffi.cast(
     "legion_processor_t (*)(legion_runtime_t, legion_context_t)",
-    c.dlsym(handle, "legion_runtime_get_executing_processor"))
+    legion_runtime_get_executing_processor)
 
 def main_task(args, user_data, proc):
     print("hello from task1 args=({!r}) userdata=({!r}) proc=({:x})".format(
         args, user_data, proc))
-    print(legion_runtime_get_executing_processor)
     legion_runtime_get_executing_processor()
