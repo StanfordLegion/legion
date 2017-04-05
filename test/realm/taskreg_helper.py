e940a2b32f15844255acdf1dce7671311899116b
Sean Treichler <sjt@cs.stanford.edu>
2017-04-05 00:27:12 -0700

    test: test python task registration if available

---
+++ test/realm/taskreg_helper.py
@@ -0,0 +1,6 @@
+# task definitions for taskreg.cc
+
+def task1(args, user_data, proc):
+    print "hello from task1 args=({!r}) userdata=({!r}) proc=({:x})".format(args,
+                                                                      user_data,
+                                                                      proc)
