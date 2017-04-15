c499e92cc297069146f68f30125c58e1a3b9fc58
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-04-14 18:07:12 -0700

    python: Rewrite the Python task wrapper to register tasks automatically.

--- bindings/python/legion.py
+++ bindings/python/legion.py
@@ -32,25 +32,74 @@ ffi = cffi.FFI()
 ffi.cdef(header)
 c = ffi.dlopen(None)
 
+_local_tasks = {}
+class Task (object):
+    __slots__ = ['body', 'wrapper']
+
+    def __init__(self, body):
+        self.body = body
+        self.wrapper = Task.make_wrapper(body)
+
+    @staticmethod
+    def make_wrapper(body):
+        def wrapper(args, user_data, proc):
+            arg_ptr = ffi.new("char[]", bytes(args))
+            arg_size = len(args)
+
+            task = ffi.new("legion_task_t *")
+            raw_regions = ffi.new("legion_physical_region_t **")
+            num_regions = ffi.new("unsigned *")
+            context = ffi.new("legion_context_t *")
+            runtime = ffi.new("legion_runtime_t *")
+            c.legion_task_preamble(
+                arg_ptr, arg_size, proc,
+                task, raw_regions, num_regions, context, runtime)
+
+            regions = []
+            for i in xrange(num_regions[0]):
+                regions.append(raw_regions[0][i])
+
+            value = body(task[0], regions, context[0], runtime[0])
+            assert(value is None) # FIXME: Support return values
+
+            c.legion_task_postamble(runtime[0], context[0], ffi.NULL, 0)
+        return wrapper
+
+    def register(self):
+        assert(self not in _local_tasks)
+
+        execution_constraints = c.legion_execution_constraint_set_create()
+        c.legion_execution_constraint_set_add_processor_constraint(
+            execution_constraints, c.PY_PROC)
+
+        layout_constraints = c.legion_task_layout_constraint_set_create()
+        # FIXME: Add layout constraints
+
+        options = ffi.new("legion_task_config_options_t *")
+        options[0].leaf = False
+        options[0].inner = False
+        options[0].idempotent = False
+
+        task_id = c.legion_runtime_register_task_variant_python_source(
+            c.legion_runtime_get_runtime(),
+            ffi.cast("legion_task_id_t", -1), # AUTO_GENERATE_ID
+            '%s.%s' % (self.body.__module__, self.body.__name__),
+            False, # global
+            execution_constraints,
+            layout_constraints,
+            options[0],
+            self.body.__module__,
+            self.body.__name__,
+            ffi.NULL,
+            0)
+
+        c.legion_execution_constraint_set_destroy(execution_constraints)
+        c.legion_task_layout_constraint_set_destroy(layout_constraints)
+
+        _local_tasks[self] = task_id
+        return task_id
+
 def task(body):
-    def wrapper(args, user_data, proc):
-        arg_ptr = ffi.new("char[]", bytes(args))
-        arg_size = len(args)
-
-        task = ffi.new("legion_task_t *")
-        raw_regions = ffi.new("legion_physical_region_t **")
-        num_regions = ffi.new("unsigned *")
-        context = ffi.new("legion_context_t *")
-        runtime = ffi.new("legion_runtime_t *")
-        c.legion_task_preamble(arg_ptr, arg_size, proc,
-                               task, raw_regions, num_regions, context, runtime)
-
-        regions = []
-        for i in xrange(num_regions[0]):
-            regions.append(raw_regions[0][i])
-
-        value = body(task[0], regions, context[0], runtime[0])
-        assert(value is None) # FIXME: Support return values
-
-        c.legion_task_postamble(runtime[0], context[0], ffi.NULL, 0)
-    return wrapper
+    t = Task(body)
+    t.register()
+    return t.wrapper
