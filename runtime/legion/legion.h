7d82c2ed6ad9d75499cc0f435afe17ff6e6c2c0f
Mike <mebauer@cs.stanford.edu>
2017-05-11 00:08:36 -0700

    legion: adding an operator access for future maps

--- runtime/legion/legion.h
+++ runtime/legion/legion.h
@@ -1059,10 +1059,12 @@ namespace Legion {
       FRIEND_ALL_RUNTIME_CLASSES
       explicit FutureMap(Internal::FutureMapImpl *impl);
     public:
-      bool operator==(const FutureMap &f) const
+      inline bool operator==(const FutureMap &f) const
         { return impl == f.impl; }
-      bool operator<(const FutureMap &f) const
+      inline bool operator<(const FutureMap &f) const
         { return impl < f.impl; }
+      inline Future operator[](const DomainPoint &point)
+        { return get_future(point); }
       FutureMap& operator=(const FutureMap &f);
     public:
       /**
