f13a7d902bc9b8f3398d9845fecbce8247f10421
Sean Treichler <sjt@cs.stanford.edu>
2017-06-23 17:33:19 -0700

    dma: another empty-copy bug fix

--- runtime/realm/transfer/transfer.cc
+++ runtime/realm/transfer/transfer.cc
@@ -1012,7 +1012,7 @@ namespace Realm {
   template <int N, typename T>
   Event TransferIteratorZIndexSpace<N,T>::request_metadata(void)
   {
-    if(!inst_impl->metadata.is_valid())
+    if(inst_impl && !inst_impl->metadata.is_valid())
       return inst_impl->request_metadata();
 
     return Event::NO_EVENT;
