75551362904c56147710b3ef4d5cdcaa934f396c
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-08-03 10:12:10 -0700

    Revert "legion: Add calls to ensure colors to the C API"
    This reverts commit 6cb7678984c9addf25ac3a1356f2cf9327e6e208.

--- runtime/legion/legion_c.h
+++ runtime/legion/legion_c.h
@@ -660,14 +660,6 @@ extern "C" {
    * @see Legion::DomainPointColoring
    */
   void
-  legion_domain_point_coloring_ensure_color(
-    legion_domain_point_coloring_t handle,
-    legion_domain_point_t color);
-
-  /**
-   * @see Legion::DomainPointColoring
-   */
-  void
   legion_domain_point_coloring_color_domain(
     legion_domain_point_coloring_t handle,
     legion_domain_point_t color,
@@ -693,14 +685,6 @@ extern "C" {
   void
   legion_multi_domain_point_coloring_destroy(
     legion_multi_domain_point_coloring_t handle);
-
-  /**
-   * @see Legion::MultiDomainPointColoring
-   */
-  void
-  legion_multi_domain_point_coloring_ensure_color(
-    legion_multi_domain_point_coloring_t handle,
-    legion_domain_point_t color);
 
   /**
    * @see Legion::MultiDomainPointColoring
