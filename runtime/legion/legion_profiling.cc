2b159307861b92bd77cfa809982367338e7071b3
Wonchan Lee <wonchan@cs.stanford.edu>
2017-09-05 20:55:11 -0700

    tools: Track fill operations correctly in Legion Prof

--- runtime/legion/legion_profiling.cc
+++ runtime/legion/legion_profiling.cc
@@ -745,9 +745,7 @@ namespace Legion {
                                           Operation *op)
     //--------------------------------------------------------------------------
     {
-      // wonchan: don't track fill operations for the moment
-      // as their requests and responses do not exactly match
-      //increment_total_outstanding_requests();
+      increment_total_outstanding_requests();
       ProfilingInfo info(LEGION_PROF_FILL);
       // No ID here
       info.op_id = (op != NULL) ? op->get_unique_op_id() : 0;
@@ -839,9 +837,7 @@ namespace Legion {
                                           UniqueID uid)
     //--------------------------------------------------------------------------
     {
-      // wonchan: don't track fill operations for the moment
-      // as their requests and responses do not exactly match
-      //increment_total_outstanding_requests();
+      increment_total_outstanding_requests();
       ProfilingInfo info(LEGION_PROF_FILL);
       // No ID here
       info.op_id = uid;
@@ -1022,9 +1018,7 @@ namespace Legion {
               delete timeline;
             if (usage != NULL)
               delete usage;
-            // wonchan: don't track fill operations for the moment
-            // as their requests and responses do not exactly match
-            //decrement_total_outstanding_requests();
+            decrement_total_outstanding_requests();
             break;
           }
         case LEGION_PROF_INST:
