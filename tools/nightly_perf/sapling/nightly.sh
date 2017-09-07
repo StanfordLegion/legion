3b631fffed90ba34e3201e681662dae7bf0212b9
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-09-07 14:38:55 -0700

    perf: Update nightly script.

--- tools/nightly_perf/sapling/nightly.sh
+++ tools/nightly_perf/sapling/nightly.sh
@@ -4,16 +4,16 @@ set -e
 
 source env.sh
 
-# Configure test
+# Configure perf test
 export CC_FLAGS="-std=c++98"
 export DEBUG=0
-export LAUNCHER="mpirun -H n0002 -n 1 -npernode 1 -bind-to none -x TERRA_PATH -x INCLUDE_PATH -x LD_LIBRARY_PATH -x LG_RT_DIR -x USE_RDIR"
 export CI_RUNNER_DESCRIPTION="n0002.sapling.stanford.edu"
 
 # Run perf test for each branch
 for branch in master; do
     if [[ -d _legion_$branch ]]; then
         git -C _legion_$branch pull --ff-only
+        git -C _legion_$branch reset --hard HEAD
         git -C _legion_$branch clean -fdx
     else
         git clone -b $branch git@github.com:StanfordLegion/legion.git _legion_$branch
