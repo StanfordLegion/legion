56a9c25a9430a9971f152299cc1ef260af8f39d1
Alan Heirich <aheirich@titan-ext1.ccs.ornl.gov>
2017-09-17 02:13:55 -0400

    fixes to nightly.sh in common and titan minor refactor

--- tools/nightly_perf/titan/nightly.sh
+++ tools/nightly_perf/titan/nightly.sh
@@ -5,7 +5,13 @@
 root_dir="$(dirname "${BASH_SOURCE[0]}")"
 cd "${root_dir}"
 
-export TERRA_DIR="$root_dir"/../../../language/terra
+if [[ "${LG_RT_DIR}" == "" ]]
+then
+  echo LG_RT_DIR is not defined
+  exit -1
+fi
+
+export TERRA_DIR="${LG_RT_DIR}/../language/terra"
 
 source titan_setup.bash
 #source env.bash  # defines PERF_ACCESS_TOKEN
