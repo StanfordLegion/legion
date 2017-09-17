90db7569c4e2bb7909e77624c5a94c985d3c633f
Alan Heirich <aheirich@titan-ext1.ccs.ornl.gov>
2017-09-17 01:18:46 -0400

    define TERRA_DIR

--- tools/nightly_perf/titan/nightly.sh
+++ tools/nightly_perf/titan/nightly.sh
@@ -5,6 +5,8 @@
 root_dir="$(dirname "${BASH_SOURCE[0]}")"
 cd "${root_dir}"
 
+export TERRA_DIR="$root_dir"/../../../language/terra
+
 source titan_setup.bash
 #source env.bash  # defines PERF_ACCESS_TOKEN
 
