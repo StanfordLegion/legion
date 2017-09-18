f536980d5a8e601906e46b85c5635ab1a63cb6a8
Alan Heirich <aheirich@titan-ext7.ccs.ornl.gov>
2017-09-18 18:55:53 -0400

    define TERRA_DIR

--- tools/nightly_perf/titan/titan_setup.bash
+++ tools/nightly_perf/titan/titan_setup.bash
@@ -9,10 +9,12 @@ fi
 export GASNET=${LG_RT_DIR}/../language/gasnet/release
 export LLVM_CONFIG=${LG_RT_DIR}/../language/llvm/install/bin/llvm-config
 export CLANG=${LG_RT_DIR}/../language/llvm/install/bin/clang
-# -I llvm/install/include
+export TERRA_DIR=${LG_RT_DIT}/../language/terra
+
 module unload PrgEnv-pgi
 module load PrgEnv-gnu
 module load python
+
 export CC=gcc
 export CXX=CC
 export HOST_CC=gcc
