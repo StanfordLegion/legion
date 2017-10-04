7e20ff48e8c4d481741bc611472baa1b964ae6a9
Sean Treichler <sjt@cs.stanford.edu>
2017-10-04 06:09:59 -0700

    test: include legion.h before USE_HDF test for cmake build support

--- examples/attach_file/attach_file.cc
+++ examples/attach_file/attach_file.cc
@@ -20,10 +20,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include "legion.h"
 #ifdef USE_HDF
 #include <hdf5.h>
 #endif
-#include "legion.h"
 using namespace Legion;
 
 /*
