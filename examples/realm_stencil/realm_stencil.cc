7ec55e180bb9099f8410af8e3893be6db5be65a6
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-10-11 14:38:01 -0700

    examples: Initial skeleton of Stencil with arg parsing.

---
+++ examples/realm_stencil/realm_stencil.cc
@@ -0,0 +1,118 @@
+/* Copyright 2017 Stanford University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "realm_stencil.h"
+
+#include "realm.h"
+
+using namespace Realm;
+
+enum {
+  TOP_LEVEL_TASK = Processor::TASK_ID_FIRST_AVAILABLE+0,
+};
+
+struct AppConfig {
+public:
+  AppConfig()
+    : nx(12), ny(12), ntx(4), nty(4), tsteps(20), tprune(0), init(1000) {}
+public:
+  size_t nx;
+  size_t ny;
+  size_t ntx;
+  size_t nty;
+  size_t tsteps;
+  size_t tprune;
+  size_t init;
+};
+
+void get_optional_arg(int argc, char **argv,
+                      const char *flag, size_t &out)
+{
+  for (int i = 0; i < argc; i++) {
+    if (strcmp(argv[i], flag) == 0) {
+      if (i+1 < argc) {
+        out = atoll(argv[i+1]);
+      }
+      return;
+    } else if (argv[i][0] == '-') {
+      i++;
+    }
+  }
+}
+
+AppConfig parse_config(int argc, char **argv)
+{
+  AppConfig config;
+  get_optional_arg(argc, argv, "-nx", config.nx);
+  get_optional_arg(argc, argv, "-ny", config.ny);
+  get_optional_arg(argc, argv, "-ntx", config.ntx);
+  get_optional_arg(argc, argv, "-nty", config.nty);
+  get_optional_arg(argc, argv, "-tsteps", config.tsteps);
+  get_optional_arg(argc, argv, "-tprune", config.tprune);
+  get_optional_arg(argc, argv, "-init", config.init);
+  return config;
+}
+
+void top_level_task(const void *args, size_t arglen,
+                    const void *userdata, size_t userlen, Processor p)
+{
+  assert(arglen == sizeof(AppConfig));
+  const AppConfig &config = *reinterpret_cast<const AppConfig *>(args);
+  printf("Stencil configuration:\n");
+  printf("      nx %lu\n", config.nx);
+  printf("      ny %lu\n", config.ny);
+  printf("     ntx %lu\n", config.ntx);
+  printf("     nty %lu\n", config.nty);
+  printf("  tsteps %lu\n", config.tsteps);
+  printf("  tprune %lu\n", config.tprune);
+  printf("    init %lu\n", config.init);
+}
+
+int main(int argc, char **argv)
+{
+  Runtime rt;
+
+  rt.init(&argc, &argv);
+
+  rt.register_task(TOP_LEVEL_TASK, top_level_task);
+
+  // select a processor to run the top level task on
+  Processor p = Processor::NO_PROC;
+  {
+    std::set<Processor> all_procs;
+    Machine::get_machine().get_all_processors(all_procs);
+    for(std::set<Processor>::const_iterator it = all_procs.begin();
+	it != all_procs.end();
+	it++)
+      if(it->kind() == Processor::LOC_PROC) {
+	p = *it;
+	break;
+      }
+  }
+  assert(p.exists());
+
+  AppConfig config = parse_config(argc, argv);
+
+  // collective launch of a single task - everybody gets the same finish event
+  Event e = rt.collective_spawn(p, TOP_LEVEL_TASK, &config, sizeof(config));
+
+  // request shutdown once that task is complete
+  rt.shutdown(e);
+
+  // now sleep this thread until that shutdown actually happens
+  rt.wait_for_shutdown();
+
+  return 0;
+}
