164a87a51252c425b14019ad1c3f6e6dc80b227e
Mike <mebauer@cs.stanford.edu>
2017-12-19 15:51:37 -0800

    examples: updating some examples to use dynamic control replication for multi-node runs

--- examples/spmd_cgsolver/cgsolver.cc
+++ examples/spmd_cgsolver/cgsolver.cc
@@ -1327,6 +1327,8 @@ int main(int argc, char **argv)
   {
     TaskVariantRegistrar tvr(TOP_LEVEL_TASK_ID, "top_level_task");
     tvr.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
+    tvr.set_idempotent();
+    tvr.set_replicable();
     Runtime::preregister_task_variant<top_level_task>(tvr, "top_level_task");
     Runtime::set_top_level_task_id(TOP_LEVEL_TASK_ID);
   }
