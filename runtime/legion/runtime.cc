324f74716f796f70cc2c086c1aa98f640d525f6f
Mike <mebauer@cs.stanford.edu>
2017-12-21 17:18:10 -0700

    legion: small fix separate runtimes

--- runtime/legion/runtime.cc
+++ runtime/legion/runtime.cc
@@ -12605,7 +12605,19 @@ namespace Legion {
     {
       // First forward the message onto any remote nodes
       int base = index * radix;
-      int init = source.address_space();
+      int init;
+      if (Runtime::separate_runtime_instances)
+      {
+        std::map<Processor,Runtime*>::const_iterator finder = 
+          runtime_map->find(source);
+#ifdef DEBUG_LEGION
+        // only works with a single process
+        assert(finder != runtime_map->end()); 
+#endif
+        init = finder->second->address_space;
+      }
+      else
+        init = source.address_space();
       // The runtime stride is the same as the number of nodes
       const int total_nodes = runtime_stride;
       for (int r = 1; r <= radix; r++)
