028c1564b40aa9125f47fd752dc9a815f6a90a98
Mike <mebauer@cs.stanford.edu>
2018-01-12 12:55:22 -0800

    legion: some tweaks for chasing down a shutdown bug

--- runtime/legion/runtime.cc
+++ runtime/legion/runtime.cc
@@ -15414,14 +15414,6 @@ namespace Legion {
     void Runtime::free_distributed_id(DistributedID did)
     //--------------------------------------------------------------------------
     {
-      // Special case for did 0 on shutdown
-      if (did == 0)
-        return;
-      did &= LEGION_DISTRIBUTED_ID_MASK;
-#ifdef DEBUG_LEGION
-      // Should only be getting back our own DIDs
-      assert(determine_owner(did) == address_space);
-#endif
       // Don't recycle distributed IDs if we're doing LegionSpy or LegionGC
 #ifndef LEGION_GC
 #ifndef LEGION_SPY
@@ -15440,12 +15432,20 @@ namespace Legion {
                                             RtEvent recycle_event)
     //--------------------------------------------------------------------------
     {
+      // Special case for did 0 on shutdown
+      if (did == 0)
+        return RtEvent::NO_RT_EVENT;
+      did &= LEGION_DISTRIBUTED_ID_MASK;
+#ifdef DEBUG_LEGION
+      // Should only be getting back our own DIDs
+      assert(determine_owner(did) == address_space);
+#endif
       if (!recycle_event.has_triggered())
       {
         DeferredRecycleArgs deferred_recycle_args;
         deferred_recycle_args.did = did;
         return issue_runtime_meta_task(deferred_recycle_args, 
-            LG_RESOURCE_PRIORITY, NULL, recycle_event);
+                LG_THROUGHPUT_PRIORITY, NULL, recycle_event);
       }
       else
       {
