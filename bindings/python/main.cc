36de1db54487ac141d21b7b5f93e6d53a9f0e1be
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-02-22 11:25:12 -0800

    python: Update Python main.

--- bindings/python/main.cc
+++ bindings/python/main.cc
@@ -40,6 +40,19 @@ VariantID preregister_python_task_variant(
     registrar.task_variant_name);
 }
 
+void preregister_python_initialization_function(
+    const char *module_name,
+    const char *function_name,
+    const void *userdata = NULL,
+    size_t userlen = 0)
+{
+  CodeDescriptor code_desc(Realm::Type::from_cpp_type<Processor::TaskFuncPtr>());
+  code_desc.add_implementation(new Realm::PythonSourceImplementation(module_name, function_name));
+
+  return Runtime::preregister_initialization_function(
+      Processor::PY_PROC, code_desc, userdata, userlen);
+}
+
 int main(int argc, char **argv)
 {
   // Add the binary directory to PYTHONPATH. This is needed for
@@ -90,6 +103,8 @@ int main(int argc, char **argv)
   }
 
   Runtime::set_top_level_task_id(MAIN_TASK_ID);
+
+  preregister_python_initialization_function("legion", "initialize");
 
   return Runtime::start(argc, argv);
 }
