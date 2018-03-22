eac77756c79f9672278e6ec34faefe51c2dc4429
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-03-22 15:48:02 -0700

    regent: Update build core counts for Titan.

--- language/scripts/titan/build_circuit.sh
+++ language/scripts/titan/build_circuit.sh
@@ -7,7 +7,7 @@ root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 mkdir "$1"
 cd "$1"
 
-for c in 9; do
+for c in 8; do
     SAVEOBJ=1 $root_dir/../../regent.py $root_dir/../../examples/circuit_sparse.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize "$c" -fopenmp 0
     mv circuit circuit.spmd"$c"
 done
