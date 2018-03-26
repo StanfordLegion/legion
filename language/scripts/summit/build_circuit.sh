7f98f1dd1090243d295e9770cc58e620c6ec8262
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-03-26 14:03:46 -0700

    regent: Update scripts for Summit.

--- language/scripts/summit/build_circuit.sh
+++ language/scripts/summit/build_circuit.sh
@@ -8,9 +8,10 @@ mkdir "$1"
 cd "$1"
 
 for c in 18; do
-    SAVEOBJ=1 STANDALONE=1 OBJNAME=./circuit.spmd"$c" $root_dir/../regent.py $root_dir/../examples/circuit_sparse.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize "$c" -fopenmp 0
+    SAVEOBJ=1 STANDALONE=1 OBJNAME=./circuit.spmd"$c" $root_dir/../../regent.py $root_dir/../../examples/circuit_sparse.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize "$c" -fopenmp 0
 done
 
-cp $root_dir/../scripts/*_circuit*.sh .
-cp $root_dir/../scripts/summit_env.sh .
-cp $root_dir/../scripts/summarize.py .
+cp $root_dir/*_circuit*.sh .
+cp $root_dir/../summarize.py .
+
+cp $root_dir/env.sh .
--- language/scripts/summit/build_pennant.sh
+++ language/scripts/summit/build_pennant.sh
@@ -8,11 +8,12 @@ mkdir "$1"
 cd "$1"
 
 for c in 9; do
-    SAVEOBJ=1 STANDALONE=1 OBJNAME=./pennant.spmd"$c" $root_dir/../regent.py $root_dir/../examples/pennant_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize "$c" -fvectorize-unsafe 1 -fopenmp 0 -fcuda 0
+    SAVEOBJ=1 STANDALONE=1 OBJNAME=./pennant.spmd"$c" $root_dir/../../regent.py $root_dir/../../examples/pennant_fast.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize "$c" -fvectorize-unsafe 1 -fopenmp 0 -fcuda 0
 done
 
-cp -r $root_dir/../examples/pennant.tests .
+cp -r $root_dir/../../examples/pennant.tests .
 
-cp $root_dir/../scripts/*_pennant*.sh .
-cp $root_dir/../scripts/summit_env.sh .
-cp $root_dir/../scripts/summarize.py .
+cp $root_dir/*_pennant*.sh .
+cp $root_dir/../summarize.py .
+
+cp $root_dir/env.sh .
