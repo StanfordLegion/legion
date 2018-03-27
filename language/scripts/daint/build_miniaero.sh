892f8b8a205641d433bb238abaec9990656db53a
Wonchan Lee <wonchan@cs.stanford.edu>
2018-03-27 04:06:32 +0200

    Update scripts for MiniAero

--- language/scripts/daint/build_miniaero.sh
+++ language/scripts/daint/build_miniaero.sh
@@ -7,7 +7,7 @@ root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 mkdir "$1"
 cd "$1"
 
-for c in 8; do
+for c in 4 8; do
     SAVEOBJ=1 $root_dir/../../regent.py $root_dir/../../miniaero/rdir_1ghost.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize "$c" -fopenmp 0
     mv miniaero_rdir_1ghost miniaero.spmd"$c"
 done
--- language/scripts/daint/sbatch_miniaero_small.sh
+++ language/scripts/daint/sbatch_miniaero_small.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 #SBATCH --nodes=8
 #SBATCH --constraint=gpu
-#SBATCH --time=00:30:00
+#SBATCH --time=01:00:00
 #SBATCH --mail-type=ALL
 
 root_dir="$PWD"
 
 export LD_LIBRARY_PATH="$PWD"
+export NUM_UTIL=2
 
-if [[ ! -d tracing ]]; then mkdir tracing; fi
-pushd tracing
+if [[ ! -d tracing_util"$NUM_UTIL" ]]; then mkdir tracing_util"$NUM_UTIL"; fi
+pushd tracing_util"$NUM_UTIL"
 
 for n in 8 4 2 1; do
     for r in 0 1 2 3 4; do
         if [[ ! -f out_"$n"x8_r"$r".log ]]; then
             echo "Running $n""x8_r$r""..."
-
-	    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/miniaero.spmd8" -blocks $(( n * 8 )) -mesh 256x512x4 -x_length 2 -y_length 0.2 -z_length 1 -ramp 0 -dt 1e-8 -viscous -second_order -time_steps 100 -output_frequency 101 -ll:cpu 8 -ll:io 1 -ll:util 2 -ll:dma 2 -ll:csize 20480 -ll:rsize 1024 -ll:gsize 0 -dm:memoize | tee out_"$n"x8_r"$r".log
+            srun -n "$((n * 2))" -N "$n" -c 12 --ntasks-per-node 2 /lib64/ld-linux-x86-64.so.2 "$root_dir/miniaero.spmd4" -blocks "$(( n * 8 ))" -mesh 8x8x32768 -x_length 2 -y_length 0.2 -z_length 1 -ramp 0 -dt 1e-8 -viscous -second_order -time_steps 100 -output_frequency 101 -ll:cpu 4 -ll:io 1 -ll:dma 2 -ll:pin_dma -ll:util "$NUM_UTIL" -ll:ht_sharing 0 -ll:csize 30000 -ll:rsize 4096 -ll:gsize 0 -dm:memoize | tee out_"$n"x8_r"$r".log
             # -lg:prof 4 -lg:prof_logfile prof_"$n"x8_r"$r"_%.gz
         fi
     done
