a9bccde05599a535eae79efe287d53e02bdcf607
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-03-28 14:13:17 -0700

    Tweak run scripts.

--- language/scripts/daint/sbatch_circuit_notracing.sh
+++ language/scripts/daint/sbatch_circuit_notracing.sh
@@ -15,7 +15,7 @@ for n in 256 128 64 32 16; do
     for r in 0 1 2 3 4; do
         if [[ ! -f out_"$n"x9_r"$r".log ]]; then
             echo "Running $n""x9_r$r""..."
-            srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/circuit.spmd9" -npp 20 -wpp 80 -l 120 -prune 5 -p $(( 256 * 9 )) -pps $(( 256 / n )) -ll:csize 30000 -ll:rsize 512 -ll:gsize 0 -ll:cpu 9 -ll:io 1 -ll:util 2 -ll:dma 1 -lg:no_physical_tracing | tee out_"$n"x9_r"$r".log
+            srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/circuit.spmd9" -npp 20 -wpp 80 -l 120 -prune 8 -p $(( 256 * 9 )) -pps $(( 256 / n )) -ll:csize 30000 -ll:rsize 512 -ll:gsize 0 -ll:cpu 9 -ll:io 1 -ll:util 2 -ll:dma 1 -lg:no_physical_tracing | tee out_"$n"x9_r"$r".log
             # -lg:prof 4 -lg:prof_logfile prof_"$n"x9_r"$r"_%.gz
         fi
     done
