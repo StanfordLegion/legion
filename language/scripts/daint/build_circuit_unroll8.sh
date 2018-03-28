e0e6645912c9e229ae789bb61e3c6a74ec9a807a
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-03-28 14:10:04 -0700

    regent: Add script for building Circuit unroll 8.

---
+++ language/scripts/daint/build_circuit_unroll8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+mkdir "$1"
+cd "$1"
+
+for c in 9; do
+    SAVEOBJ=1 STANDALONE=1 OBJNAME=./circuit.spmd"$c" $root_dir/../../regent.py $root_dir/../../examples/circuit_sparse_unroll8.rg -fflow 1 -fflow-spmd 1 -fflow-spmd-shardsize "$c" -fopenmp 0
+done
+
+cp $root_dir/*_circuit*.sh .
+cp $root_dir/../summarize.py .
