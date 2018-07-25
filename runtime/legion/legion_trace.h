f7c9d2a75bd37b24e105821b28f0974bca8a0851
Wonchan Lee <wonchan@cs.stanford.edu>
2018-07-25 10:26:51 -0700

    legion: Re-implement transitive reduction and copy propagation on traces

--- runtime/legion/legion_trace.h
+++ runtime/legion/legion_trace.h
@@ -559,6 +559,8 @@ namespace Legion {
       void optimize(void);
       void elide_fences(std::vector<unsigned> &gen);
       void propagate_merges(std::vector<unsigned> &gen);
+      void transitive_reduction(void);
+      void propagate_copies(std::vector<unsigned> &gen);
       void prepare_parallel_replay(const std::vector<unsigned> &gen);
       void push_complete_replays();
       void generate_summary_operations(void);
