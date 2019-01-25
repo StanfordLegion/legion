1f6650a9ecd22dff3af09fab7c587592f3d4df16
Mike <mebauer@cs.stanford.edu>
2019-01-25 13:27:00 -0800

    legion: support for index fills in the C API

--- runtime/legion/legion_c.cc
+++ runtime/legion/legion_c.cc
@@ -3034,6 +3034,61 @@ legion_runtime_fill_field_future(
   runtime->fill_field(ctx, handle, parent, fid, *f, *pred);
 }
 
+void
+legion_runtime_index_fill_field(
+  legion_runtime_t runtime_,
+  legion_context_t ctx_,
+  legion_logical_partition_t handle_,
+  legion_logical_region_t parent_,
+  legion_field_id_t fid,
+  const void *value,
+  size_t value_size,
+  legion_projection_id_t proj /* = 0 */,
+  legion_predicate_t pred_ /* = legion_predicate_true() */,
+  legion_mapper_id_t id /* = 0 */,
+  legion_mapping_tag_id_t launcher_tag /* = 0 */)
+{
+  Runtime *runtime = CObjectWrapper::unwrap(runtime_);
+  Context ctx = CObjectWrapper::unwrap(ctx_)->context();
+  LogicalPartition handle = CObjectWrapper::unwrap(handle_);
+  LogicalRegion parent = CObjectWrapper::unwrap(parent_);
+  Predicate *pred = CObjectWrapper::unwrap(pred_);
+
+  IndexFillLauncher launcher(
+      runtime->get_index_partition_color_space_name(handle.get_index_partition()),
+      handle, parent, TaskArgument(value, value_size), proj,
+      *pred, id, launcher_tag);
+  launcher.add_field(fid);
+  runtime->fill_fields(ctx, launcher);
+}
+
+void
+legion_runtime_index_fill_field_future(
+  legion_runtime_t runtime_,
+  legion_context_t ctx_,
+  legion_logical_partition_t handle_,
+  legion_logical_region_t parent_,
+  legion_field_id_t fid,
+  legion_future_t f_,
+  legion_projection_id_t proj /* = 0 */,
+  legion_predicate_t pred_ /* = legion_predicate_true() */,
+  legion_mapper_id_t id /* = 0 */,
+  legion_mapping_tag_id_t launcher_tag /* = 0 */)
+{
+  Runtime *runtime = CObjectWrapper::unwrap(runtime_);
+  Context ctx = CObjectWrapper::unwrap(ctx_)->context();
+  LogicalPartition handle = CObjectWrapper::unwrap(handle_);
+  LogicalRegion parent = CObjectWrapper::unwrap(parent_);
+  Future *f = CObjectWrapper::unwrap(f_);
+  Predicate *pred = CObjectWrapper::unwrap(pred_);
+
+  IndexFillLauncher launcher(
+      runtime->get_index_partition_color_space_name(handle.get_index_partition()),
+      handle, parent, *f, proj, *pred, id, launcher_tag);
+  launcher.add_field(fid);
+  runtime->fill_fields(ctx, launcher);
+}
+
 // -----------------------------------------------------------------------
 // File Operations
 // -----------------------------------------------------------------------
