d15aa6f20851ca0a4131a6aa4eae0c17ca007b4e
Sean Treichler <sjt@cs.stanford.edu>
2019-04-02 12:36:30 -0700

    regent: fix typo to really disable zlib

--- language/scripts/setup_env.py
+++ language/scripts/setup_env.py
@@ -113,7 +113,7 @@ def build_llvm(source_dir, build_dir, install_dir, use_cmake, cmake_exe, thread_
             [cmake_exe,
              '-DCMAKE_INSTALL_PREFIX=%s' % install_dir,
              '-DCMAKE_BUILD_TYPE=Release',
-             '-DLLVM_ENABLE_ASSERTIONS=OFF'
+             '-DLLVM_ENABLE_ASSERTIONS=OFF',
              '-DLLVM_ENABLE_ZLIB=OFF',
              '-DLLVM_ENABLE_TERMINFO=OFF',
              '-DLLVM_ENABLE_LIBEDIT=OFF',
