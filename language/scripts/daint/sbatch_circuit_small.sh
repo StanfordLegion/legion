42575aba0b57d41fc60a363c57bd3a321c134a29
Wonchan Lee <wonchan@cs.stanford.edu>
2019-04-10 07:07:19 +0200

    scripts: Final build and job scripts

--- language/scripts/daint/sbatch_circuit_small.sh
+++ language/scripts/daint/sbatch_circuit_small.sh
@@ -13,7 +13,7 @@ pushd auto
 for n in 64 32 16; do
   for r in 0 1 2 3 4; do
     echo "Running $n""x1_r$r"
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/circuit.auto" -npp 2500 -wpp 10000 -l 50 -p $(( $n * 10 )) -pps 10 -prune 5 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:dma 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:same_address_space | tee out_"$n"x1_r"$r".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/circuit.auto" -npp 2500 -wpp 10000 -l 50 -p $(( $n * 10 )) -pps 10 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:dma 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:same_address_space | tee out_"$n"x1_r"$r".log
   done
 done
 
@@ -25,7 +25,7 @@ pushd manual
 for n in 64 32 16; do
   for r in 0 1 2 3 4; do
     echo "Running $n""x1_r$r"
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/circuit.manual"  -npp 2500 -wpp 10000 -l 50 -p $(( $n * 10 )) -pps 10 -prune 5 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:dma 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:same_address_space | tee out_"$n"x1_r"$r".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/circuit.manual"  -npp 2500 -wpp 10000 -l 50 -p $(( $n * 10 )) -pps 10 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:dma 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:same_address_space | tee out_"$n"x1_r"$r".log
   done
 done
 
