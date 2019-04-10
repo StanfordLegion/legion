0359b5fb3fbf96f8239e24ca08ee96265754fc0e
Wonchan Lee <wonchan@cs.stanford.edu>
2019-04-10 11:43:36 -0700

    results: Updates to artifacts

--- language/sc19_results/summarize_all.sh
+++ language/sc19_results/summarize_all.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-for dir in $(ls */ -d); do
-  APP=`echo "##### $dir #####" | sed s/"\/"/""/g`
+for app in $(ls */ -d); do
+  APP=`echo "##### $app #####" | sed s/"\/"/""/g`
   echo "$APP"
-  for run in $(ls $dir/); do
-    echo "  * $run"
-    ./summarize.sh "$dir/$run" 2> /dev/null | sed s/"^"/"    "/g
+  for run in $(ls $app*/ -d); do
+    RUN=`echo "$run" | cut -f2 -d'/'`
+    echo "  * $RUN"
+    PROBLEM_SIZE=`cat "$app"/problem_size`
+    ITERATIONS=`cat "$app"/iterations`
+    N=$(( PROBLEM_SIZE * ITERATIONS ))
+    ./summarize.sh "$run" 2> /dev/null | awk -v n="$N" '{ print n / $1 }' | sed s/"^"/"    "/g
   done
 done
