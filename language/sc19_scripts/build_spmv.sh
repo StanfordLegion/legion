a3169c8a60df8564c3120b5d27e6f58f1d1c3a67
Wonchan Lee <wonchan@cs.stanford.edu>
2019-05-26 10:16:56 +0200

    scripts: Update scripts for SpMV

--- language/sc19_scripts/build_spmv.sh
+++ language/sc19_scripts/build_spmv.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 mkdir "$1"
 cd "$1"
 
-if [[ ! -z "${HDF_ROOT:-}" ]]; then
-    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$HDF_ROOT/lib"
-fi
 SAVEOBJ=1 OBJNAME=./spmv.auto "$ROOT_DIR"/../regent.py "$ROOT_DIR"/../examples/spmv_sequential.rg -fflow 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal
+
+cp $ROOT_DIR/../../bindings/regent/libregent.so .
+cp $ROOT_DIR/*_spmv*.sh .
