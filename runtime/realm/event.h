428043cf12e32c121d8274b70bf8ce1357b8396a
Sean Treichler <sjt@cs.stanford.edu>
2019-06-20 11:31:25 -0700

    realm: initial version of CompletionQueue API

--- runtime/realm/event.h
+++ runtime/realm/event.h
@@ -138,6 +138,61 @@ namespace Realm {
       bool get_result(void *value, size_t value_size) const;
     };
 
+    // a CompletionQueue funnels the completion of unordered events into a
+    //  single stream that can be queried (and waited on) by a single servicer
+    class CompletionQueue {
+    public:
+      typedef ::realm_id_t id_t;
+
+      id_t id;
+      bool operator<(const CompletionQueue& rhs) const;
+      bool operator==(const CompletionQueue& rhs) const;
+      bool operator!=(const CompletionQueue& rhs) const;
+
+      static const CompletionQueue NO_QUEUE;
+
+      bool exists(void) const;
+
+      // creates a completion queue that can hold at least 'max_size'
+      //  triggered events (at the moment, overflow is a fatal error)
+      // a 'max_size' of 0 allows for arbitrary queue growth, at the cost
+      //  of additional overhead
+      static CompletionQueue create_completion_queue(size_t max_size);
+
+      // destroy a completion queue
+      void destroy(Event wait_on = Event::NO_EVENT);
+
+      // adds an event to the completion queue (once it triggers)
+      // non-faultaware version raises a fatal error if the specified 'event'
+      //  is poisoned
+      void add_event(Event event);
+      void add_event_faultaware(Event event);
+
+      // requests up to 'max_events' triggered events to be popped from the
+      //  queue and stored in the provided 'events' array (if null, the
+      //  identities of the triggered events are discarded)
+      // this call returns the actual number of events popped, which may be
+      //  zero (this call is nonblocking)
+      // when 'add_event_faultaware' is used, any poisoning of the returned
+      //  events is not signalled explicitly - the caller is expected to
+      //  check via 'has_triggered_faultaware' itself
+      size_t pop_events(Event *events, size_t max_events);
+
+      // get an event that, once triggered, guarantees that (at least) one
+      //  call to pop_events made since the non-empty event was requested
+      //  will return a non-zero number of triggered events
+      // once a call to pop_events has been made (by the caller of
+      //  get_nonempty_event or anybody else), the guarantee is lost and
+      //  a new non-empty event must be requested
+      // note that 'get_nonempty_event().has_triggered()' is unlikely to
+      //  ever return 'true' if called from a node other than the one that
+      //  created the completion queue (i.e. the query at least has the
+      //  round-trip network communication latency to deal with) - if polling
+      //  on the completion queue is unavoidable, the loop should poll on
+      //  pop_events directly
+      Event get_nonempty_event(void);
+    };
+    
 
 }; // namespace Realm
 
