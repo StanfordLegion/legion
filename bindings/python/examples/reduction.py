245813c095cbc5ccfec533e19853a8cfcd1cd594
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-06-27 16:52:19 -0700

    python: Support for reductions.

---
+++ bindings/python/examples/reduction.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# Copyright 2019 Stanford University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import print_function
+
+import legion
+from legion import task, Reduce
+import numpy
+
+@task(privileges=[Reduce('+', legion.float64)])
+def inc(R, step):
+    numpy.add(R.x, step, out=R.x)
+    numpy.add(R.y, step, out=R.y)
+
+@task
+def main():
+    R = legion.Region.create([4, 4], {'x': legion.float64, 'y': legion.float64})
+    legion.fill(R, 'x', 1)
+    legion.fill(R, 'y', 2)
+    inc(R, 20)
+    print(R.x)
+    print(R.y)
+    assert R.x[0, 0] == 21.0
+    assert R.y[0, 0] == 22.0
+
+if __name__ == '__legion_main__':
+    main()
