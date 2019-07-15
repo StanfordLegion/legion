6c270e21b4f0f12c7a91d609041d206e5eb3ac41
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-07-16 00:33:31 +0200

    Initial PAW scripts.

---
+++ language/paw19_scripts/build_pennant.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+mkdir "$1"
+cd "$1"
+
+SAVEOBJ=1 STANDALONE=1 OBJNAME=./pennant.normal $root_dir/../regent.py $root_dir/../examples/pennant_fast.rg -fflow 0 -fopenmp 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal
+
+SAVEOBJ=1 STANDALONE=0 OBJNAME=./pennant.python $root_dir/../regent.py $root_dir/../../apps/pennant/python/pennant_fast.rg -fflow 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal
+
+cp $root_dir/*_pennant*.sh .
+
+cp $root_dir/../../apps/pennant/python/*.py .
+cp $root_dir/../../apps/pennant/python/*.h .
