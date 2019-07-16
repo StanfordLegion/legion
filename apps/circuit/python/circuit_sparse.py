e7989cbe62abeab10deeed206510fcba61d84838
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-07-16 13:41:55 -0700

    python: Fix Python 2 compat in apps.

--- apps/circuit/python/circuit_sparse.py
+++ apps/circuit/python/circuit_sparse.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import argparse
 from collections import OrderedDict
--- apps/pennant/python/pennant_fast.py
+++ apps/pennant/python/pennant_fast.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from collections import OrderedDict
 import numpy as np
