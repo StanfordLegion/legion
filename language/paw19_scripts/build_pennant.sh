afe16003851db8fd2d77074a0f8aad1256fd951f
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-07-18 00:19:35 +0200

    Update build scripts.

--- language/paw19_scripts/build_pennant.sh
+++ language/paw19_scripts/build_pennant.sh
@@ -7,9 +7,9 @@ root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 mkdir "$1"
 cd "$1"
 
-SAVEOBJ=1 STANDALONE=1 OBJNAME=./pennant.normal $root_dir/../regent.py $root_dir/../examples/pennant_fast.rg -fflow 0 -fopenmp 0 -fcuda 0 # -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal
+SAVEOBJ=1 STANDALONE=1 OBJNAME=./pennant.normal $root_dir/../regent.py $root_dir/../examples/pennant.rg -fflow 0 -fopenmp 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal
 
-SAVEOBJ=1 STANDALONE=0 OBJNAME=./pennant.python $root_dir/../regent.py $root_dir/../../apps/pennant/python/pennant_fast.rg -fflow 0 -fcuda 0 # -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal
+SAVEOBJ=1 STANDALONE=0 OBJNAME=./pennant.python $root_dir/../regent.py $root_dir/../../apps/pennant/python/pennant_python.rg -fflow 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal
 
 cp $root_dir/*_pennant*.sh .
 
