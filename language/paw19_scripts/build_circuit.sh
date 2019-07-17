a4ff37f795941a872eabc48b1ea4293f75f51072
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-07-18 00:19:35 +0200

    Update build scripts.

--- language/paw19_scripts/build_circuit.sh
+++ language/paw19_scripts/build_circuit.sh
@@ -9,7 +9,7 @@ cd "$1"
 
 SAVEOBJ=1 STANDALONE=1 OBJNAME=./circuit.normal $root_dir/../regent.py $root_dir/../examples/circuit_sparse.rg -fflow 0 -fopenmp 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal
 
-SAVEOBJ=1 STANDALONE=1 OBJNAME=./circuit.python $root_dir/../regent.py $root_dir/../../apps/circuit/python/circuit_sparse.rg -fflow 0 -fopenmp 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal
+SAVEOBJ=1 STANDALONE=1 OBJNAME=./circuit.python $root_dir/../regent.py $root_dir/../../apps/circuit/python/circuit_sparse_python.rg -fflow 0 -fopenmp 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal
 
 cp $root_dir/*_circuit*.sh .
 
