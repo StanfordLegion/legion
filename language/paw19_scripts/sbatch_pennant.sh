86513ae4646355cab72d38855b844bcb0b8bbdd6
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-07-23 00:38:03 +0200

    Update pennant problem size in script.

--- language/paw19_scripts/sbatch_pennant.sh
+++ language/paw19_scripts/sbatch_pennant.sh
@@ -17,7 +17,7 @@ pushd normal
 for n in $SLURM_JOB_NUM_NODES; do
   for r in 0 1 2 3 4; do
     echo "Running $n""x1_r$r"
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/pennant.normal" "$root_dir"/pennant.tests/leblanc_long"$n"x30/leblanc.pnt -npieces "$n" -numpcx 1 -numpcy "$n" -seq_init 0 -par_init 1 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:dma 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 15000 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:same_address_space 1 | tee out_"$n"x1_r"$r".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/pennant.normal" "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x30/leblanc.pnt -npieces "$n" -numpcx 1 -numpcy "$n" -seq_init 0 -par_init 1 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:dma 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 15000 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:same_address_space 1 | tee out_"$n"x1_r"$r".log
   done
 done
 
@@ -32,7 +32,7 @@ export PYTHONPATH="$PYTHONPATH:$LG_RT_DIR/../bindings/python:$root_dir"
 for n in $SLURM_JOB_NUM_NODES; do
   for r in 0 1 2 3 4; do
     echo "Running $n""x1_r$r"
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/pennant.python" "$root_dir"/pennant.tests/leblanc_long"$n"x30/leblanc.pnt -npieces "$n" -numpcx 1 -numpcy "$n" -seq_init 0 -par_init 1 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:dma 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 15000 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:same_address_space 1 -ll:py 1 -ll:pyimport pennant | tee out_"$n"x1_r"$r".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/pennant.python" "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x30/leblanc.pnt -npieces "$n" -numpcx 1 -numpcy "$n" -seq_init 0 -par_init 1 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:dma 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 15000 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:same_address_space 1 -ll:py 1 -ll:pyimport pennant | tee out_"$n"x1_r"$r".log
   done
 done
 
