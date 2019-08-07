932d75c43c143d19190d6c356c49b295f97b5b17
Mike <mebauer@cs.stanford.edu>
2019-08-07 16:15:53 -0700

    bitmask: switch to cinttypes for c++ compatability

--- runtime/bitmask.h
+++ runtime/bitmask.h
@@ -946,11 +946,11 @@
 #define MASK_FMT "llx"
 #else
 // SJT: this comes first because some systems require __STDC_FORMAT_MACROS
-//  to be defined before inttypes.h is included anywhere
+//  to be defined before cinttypes.h is included anywhere
 #ifndef __STDC_FORMAT_MACROS
 #define __STDC_FORMAT_MACROS
 #endif
-#include <inttypes.h>
+#include <cinttypes.h>
 #define MASK_FMT PRIx64
 #endif
     //--------------------------------------------------------------------------
