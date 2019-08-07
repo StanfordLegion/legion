846231229734e63ac0c6cc3ec813ee1ae917643c
Wonchan Lee <wonchan@cs.stanford.edu>
2019-08-07 00:07:52 -0700

    Revert "legion: Remove an unused operation kind"
    This reverts commit 983b18b7c85ddf96461a0b2c81a364a66022b364.

--- runtime/legion/legion_ops.h
+++ runtime/legion/legion_ops.h
@@ -67,6 +67,7 @@ namespace Legion {
         TRACE_CAPTURE_OP_KIND,
         TRACE_COMPLETE_OP_KIND,
         TRACE_REPLAY_OP_KIND,
+        TRACE_BEGIN_OP_KIND,
         TRACE_SUMMARY_OP_KIND,
         TASK_OP_KIND,
         LAST_OP_KIND,
@@ -99,6 +100,7 @@ namespace Legion {
         "Trace Capture",            \
         "Trace Complete",           \
         "Trace Replay",             \
+        "Trace Begin",              \
         "Trace Summary",            \
         "Task",                     \
       } 
