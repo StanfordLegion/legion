bd0f48590953b16b322a348b6095899fb03df5f4
Mike <mebauer@cs.stanford.edu>
2019-10-16 17:28:20 -0700

    legion: fix for premap of index space task launches

--- runtime/legion/legion_tasks.cc
+++ runtime/legion/legion_tasks.cc
@@ -7524,6 +7524,14 @@ namespace Legion {
     }
 
     //--------------------------------------------------------------------------
+    void IndexTask::add_copy_profiling_request(Realm::ProfilingRequestSet &reqs)
+    //--------------------------------------------------------------------------
+    {
+      // Nothing to do, there are no copy profiling requests for premap_task
+      // If that ever changes then we need to put something here
+    }
+
+    //--------------------------------------------------------------------------
     void IndexTask::register_must_epoch(void)
     //--------------------------------------------------------------------------
     {
