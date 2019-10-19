9c16f6418529d95c85c21ea33f7fa29f35ef2da1
Mike <mebauer@cs.stanford.edu>
2019-10-18 23:22:23 -0700

    legion: fix for interfering index space task launch check

--- runtime/legion/legion_tasks.cc
+++ runtime/legion/legion_tasks.cc
@@ -6643,6 +6643,7 @@ namespace Legion {
       version_infos.clear();
 #ifdef DEBUG_LEGION
       interfering_requirements.clear();
+      point_requirements.clear();
       assert(acquired_instances.empty());
 #endif
       acquired_instances.clear();
@@ -7871,12 +7872,9 @@ namespace Legion {
       // Nothing to do if there are no interfering requirements
       if (local_interfering.empty())
         return;
-      std::map<DomainPoint,std::vector<LogicalRegion> > point_requirements;
       for (std::map<DomainPoint,std::vector<LogicalRegion> >::const_iterator 
             pit = point_reqs.begin(); pit != point_reqs.end(); pit++)
-      {
-        // Add it to the set of point requirements
-        point_requirements.insert(*pit);
+      { 
         const std::vector<LogicalRegion> &point_reqs = pit->second;
         for (std::map<DomainPoint,std::vector<LogicalRegion> >::const_iterator
               oit = point_requirements.begin(); 
@@ -8084,6 +8082,8 @@ namespace Legion {
             }
           }
         }
+        // Add it to the set of point requirements
+        point_requirements.insert(*pit);
       }
     }
 #endif
