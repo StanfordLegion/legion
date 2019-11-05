d69961998e848b37ac7a7996f78cd8d9bed83c97
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-11-05 01:46:49 +0100

    Update mappers to be compatible with SCR.

--- language/examples/stencil_mapper.cc
+++ language/examples/stencil_mapper.cc
@@ -28,7 +28,8 @@ class StencilMapper : public DefaultMapper
 {
 public:
   StencilMapper(MapperRuntime *rt, Machine machine, Processor local,
-                const char *mapper_name);
+                const char *mapper_name,
+                std::vector<Processor>* procs_list);
   virtual void default_policy_select_target_processors(
                                     MapperContext ctx,
                                     const Task &task,
@@ -39,11 +40,23 @@ public:
                                 const LayoutConstraintSet &constraints,
                                 bool force_new_instances,
                                 bool meets_constraints);
+  virtual void map_copy(const MapperContext ctx,
+                        const Copy &copy,
+                        const MapCopyInput &input,
+                        MapCopyOutput &output);
+  template<bool IS_SRC>
+  void stencil_create_copy_instance(MapperContext ctx, const Copy &copy,
+                                    const RegionRequirement &req, unsigned index,
+                                    std::vector<PhysicalInstance> &instances);
+private:
+  std::vector<Processor>& procs_list;
 };
 
 StencilMapper::StencilMapper(MapperRuntime *rt, Machine machine, Processor local,
-                             const char *mapper_name)
+                             const char *mapper_name,
+                             std::vector<Processor>* _procs_list)
   : DefaultMapper(rt, machine, local, mapper_name)
+  , procs_list(*_procs_list)
 {
 }
 
@@ -65,13 +78,122 @@ LogicalRegion StencilMapper::default_policy_select_instance_region(
   return req.region;
 }
 
+void StencilMapper::map_copy(const MapperContext ctx,
+                             const Copy &copy,
+                             const MapCopyInput &input,
+                             MapCopyOutput &output)
+{
+  log_stencil.spew("Stencil mapper map_copy");
+  for (unsigned idx = 0; idx < copy.src_requirements.size(); idx++)
+  {
+    // Always use a virtual instance for the source.
+    output.src_instances[idx].clear();
+    output.src_instances[idx].push_back(
+      PhysicalInstance::get_virtual_instance());
+
+    // Place the destination instance on the remote node.
+    output.dst_instances[idx].clear();
+    if (!copy.dst_requirements[idx].is_restricted()) {
+      // Call a customized method to create an instance on the desired node.
+      stencil_create_copy_instance<false/*is src*/>(ctx, copy, 
+        copy.dst_requirements[idx], idx, output.dst_instances[idx]);
+    } else {
+      // If it's restricted, just take the instance. This will only
+      // happen inside the shard task.
+      output.dst_instances[idx] = input.dst_instances[idx];
+      if (!output.dst_instances[idx].empty())
+        runtime->acquire_and_filter_instances(ctx,
+                                output.dst_instances[idx]);
+    }
+  }
+}
+
+//--------------------------------------------------------------------------
+template<bool IS_SRC>
+void StencilMapper::stencil_create_copy_instance(MapperContext ctx,
+                     const Copy &copy, const RegionRequirement &req, 
+                     unsigned idx, std::vector<PhysicalInstance> &instances)
+//--------------------------------------------------------------------------
+{
+  // This method is identical to the default version except that it
+  // chooses an intelligent memory based on the destination of the
+  // copy.
+
+  // See if we have all the fields covered
+  std::set<FieldID> missing_fields = req.privilege_fields;
+  for (std::vector<PhysicalInstance>::const_iterator it = 
+        instances.begin(); it != instances.end(); it++)
+  {
+    it->remove_space_fields(missing_fields);
+    if (missing_fields.empty())
+      break;
+  }
+  if (missing_fields.empty())
+    return;
+  // If we still have fields, we need to make an instance
+  // We clearly need to take a guess, let's see if we can find
+  // one of our instances to use.
+
+  // ELLIOTT: Get the remote node here.
+  Color index = runtime->get_logical_region_color(ctx, copy.src_requirements[idx].region);
+  Memory target_memory = default_policy_select_target_memory(ctx,
+                           procs_list[index % procs_list.size()],
+                           req);
+  log_stencil.warning("Building instance for copy of a region with index %u to be in memory %llx",
+                      index, target_memory.id);
+  bool force_new_instances = false;
+  LayoutConstraintID our_layout_id = 
+   default_policy_select_layout_constraints(ctx, target_memory, 
+                                            req, COPY_MAPPING,
+                                            true/*needs check*/, 
+                                            force_new_instances);
+  LayoutConstraintSet creation_constraints = 
+              runtime->find_layout_constraints(ctx, our_layout_id);
+  creation_constraints.add_constraint(
+      FieldConstraint(missing_fields,
+                      false/*contig*/, false/*inorder*/));
+  instances.resize(instances.size() + 1);
+  if (!default_make_instance(ctx, target_memory, 
+        creation_constraints, instances.back(), 
+        COPY_MAPPING, force_new_instances, true/*meets*/, req))
+  {
+    // If we failed to make it that is bad
+    log_stencil.error("Stencil mapper failed allocation for "
+                   "%s region requirement %d of explicit "
+                   "region-to-region copy operation in task %s "
+                   "(ID %lld) in memory " IDFMT " for processor "
+                   IDFMT ". This means the working set of your "
+                   "application is too big for the allotted "
+                   "capacity of the given memory under the default "
+                   "mapper's mapping scheme. You have three "
+                   "choices: ask Realm to allocate more memory, "
+                   "write a custom mapper to better manage working "
+                   "sets, or find a bigger machine. Good luck!",
+                   IS_SRC ? "source" : "destination", idx, 
+                   copy.parent_task->get_task_name(),
+                   copy.parent_task->get_unique_id(),
+		       target_memory.id,
+		       copy.parent_task->current_proc.id);
+    assert(false);
+  }
+}
+
 static void create_mappers(Machine machine, Runtime *runtime, const std::set<Processor> &local_procs)
 {
+  std::vector<Processor>* procs_list = new std::vector<Processor>();
+
+  Machine::ProcessorQuery procs_query(machine);
+  procs_query.only_kind(Processor::TOC_PROC);
+  for (Machine::ProcessorQuery::iterator it = procs_query.begin();
+        it != procs_query.end(); it++)
+    procs_list->push_back(*it);
+
   for (std::set<Processor>::const_iterator it = local_procs.begin();
         it != local_procs.end(); it++)
   {
     StencilMapper* mapper = new StencilMapper(runtime->get_mapper_runtime(),
-                                              machine, *it, "stencil_mapper");
+                                              machine, *it, "stencil_mapper",
+                                              procs_list);
     runtime->replace_default_mapper(mapper, *it);
   }
 }
