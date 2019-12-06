0291ea2a18a956ed54a141015128d13c118165c1
Mike <mebauer@cs.stanford.edu>
2019-12-05 17:43:16 -0800

    legion: fixes for nowait for logging index spaces

--- runtime/legion/region_tree.cc
+++ runtime/legion/region_tree.cc
@@ -75,10 +75,7 @@ namespace Legion {
                                         const void *realm_is, DistributedID did)
     //--------------------------------------------------------------------------
     {
-      IndexSpaceNode *node = 
-        create_node(handle, realm_is, NULL/*parent*/, 0/*color*/, did);
-      if (runtime->legion_spy_enabled || (runtime->profiler != NULL))
-        node->log_index_space_points();
+      create_node(handle, realm_is, NULL/*parent*/, 0/*color*/, did);
     }
 
     //--------------------------------------------------------------------------
