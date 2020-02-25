2be6465f33d56b14a822192629355d3cfd2e9bf8
Mike <mebauer@cs.stanford.edu>
2020-02-24 16:07:37 -0800

    python: fix to make sure we use the same python dictionary for globals and locals to ensure the module executes the way we want

--- bindings/python/legion_top.py
+++ bindings/python/legion_top.py
@@ -124,13 +124,24 @@ def run_cmd(cmd, run_name=None):
     setattr(module, '__package__', None)
 
     # Hide the current module if it exists.
+    old_module = sys.modules[run_name] if run_name in sys.modules else None
     sys.modules[run_name] = module
-    exec_locals = dict()
     code = compile(cmd, '<string>', 'eval')
-    exec(code, module.__dict__, exec_locals)
-    # Remove the reference to execution locals to ensure nothing
-    # survives past the end of the top-level task
-    del exec_locals
+    exec(code, module.__dict__, module.__dict__)
+    # Wait for execution to finish here before removing the module
+    # because executing tasks might still need to refer to it
+    future = c.legion_runtime_issue_execution_fence(
+            top_level.runtime[0], top_level.context[0])
+    # block waiting on the future
+    c.legion_future_get_void_result(future)
+    c.legion_future_destroy(future)
+    # Make sure our module gets deleted to clean up any references
+    # to variables the user might have made
+    if old_module is None:
+        del sys.modules[run_name]
+    else:
+        sys.modules[run_name] = old_module
+    del module
 
 
 # We can't use runpy for this since runpy is aggressive about
@@ -150,20 +161,23 @@ def run_path(filename, run_name=None):
 
     sys.path.append(os.path.dirname(filename))
 
-    exec_locals = dict()
     with open(filename) as f:
         code = compile(f.read(), filename, 'exec')
-        exec(code, module.__dict__, exec_locals)
-    # Remove the reference to execution locals to ensure nothing
-    # survives past the end of the top-level task
-    del exec_locals
-
-    # FIXME: Can't restore the old module because tasks may be
-    # continuing to execute asynchronously. We could fix this with
-    # an execution fence but it doesn't seem worth it given that
-    # we'll be cleaning up the process right after this.
-
-    # sys.modules[run_name] = old_module
+        exec(code, module.__dict__, module.__dict__)
+    # Wait for execution to finish here before removing the module
+    # because executing tasks might still need to refer to it
+    future = c.legion_runtime_issue_execution_fence(
+            top_level.runtime[0], top_level.context[0])
+    # block waiting on the future
+    c.legion_future_get_void_result(future)
+    c.legion_future_destroy(future)
+    # Make sure our module gets deleted to clean up any references
+    # to variables the user might have made
+    if old_module is None:
+        del sys.modules[run_name]
+    else:
+        sys.modules[run_name] = old_module
+    del module
 
 
 def import_global(module, check_depth=True):
