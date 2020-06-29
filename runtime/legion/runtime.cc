a90a8592d869b0c8f2b50a4b6fd79eb1cf92b547
Mike <mebauer@cs.stanford.edu>
2020-06-29 00:09:58 -0700

    legion: fix for running without a default mapper

--- runtime/legion/runtime.cc
+++ runtime/legion/runtime.cc
@@ -3195,7 +3195,11 @@ namespace Legion {
         finder->second = std::pair<MapperManager*,bool>(m, own);
       }
       else
+      {
         mappers[0] = std::pair<MapperManager*,bool>(m, own);
+        AutoLock q_lock(queue_lock);
+        mapper_states[0] = MapperState();
+      }
     }
 
     //--------------------------------------------------------------------------
