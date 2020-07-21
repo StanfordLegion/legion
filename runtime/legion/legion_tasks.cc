268132e77d2893aa61020fa59c4dd3119fc17d29
Mike <mebauer@cs.stanford.edu>
2020-07-21 02:55:08 -0700

    legion: fixes for serialization for origin-mapped tasks

--- runtime/legion/legion_tasks.cc
+++ runtime/legion/legion_tasks.cc
@@ -2160,7 +2160,7 @@ namespace Legion {
       DETAILED_PROFILER(runtime, PACK_SINGLE_TASK_CALL);
       RezCheck z(rez);
       pack_base_task(rez, target);
-      if (map_origin)
+      if (is_origin_mapped())
       {
         rez.serialize(selected_variant);
         rez.serialize<size_t>(target_processors.size());
@@ -2170,6 +2170,17 @@ namespace Legion {
           rez.serialize<bool>(virtual_mapped[idx]);
         rez.serialize(deferred_complete_mapping);
         deferred_complete_mapping = RtUserEvent::NO_RT_USER_EVENT;
+        rez.serialize<size_t>(physical_instances.size());
+        for (unsigned idx = 0; idx < physical_instances.size(); idx++)
+          physical_instances[idx].pack_references(rez);
+        rez.serialize<size_t>(task_profiling_requests.size());
+        for (unsigned idx = 0; idx < task_profiling_requests.size(); idx++)
+          rez.serialize(task_profiling_requests[idx]);
+        rez.serialize<size_t>(copy_profiling_requests.size());
+        for (unsigned idx = 0; idx < copy_profiling_requests.size(); idx++)
+          rez.serialize(copy_profiling_requests[idx]);
+        if (!task_profiling_requests.empty() || !copy_profiling_requests.empty())
+          rez.serialize(profiling_priority);
       }
       else
       {
@@ -2190,10 +2201,7 @@ namespace Legion {
           // Should be empty after too
           assert(map_applied_conditions.empty());
 #endif
-        }
-        rez.serialize<size_t>(copy_profiling_requests.size());
-        for (unsigned idx = 0; idx < copy_profiling_requests.size(); idx++)
-          rez.serialize(copy_profiling_requests[idx]);
+        } 
         if (!deferred_complete_mapping.exists())
         {
 #ifdef DEBUG_LEGION
@@ -2214,14 +2222,6 @@ namespace Legion {
           deferred_complete_mapping = RtUserEvent::NO_RT_USER_EVENT;
         }
       }
-      rez.serialize<size_t>(physical_instances.size());
-      for (unsigned idx = 0; idx < physical_instances.size(); idx++)
-        physical_instances[idx].pack_references(rez);
-      rez.serialize<size_t>(task_profiling_requests.size());
-      for (unsigned idx = 0; idx < task_profiling_requests.size(); idx++)
-        rez.serialize(task_profiling_requests[idx]);
-      if (!task_profiling_requests.empty() || !copy_profiling_requests.empty())
-        rez.serialize(profiling_priority);
     }
 
     //--------------------------------------------------------------------------
@@ -2252,14 +2252,21 @@ namespace Legion {
         }
         derez.deserialize(deferred_complete_mapping);
         complete_mapping(deferred_complete_mapping);
-      }
-      else
-      {
-#ifdef DEBUG_LEGION
-        assert(remote_trace_info == NULL);
-#endif
-        remote_trace_info = 
-          TraceInfo::unpack_remote_trace_info(derez, this, runtime);
+        size_t num_phy;
+        derez.deserialize(num_phy);
+        physical_instances.resize(num_phy);
+        for (unsigned idx = 0; idx < num_phy; idx++)
+          physical_instances[idx].unpack_references(runtime,
+                                                    derez, ready_events);
+        update_no_access_regions();
+        size_t num_task_requests;
+        derez.deserialize(num_task_requests);
+        if (num_task_requests > 0)
+        {
+          task_profiling_requests.resize(num_task_requests);
+          for (unsigned idx = 0; idx < num_task_requests; idx++)
+            derez.deserialize(task_profiling_requests[idx]);
+        }
         size_t num_copy_requests;
         derez.deserialize(num_copy_requests);
         if (num_copy_requests > 0)
@@ -2268,25 +2275,19 @@ namespace Legion {
           for (unsigned idx = 0; idx < num_copy_requests; idx++)
             derez.deserialize(copy_profiling_requests[idx]);
         }
+        if (!task_profiling_requests.empty() || 
+            !copy_profiling_requests.empty())
+          derez.deserialize(profiling_priority);
+      }
+      else
+      {
+#ifdef DEBUG_LEGION
+        assert(remote_trace_info == NULL);
+#endif
+        remote_trace_info = 
+          TraceInfo::unpack_remote_trace_info(derez, this, runtime); 
         derez.deserialize(deferred_complete_mapping);
       }
-      size_t num_phy;
-      derez.deserialize(num_phy);
-      physical_instances.resize(num_phy);
-      for (unsigned idx = 0; idx < num_phy; idx++)
-        physical_instances[idx].unpack_references(runtime,
-                                                  derez, ready_events);
-      update_no_access_regions();
-      size_t num_task_requests;
-      derez.deserialize(num_task_requests);
-      if (num_task_requests > 0)
-      {
-        task_profiling_requests.resize(num_task_requests);
-        for (unsigned idx = 0; idx < num_task_requests; idx++)
-          derez.deserialize(task_profiling_requests[idx]);
-      }
-      if (!task_profiling_requests.empty() || !copy_profiling_requests.empty())
-        derez.deserialize(profiling_priority);
     } 
 
     //--------------------------------------------------------------------------
@@ -9371,9 +9372,9 @@ namespace Legion {
             point_futures[idx] = FutureMap(impl, false/*need reference*/);
           }
         }
-        // Set the first mapping to false since we know things are mapped
-        first_mapping = false;
       }
+      else // Set the first mapping to false since we know things are mapped
+        first_mapping = false;
       // Return true to add this to the ready queue
       return true;
     }
