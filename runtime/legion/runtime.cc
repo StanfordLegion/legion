5e48fef053d647b41f975ff25b6c3eaa3602adb6
Mike Bauer <mike@lightsighter.org>
2021-02-18 14:34:11 -0800

    legion: fix bug with calling select_task_options

--- runtime/legion/runtime.cc
+++ runtime/legion/runtime.cc
@@ -19746,7 +19746,8 @@ namespace Legion {
     }
 
     //--------------------------------------------------------------------------
-    void Runtime::add_to_ready_queue(Processor p, TaskOp *op, RtEvent wait_on)
+    void Runtime::add_to_ready_queue(Processor p, TaskOp *task,
+                                     RtEvent wait_on, bool select_options)
     //--------------------------------------------------------------------------
     {
 #ifdef DEBUG_LEGION
@@ -19755,11 +19756,15 @@ namespace Legion {
 #endif
       if (wait_on.exists() && !wait_on.has_triggered())
       {
-        TaskOp::DeferredEnqueueArgs args(proc_managers[p], op);
+        TaskOp::DeferredEnqueueArgs args(proc_managers[p], task,select_options);
         issue_runtime_meta_task(args, LG_LATENCY_DEFERRED_PRIORITY, wait_on);
       }
       else
-        proc_managers[p]->add_to_ready_queue(op);
+      {
+        if (select_options)
+          task->select_task_options(false/*prioritize*/); 
+        proc_managers[p]->add_to_ready_queue(task);
+      }
     }
 
     //--------------------------------------------------------------------------
@@ -22926,7 +22931,8 @@ namespace Legion {
                               Runtime::protect_event(pre));
       // Put the task in the ready queue, make sure that the runtime is all
       // set up across the machine before we launch it as well
-      add_to_ready_queue(target, top_task, runtime_started_event);
+      // Also indicate that we need to select task options when ready
+      add_to_ready_queue(target, top_task, runtime_started_event, true);
       return result;
     }
 
@@ -24657,6 +24663,8 @@ namespace Legion {
           {
             const TaskOp::DeferredEnqueueArgs *enqueue_args = 
               (const TaskOp::DeferredEnqueueArgs*)args;
+            if (enqueue_args->select_options)
+              enqueue_args->task->select_task_options(false/*prioritize*/);
             enqueue_args->manager->add_to_ready_queue(enqueue_args->task);
             break;
           }
