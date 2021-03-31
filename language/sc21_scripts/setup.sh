fe3ecdfda2248342eb549a9add6b1d2ad38b3182
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-03-31 16:33:52 -0700

    regent: Use new GASNet 2021.03.0.

--- language/sc21_scripts/setup.sh
+++ language/sc21_scripts/setup.sh
@@ -4,4 +4,4 @@ set -e
 
 root_dir="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 
-USE_CUDA=1 CUDA_HOME=$CUDATOOLKIT_HOME HOST_CC=gcc HOST_CXX=g++ "$root_dir"/scripts/setup_env.py --llvm-version 60
+REALM_NETWORKS=gasnetex GASNET_VERSION=GASNet-2021.3.0 USE_CUDA=1 CUDA_HOME=$CUDATOOLKIT_HOME HOST_CC=gcc HOST_CXX=g++ "$root_dir"/scripts/setup_env.py --llvm-version 60
