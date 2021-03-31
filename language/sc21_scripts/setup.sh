3e53db65a4382b1fda8b2ca167bbe1727625b23c
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-03-31 14:31:08 -0700

    regent: Switch to LLVM 6.0 for experiments.

--- language/sc21_scripts/setup.sh
+++ language/sc21_scripts/setup.sh
@@ -4,4 +4,4 @@ set -e
 
 root_dir="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 
-USE_CUDA=1 CUDA_HOME=$CUDATOOLKIT_HOME HOST_CC=gcc HOST_CXX=g++ "$root_dir"/scripts/setup_env.py --llvm-version 38
+USE_CUDA=1 CUDA_HOME=$CUDATOOLKIT_HOME HOST_CC=gcc HOST_CXX=g++ "$root_dir"/scripts/setup_env.py --llvm-version 60
