9ed3e585abfffd16ff5bbaf168fa6069df00299a
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-04-01 14:22:45 -0700

    regent: Fix typo in build script.

--- language/sc21_scripts/build_stencil.sh
+++ language/sc21_scripts/build_stencil.sh
@@ -8,6 +8,6 @@ mkdir "$1"
 cd "$1"
 
 USE_FOREIGN=0 SAVEOBJ=1 STANDALONE=1 OBJNAME=./stencil.idx $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -findex-launch 1
-USE_FOREIGN=0 SAVEOBJ=1 STANDALONE=1 OBJNAME=./stencil.idx $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -findex-launch 0
+USE_FOREIGN=0 SAVEOBJ=1 STANDALONE=1 OBJNAME=./stencil.noidx $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -findex-launch 0
 
 cp $root_dir/*_stencil*.sh .
