a7e45cf0da91ff1530932f1576cf7c39feef2d38
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-04-04 18:12:14 +0200

    regent: Disable future optimization on Circuit/Stencil to work around performance regression.

--- language/sc21_scripts/build_stencil.sh
+++ language/sc21_scripts/build_stencil.sh
@@ -7,7 +7,9 @@ root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 mkdir "$1"
 cd "$1"
 
-USE_FOREIGN=0 SAVEOBJ=1 STANDALONE=1 OBJNAME=./stencil.idx $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -findex-launch 1
-USE_FOREIGN=0 SAVEOBJ=1 STANDALONE=1 OBJNAME=./stencil.noidx $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -findex-launch 0
+# Hack: -ffuture 0 is a workaround for blocking on a future with the trace loop
+
+USE_FOREIGN=0 SAVEOBJ=1 STANDALONE=1 OBJNAME=./stencil.idx $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -findex-launch 1 -ffuture 0
+USE_FOREIGN=0 SAVEOBJ=1 STANDALONE=1 OBJNAME=./stencil.noidx $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -findex-launch 0 -ffuture 0
 
 cp $root_dir/*_stencil*.sh .
