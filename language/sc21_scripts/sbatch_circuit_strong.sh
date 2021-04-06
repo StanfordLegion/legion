94ae2cdfcc1b51a69c6a3b2f564c37ca2344a366
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-04-07 01:04:52 +0200

    regent: Give up on nodcr_noidx configurations for now.

--- language/sc21_scripts/sbatch_circuit_strong.sh
+++ language/sc21_scripts/sbatch_circuit_strong.sh
@@ -48,14 +48,14 @@ done
 
 popd
 
-if [[ ! -d nodcr_noidx ]]; then mkdir nodcr_noidx; fi
-pushd nodcr_noidx
+# if [[ ! -d nodcr_noidx ]]; then mkdir nodcr_noidx; fi
+# pushd nodcr_noidx
 
-for n in $SLURM_JOB_NUM_NODES; do
-  for r in 0 1 2 3 4; do
-    echo "Running $n""x1_r$r"
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/circuit.noidx" -npp 2500 -wpp 10000 -l 50 -p $(( 512 )) -pps $(( 512 / n )) -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:pin_util -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 0 -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2 | tee out_"$n"x1_r"$r".log
-  done
-done
+# for n in $SLURM_JOB_NUM_NODES; do
+#   for r in 0 1 2 3 4; do
+#     echo "Running $n""x1_r$r"
+#     srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/circuit.noidx" -npp 2500 -wpp 10000 -l 50 -p $(( 512 )) -pps $(( 512 / n )) -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:pin_util -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 0 -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2 | tee out_"$n"x1_r"$r".log
+#   done
+# done
 
-popd
+# popd
--- language/sc21_scripts/sbatch_stencil_strong.sh
+++ language/sc21_scripts/sbatch_stencil_strong.sh
@@ -60,17 +60,17 @@ done
 
 popd
 
-if [[ ! -d nodcr_noidx ]]; then mkdir nodcr_noidx; fi
-pushd nodcr_noidx
+# if [[ ! -d nodcr_noidx ]]; then mkdir nodcr_noidx; fi
+# pushd nodcr_noidx
 
-for i in $power; do
-  n=$(( 2 ** i))
-  nx=$(( 2 ** ((i+1)/2) ))
-  ny=$(( 2 ** (i/2) ))
-  for r in 0 1 2 3 4; do
-    echo "Running $n""x1_r$r"" ($n = $nx * $ny)..."
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.noidx" -nx 30000 -ny 30000 -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:pin_util -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 0 -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2 | tee out_"$n"x1_r"$r".log
-  done
-done
+# for i in $power; do
+#   n=$(( 2 ** i))
+#   nx=$(( 2 ** ((i+1)/2) ))
+#   ny=$(( 2 ** (i/2) ))
+#   for r in 0 1 2 3 4; do
+#     echo "Running $n""x1_r$r"" ($n = $nx * $ny)..."
+#     srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.noidx" -nx 30000 -ny 30000 -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:pin_util -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 0 -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2 | tee out_"$n"x1_r"$r".log
+#   done
+# done
 
-popd
+# popd
