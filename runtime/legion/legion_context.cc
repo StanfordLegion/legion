8f497e3068853a7319784aaebc4670669b6051e2
Mike Bauer <mike@lightsighter.org>
2021-05-24 17:00:33 -0700

    legion: avoid leaking created index spaces for inline index task launches in leaf task contexts

--- runtime/legion/legion_context.cc
+++ runtime/legion/legion_context.cc
@@ -12344,6 +12344,13 @@ namespace Legion {
       }
       if (!task_local_instances.empty())
         release_task_local_instances(deferred_result_instance);
+      if (!index_launch_spaces.empty())
+      {
+        for (std::map<Domain,IndexSpace>::const_iterator it = 
+              index_launch_spaces.begin(); it != 
+              index_launch_spaces.end(); it++)
+          destroy_index_space(it->second, false/*unordered*/, true/*recurse*/);
+      }
       // No need to unmap the physical regions, they never had events
       if (!execution_events.empty())
       {
