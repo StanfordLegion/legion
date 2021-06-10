114b35701f50caaceacb506b9d4a843741eb6f8c
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-06-10 10:38:11 -0700

    regent: Add check for memory utilization.

--- language/test.py
+++ language/test.py
@@ -171,6 +171,7 @@ def compare_prof_results(verbose, py_exe_path, profile_dirs):
            '--file-pattern', 'tsv/*GPU*_util.tsv',
            '--file-pattern', 'tsv/*Utility*_util.tsv',
            '--file-pattern', 'tsv/*Channel*_util.tsv',
+           '--file-pattern', 'tsv/*Memory*_util.tsv',
            '--exclude-field', 'tsv/Proc*.tsv:title',
            '--exclude-field', 'tsv/Proc*.tsv:prof_uid',
            '--exclude-field', 'tsv/Proc*.tsv:initiation',
