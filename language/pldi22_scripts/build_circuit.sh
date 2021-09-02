fd8b5db8d38c34334dd4e23d5a8d80ced02e12d3
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-09-02 14:34:04 -0700

    paper: Update scripts for new experiments.

--- language/pldi22_scripts/build_circuit.sh
+++ language/pldi22_scripts/build_circuit.sh
@@ -10,6 +10,5 @@ cd "$1"
 # Hack: -ffuture 0 is a workaround for blocking on a future with the trace loop
 
 SAVEOBJ=1 STANDALONE=1 OBJNAME=./circuit.idx $root_dir/../regent.py $root_dir/../examples/circuit_sparse.rg -fflow 0 -fopenmp 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -findex-launch 1 -ffuture 0
-SAVEOBJ=1 STANDALONE=1 OBJNAME=./circuit.noidx $root_dir/../regent.py $root_dir/../examples/circuit_sparse.rg -fflow 0 -fopenmp 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -findex-launch 0 -ffuture 0
 
 cp $root_dir/*_circuit*.sh .
--- language/pldi22_scripts/sbatch_stencil.sh
+++ language/pldi22_scripts/sbatch_stencil.sh
@@ -24,52 +24,8 @@ for i in $power; do
   ny=$(( 2 ** (i/2) ))
   for r in 0 1 2 3 4; do
     echo "Running $n""x1_r$r"" ($n = $nx * $ny)..."
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.idx" -nx $(( nx * 30000 )) -ny $(( ny * 30000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 1 -dm:same_address_space -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2 | tee out_"$n"x1_r"$r".log
-  done
-done
-
-popd
-
-if [[ ! -d dcr_noidx ]]; then mkdir dcr_noidx; fi
-pushd dcr_noidx
-
-for i in $power; do
-  n=$(( 2 ** i))
-  nx=$(( 2 ** ((i+1)/2) ))
-  ny=$(( 2 ** (i/2) ))
-  for r in 0 1 2 3 4; do
-    echo "Running $n""x1_r$r"" ($n = $nx * $ny)..."
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.noidx" -nx $(( nx * 30000 )) -ny $(( ny * 30000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 1 -dm:same_address_space -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2 | tee out_"$n"x1_r"$r".log
-  done
-done
-
-popd
-
-if [[ ! -d nodcr_idx ]]; then mkdir nodcr_idx; fi
-pushd nodcr_idx
-
-for i in $power; do
-  n=$(( 2 ** i))
-  nx=$(( 2 ** ((i+1)/2) ))
-  ny=$(( 2 ** (i/2) ))
-  for r in 0 1 2 3 4; do
-    echo "Running $n""x1_r$r"" ($n = $nx * $ny)..."
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.idx" -nx $(( nx * 30000 )) -ny $(( ny * 30000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 0 -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2 | tee out_"$n"x1_r"$r".log
-  done
-done
-
-popd
-
-if [[ ! -d nodcr_noidx ]]; then mkdir nodcr_noidx; fi
-pushd nodcr_noidx
-
-for i in $power; do
-  n=$(( 2 ** i))
-  nx=$(( 2 ** ((i+1)/2) ))
-  ny=$(( 2 ** (i/2) ))
-  for r in 0 1 2 3 4; do
-    echo "Running $n""x1_r$r"" ($n = $nx * $ny)..."
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.noidx" -nx $(( nx * 30000 )) -ny $(( ny * 30000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 0 -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2 | tee out_"$n"x1_r"$r".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.idx" -nx $(( nx * 30000 )) -ny $(( ny * 30000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 1 -dm:same_address_space | tee out_"$n"x1_r"$r".log
+    #  -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2
   done
 done
 
