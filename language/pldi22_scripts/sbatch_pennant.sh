8a04496dc280fb174ce5c52347caa59d24623742
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-09-09 18:28:29 +0200

    paper: Update scripts for no-DCR configuration.

--- language/pldi22_scripts/sbatch_pennant.sh
+++ language/pldi22_scripts/sbatch_pennant.sh
@@ -12,13 +12,13 @@ ulimit -S -c 0 # disable core dumps
 
 export GASNET_PHYSMEM_MAX=16G # hack for some reason this seems to be necessary on Piz Daint now
 
-if [[ ! -d paint ]]; then mkdir paint; fi
-pushd paint
+if [[ ! -d paint_nodcr ]]; then mkdir paint_nodcr; fi
+pushd paint_nodcr
 
 for n in $SLURM_JOB_NUM_NODES; do
   for r in 0 1 2 3 4; do
     echo "Running $n""x1_r$r"
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/pennant.idx" "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x30/leblanc.pnt -npieces "$n" -numpcx 1 -numpcy "$n" -seq_init 0 -par_init 1 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 1 -dm:same_address_space | tee out_"$n"x1_r"$r".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/pennant.idx" "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x30/leblanc.pnt -npieces "$n" -numpcx 1 -numpcy "$n" -seq_init 0 -par_init 1 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:dma 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -level 5 -lg:no_physical_tracing | tee out_"$n"x1_r"$r".log
     #  -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2
   done
 done
--- language/pldi22_scripts/sbatch_stencil.sh
+++ language/pldi22_scripts/sbatch_stencil.sh
@@ -15,8 +15,8 @@ export GASNET_PHYSMEM_MAX=16G # hack for some reason this seems to be necessary 
 nodes=$SLURM_JOB_NUM_NODES
 power=$(echo "l($nodes)/l(2)" | bc -l | xargs printf '%.0f\n')
 
-if [[ ! -d paint ]]; then mkdir paint; fi
-pushd paint
+if [[ ! -d paint_nodcr ]]; then mkdir paint_nodcr; fi
+pushd paint_nodcr
 
 for i in $power; do
   n=$(( 2 ** i))
@@ -24,7 +24,7 @@ for i in $power; do
   ny=$(( 2 ** (i/2) ))
   for r in 0 1 2 3 4; do
     echo "Running $n""x1_r$r"" ($n = $nx * $ny)..."
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.idx" -nx $(( nx * 30000 )) -ny $(( ny * 30000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 1 -dm:same_address_space | tee out_"$n"x1_r"$r".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.idx" -nx $(( nx * 30000 )) -ny $(( ny * 30000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:dma 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -lg:no_physical_tracing | tee out_"$n"x1_r"$r".log
     #  -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2
   done
 done
