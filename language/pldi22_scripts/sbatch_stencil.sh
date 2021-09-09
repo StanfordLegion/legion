bfc97fc6d0a7dd35faf7c61c1fe8ab8e3d744882
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-09-10 01:02:05 +0200

    paper: Update stencil problem size.

--- language/pldi22_scripts/sbatch_stencil.sh
+++ language/pldi22_scripts/sbatch_stencil.sh
@@ -24,7 +24,7 @@ for i in $power; do
   ny=$(( 2 ** (i/2) ))
   for r in 0 1 2 3 4; do
     echo "Running $n""x1_r$r"" ($n = $nx * $ny)..."
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.idx" -nx $(( nx * 30000 )) -ny $(( ny * 30000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 1 -dm:same_address_space -lg:no_physical_tracing | tee out_"$n"x1_r"$r".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.idx" -nx $(( nx * 20000 )) -ny $(( ny * 20000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 1 -dm:same_address_space -lg:no_physical_tracing | tee out_"$n"x1_r"$r".log
     #  -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2
   done
 done
@@ -40,7 +40,7 @@ for i in $power; do
   ny=$(( 2 ** (i/2) ))
   for r in 0 1 2 3 4; do
     echo "Running $n""x1_r$r"" ($n = $nx * $ny)..."
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.idx" -nx $(( nx * 30000 )) -ny $(( ny * 30000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 0 -lg:no_physical_tracing | tee out_"$n"x1_r"$r".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.idx" -nx $(( nx * 20000 )) -ny $(( ny * 20000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -level 5 -dm:replicate 0 -lg:no_physical_tracing | tee out_"$n"x1_r"$r".log
     #  -dm:memoize -lg:no_fence_elision -lg:parallel_replay 2
   done
 done
