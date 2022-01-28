eae4ba19903cbcf57b20381ceb3c2248dd19dbd0
Mike Bauer <mike@lightsighter.org>
2022-01-28 13:31:59 -0800

    legion: fix undefined behavior in murmur3 hasher

--- runtime/legion/legion_utilities.h
+++ runtime/legion/legion_utilities.h
@@ -567,10 +567,7 @@ namespace Legion {
       inline uint64_t rotl64(uint64_t x, uint8_t r);
       inline uint64_t fmix64(uint64_t k);
     protected:
-      union {
-        uint64_t k[2];
-        uint8_t b[16];
-      } blocks;
+      uint8_t blocks[16];
       uint64_t h1, h2, len;
       uint8_t bytes;
       bool finalized;
@@ -1699,12 +1696,14 @@ namespace Legion {
       memcpy(&data, &ptr, sizeof(data));
       for (unsigned idx = 0; idx < sizeof(T); idx++)
       {
-        blocks.b[bytes++] = data[idx];
+        blocks[bytes++] = data[idx];
         if (bytes == 16)
         {
           // body
-          uint64_t k1 = blocks.k[0];
-          uint64_t k2 = blocks.k[1];
+          uint64_t k1, k2;
+          memcpy(&k1, blocks, sizeof(k1));
+          memcpy(&k2, blocks+sizeof(k1), sizeof(k2));
+          static_assert(sizeof(blocks) == (sizeof(k1)+sizeof(k2)), "sanity");
           k1 *= c1; k1  = rotl64(k1,31); k1 *= c2; h1 ^= k1;
           h1 = rotl64(h1,27); h1 += h2; h1 = h1*5+0x52dce729;
           k2 *= c2; k2  = rotl64(k2,33); k2 *= c1; h2 ^= k2;
@@ -1746,12 +1745,14 @@ namespace Legion {
       memcpy(&data, &value, sizeof(data));
       for (unsigned idx = 0; idx < size; idx++)
       {
-        blocks.b[bytes++] = data[idx];
+        blocks[bytes++] = data[idx];
         if (bytes == 16)
         {
           // body
-          uint64_t k1 = blocks.k[0];
-          uint64_t k2 = blocks.k[1];
+          uint64_t k1, k2;
+          memcpy(&k1, blocks, sizeof(k1));
+          memcpy(&k2, blocks+sizeof(k1), sizeof(k2));
+          static_assert(sizeof(blocks) == (sizeof(k1)+sizeof(k2)), "sanity");
           k1 *= c1; k1  = rotl64(k1,31); k1 *= c2; h1 ^= k1;
           h1 = rotl64(h1,27); h1 += h2; h1 = h1*5+0x52dce729;
           k2 *= c2; k2  = rotl64(k2,33); k2 *= c1; h2 ^= k2;
@@ -1773,23 +1774,23 @@ namespace Legion {
         uint64_t k2 = 0;
         switch (bytes)
         {
-          case 15: k2 ^= ((uint64_t)blocks.b[14]) << 48;
-          case 14: k2 ^= ((uint64_t)blocks.b[13]) << 40;
-          case 13: k2 ^= ((uint64_t)blocks.b[12]) << 32;
-          case 12: k2 ^= ((uint64_t)blocks.b[11]) << 24;
-          case 11: k2 ^= ((uint64_t)blocks.b[10]) << 16;
-          case 10: k2 ^= ((uint64_t)blocks.b[ 9]) << 8;
-          case  9: k2 ^= ((uint64_t)blocks.b[ 8]) << 0;
+          case 15: k2 ^= ((uint64_t)blocks[14]) << 48;
+          case 14: k2 ^= ((uint64_t)blocks[13]) << 40;
+          case 13: k2 ^= ((uint64_t)blocks[12]) << 32;
+          case 12: k2 ^= ((uint64_t)blocks[11]) << 24;
+          case 11: k2 ^= ((uint64_t)blocks[10]) << 16;
+          case 10: k2 ^= ((uint64_t)blocks[ 9]) << 8;
+          case  9: k2 ^= ((uint64_t)blocks[ 8]) << 0;
                    k2 *= c2; k2  = rotl64(k2,33); k2 *= c1; h2 ^= k2;
 
-          case  8: k1 ^= ((uint64_t)blocks.b[ 7]) << 56;
-          case  7: k1 ^= ((uint64_t)blocks.b[ 6]) << 48;
-          case  6: k1 ^= ((uint64_t)blocks.b[ 5]) << 40;
-          case  5: k1 ^= ((uint64_t)blocks.b[ 4]) << 32;
-          case  4: k1 ^= ((uint64_t)blocks.b[ 3]) << 24;
-          case  3: k1 ^= ((uint64_t)blocks.b[ 2]) << 16;
-          case  2: k1 ^= ((uint64_t)blocks.b[ 1]) << 8;
-          case  1: k1 ^= ((uint64_t)blocks.b[ 0]) << 0;
+          case  8: k1 ^= ((uint64_t)blocks[ 7]) << 56;
+          case  7: k1 ^= ((uint64_t)blocks[ 6]) << 48;
+          case  6: k1 ^= ((uint64_t)blocks[ 5]) << 40;
+          case  5: k1 ^= ((uint64_t)blocks[ 4]) << 32;
+          case  4: k1 ^= ((uint64_t)blocks[ 3]) << 24;
+          case  3: k1 ^= ((uint64_t)blocks[ 2]) << 16;
+          case  2: k1 ^= ((uint64_t)blocks[ 1]) << 8;
+          case  1: k1 ^= ((uint64_t)blocks[ 0]) << 0;
                    k1 *= c1; k1  = rotl64(k1,31); k1 *= c2; h1 ^= k1;
         }
         
