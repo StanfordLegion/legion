90fba2ec9e07fb2aeaaf863e229c81194c26dbab
Sean Treichler <sean@nvidia.com>
2022-02-20 11:09:11 -0800

    realm: -cuda:legacysync weakens non-hijack sync to just legacy stream

--- runtime/realm/cuda/cuda_module.h
+++ runtime/realm/cuda/cuda_module.h
@@ -90,6 +90,8 @@ namespace Realm {
       unsigned cfg_skip_gpu_count;
       bool cfg_skip_busy_gpus;
       size_t cfg_min_avail_mem;
+      int cfg_task_legacy_sync; // 0 = no, 1 = yes
+      int cfg_task_context_sync; // 0 = no, 1 = yes, -1 = default (based on hijack)
       int cfg_max_ctxsync_threads;
       bool cfg_lmem_resize_to_max;
       bool cfg_multithread_dma;
