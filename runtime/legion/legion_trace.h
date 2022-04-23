f5230b6d650d70f901cc7b2bb04c66886c103c67
Mike Bauer <mike@lightsighter.org>
2022-04-22 20:13:56 -0700

    legion: only trust the fence elision when we actually perform it

--- runtime/legion/legion_trace.h
+++ runtime/legion/legion_trace.h
@@ -468,10 +468,10 @@ namespace Legion {
     class PhysicalTrace {
     public:
       PhysicalTrace(Runtime *runtime, LegionTrace *logical_trace);
-      PhysicalTrace(const PhysicalTrace &rhs);
+      PhysicalTrace(const PhysicalTrace &rhs) = delete;
       ~PhysicalTrace(void);
     public:
-      PhysicalTrace& operator=(const PhysicalTrace &rhs);
+      PhysicalTrace& operator=(const PhysicalTrace &rhs) = delete;
     public:
       void clear_cached_template(void) { current_template = NULL; }
       void check_template_preconditions(TraceReplayOp *op,
@@ -503,6 +503,7 @@ namespace Legion {
     public:
       Runtime * const runtime;
       const LegionTrace *logical_trace;
+      const bool perform_fence_elision;
     private:
       mutable LocalLock trace_lock;
       FenceOp *previous_replay;
