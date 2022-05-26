808681add5610f2fde6752a7a0a7c5188bf0c62d
Mike <mebauer@cs.stanford.edu>
2022-05-26 02:25:28 -0700

    legion: more work on the garbage collector to be more intelligent about collections

--- runtime/legion/legion_instances.h
+++ runtime/legion/legion_instances.h
@@ -272,12 +272,12 @@ namespace Legion {
       virtual void notify_invalid(ReferenceMutator *mutator);
     public:
       bool acquire_instance(ReferenceSource source, ReferenceMutator *mutator);
-      bool try_collection(AddressSpaceID source, RtEvent &ready, 
-                          bool &already_collected);
-      bool verify_collection(RtEvent &collected);
-      void release_collection(AddressSpaceID source);
+      bool can_collect(AddressSpaceID source, bool &already_collected);
+      bool collect(RtEvent &collected);
       RtEvent set_garbage_collection_priority(MapperID mapper_id,
                                               Processor p, GCPriority priority);
+      virtual void get_instance_pointers(Memory memory, 
+                                    std::vector<uintptr_t> &pointers) const = 0;
       virtual RtEvent perform_deletion(AddressSpaceID source, 
                                        AutoLock *i_lock = NULL) = 0;
       virtual void force_deletion(void) = 0;
@@ -321,11 +321,6 @@ namespace Legion {
       static void handle_garbage_collection_acquire(Runtime *runtime,
           Deserializer &derez, AddressSpaceID source);
       static void handle_garbage_collection_acquired(Deserializer &derez);
-      static void handle_garbage_collection_release(Runtime *runtime,
-          Deserializer &derez, AddressSpaceID source);
-      static void handle_garbage_collection_verification(Runtime *runtime,
-          Deserializer &derez, AddressSpaceID source);
-      static void handle_garbage_collection_verified(Deserializer &derez);
       static void handle_garbage_collection_priority_update(Runtime *runtime,
           Deserializer &derez, AddressSpaceID source);
       static void handle_garbage_collection_debug_request(Runtime *runtime,
@@ -494,6 +489,8 @@ namespace Legion {
           LayoutConstraints *constraints, ApEvent use_event,
           ReductionOpID redop, GarbageCollectionState state);
     public:
+      virtual void get_instance_pointers(Memory memory, 
+                                    std::vector<uintptr_t> &pointers) const;
       virtual RtEvent perform_deletion(AddressSpaceID source, 
                                        AutoLock *i_lock = NULL);
       virtual void force_deletion(void);
@@ -586,6 +583,8 @@ namespace Legion {
         LegionRuntime::Accessor::AccessorType::Generic>
           get_field_accessor(FieldID fid) const;
     public:
+      virtual void get_instance_pointers(Memory memory, 
+                                    std::vector<uintptr_t> &pointers) const;
       virtual RtEvent perform_deletion(AddressSpaceID source,
                                        AutoLock *i_lock = NULL);
       virtual void force_deletion(void);
