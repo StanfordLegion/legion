0553f7686c71b5302b8e6b07b1aefa3a9c783681
Mike <mebauer@cs.stanford.edu>
2022-09-29 21:40:00 -0700

    legion: fix typo

--- runtime/legion/legion_replication.cc
+++ runtime/legion/legion_replication.cc
@@ -7117,7 +7117,7 @@ namespace Legion {
       if (points.empty())
       {
         attach_coregions_collective->perform_collective_async();
-        constconst  RtEvent collective_done =
+        const RtEvent collective_done =
           attach_coregions_collective->perform_collective_wait(false/*block*/);
         map_applied_conditions.insert(collective_done);
       }
