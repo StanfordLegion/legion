104b985660028d07da995a28b7750f774b7ae070
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-11-17 09:44:20 -0600

    Pin Terra to 1.0.6.

--- language/ppopp23_scripts/setup.sh
+++ language/ppopp23_scripts/setup.sh
@@ -4,4 +4,4 @@ set -e
 
 root_dir="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 
-REALM_NETWORKS=gasnet1 GASNET_VERSION=GASNet-1.30.0 USE_CUDA=1 CUDA_HOME=$CUDATOOLKIT_HOME HOST_CC=gcc HOST_CXX=g++ "$root_dir"/scripts/setup_env.py --llvm-version 38 --terra-cmake
+REALM_NETWORKS=gasnet1 GASNET_VERSION=GASNet-1.30.0 USE_CUDA=1 CUDA_HOME=$CUDATOOLKIT_HOME HOST_CC=gcc HOST_CXX=g++ "$root_dir"/scripts/setup_env.py --llvm-version 38 --terra-cmake --terra-url https://github.com/terralang/terra.git --terra-branch release-1.0.6
