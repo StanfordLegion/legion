1ff3bf3c27c6cce7fb36eea11996fb41fa2ff562
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-11-17 11:01:29 -0600

    Bump GASNet to 2022.9.0 to fix compilation issue.

--- language/ppopp23_scripts/setup.sh
+++ language/ppopp23_scripts/setup.sh
@@ -4,4 +4,4 @@ set -e
 
 root_dir="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 
-REALM_NETWORKS=gasnet1 GASNET_VERSION=GASNet-2021.3.0 USE_CUDA=1 CUDA_HOME=$CUDATOOLKIT_HOME HOST_CC=gcc HOST_CXX=g++ "$root_dir"/scripts/setup_env.py --llvm-version 38 --terra-cmake --terra-url https://github.com/terralang/terra.git --terra-branch release-1.0.6
+REALM_NETWORKS=gasnet1 GASNET_VERSION=GASNet-2022.9.0 USE_CUDA=1 CUDA_HOME=$CUDATOOLKIT_HOME HOST_CC=gcc HOST_CXX=g++ "$root_dir"/scripts/setup_env.py --llvm-version 38 --terra-cmake --terra-url https://github.com/terralang/terra.git --terra-branch release-1.0.6
