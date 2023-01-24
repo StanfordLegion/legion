0a6038e903ccbe6ed08361672266005487e2737b
apryakhin <apriakhin@nvidia.com>
2023-01-24 08:43:11 -0800

    Make example to create an instance with 2 logical layouts.

--- tutorial/realm/region_instances/region_instances.cc
+++ tutorial/realm/region_instances/region_instances.cc
@@ -21,47 +21,64 @@ using namespace Realm;
 enum
 {
   TOP_LEVEL_TASK = Processor::TASK_ID_FIRST_AVAILABLE + 0,
-  CREATE_REGION_TASK,
 };
 
-enum {
-  FID_INPUT = 101,
+enum
+{
+  FID1 = 101,
+  FID2 = 102,
 };
 
 Logger log_app("app");
 
 struct CreateRegionArgs {
+  RegionInstance *inst;
   Memory memory;
   Rect<1, int> bounds;
 };
 
-struct InstanceLogicalLayout {
+struct InstanceLogicalLayout1 {
   int x;
   float y;
 };
 
-void create_region_task(const void *args, size_t arglen, const void *userdata,
-                 size_t userlen, Processor p)
+struct InstanceLogicalLayout2 {
+  long long x;
+  double y;
+};
+
+template <typename FT, typename T0, typename T1>
+void update(RegionInstance inst, Rect<1, int> bounds, FieldID fid, int add)
 {
-  assert(arglen == sizeof(CreateRegionArgs));
-  const CreateRegionArgs &a = *reinterpret_cast<const CreateRegionArgs *>(args);
+  AffineAccessor<FT, 1, int> accessor(inst, fid);
+  PointInRectIterator<1, int> pit(bounds);
+  while(pit.valid) {
+    accessor[pit.p].x = static_cast<T0>(pit.p.x + add);
+    accessor[pit.p].y = static_cast<T1>(pit.p.x + add + 1);
+    pit.step();
+  }
+}
 
-  std::map<FieldID, size_t> field_sizes;
-  field_sizes[FID_INPUT] = sizeof(InstanceLogicalLayout);
-
-  RegionInstance inst = RegionInstance::NO_INST;
-  RegionInstance::create_instance(inst, a.memory, a.bounds, field_sizes,
-                                  /*SOA=*/0, ProfilingRequestSet())
-      .wait();
-  log_app.info() << "Create region task finished!";
+template <typename FT, typename T0, typename T1>
+void verify(RegionInstance inst, Rect<1, int> bounds, FieldID fid, int add)
+{
+  AffineAccessor<FT, 1, int> accessor(inst, fid);
+  PointInRectIterator<1, int> pit(bounds);
+  while(pit.valid) {
+    assert(accessor[pit.p].x == static_cast<T0>(pit.p.x + add));
+    assert(accessor[pit.p].y == static_cast<T1>(pit.p.x + add + 1));
+    log_app.info() << "p=" << pit.p << " x=" << accessor[pit.p].x
+                   << " y=" << accessor[pit.p].y;
+    pit.step();
+  }
 }
 
 void top_level_task(const void *args, size_t arglen, const void *userdata,
                     size_t userlen, Processor p)
 {
-  CreateRegionArgs region_args;
-  region_args.bounds.lo = Point<1, int>::ZEROES();
-  region_args.bounds.hi = Point<1, int>(7);
+  Rect<1, int> bounds;
+  bounds.lo = Point<1, int>(0);
+  bounds.hi = Point<1, int>(7);
 
   std::vector<Memory> memories;
   Machine::MemoryQuery mq(Machine::get_machine());
@@ -69,11 +86,23 @@ void top_level_task(const void *args, size_t arglen, const void *userdata,
   memories.assign(mq.begin(), mq.end());
   assert(!memories.empty());
 
-  region_args.memory = *memories.begin();
+  std::map<FieldID, size_t> field_sizes;
+  field_sizes[FID1] = sizeof(InstanceLogicalLayout1);
+  field_sizes[FID2] = sizeof(InstanceLogicalLayout2);
 
-  p.spawn(CREATE_REGION_TASK, &region_args, sizeof(CreateRegionArgs),
-          Event::NO_EVENT)
+  RegionInstance inst = RegionInstance::NO_INST;
+  RegionInstance::create_instance(inst, *memories.begin(), bounds, field_sizes,
+                                  /*SOA=*/0, ProfilingRequestSet())
       .wait();
+
+  update<InstanceLogicalLayout1, int, float>(inst, bounds, FID1, /*add=*/1);
+  update<InstanceLogicalLayout2, long long, double>(inst, bounds, FID2,
+                                                    /*add=*/2);
+
+  verify<InstanceLogicalLayout1, int, float>(inst, bounds, FID1, /*add=*/1);
+  verify<InstanceLogicalLayout2, long long, double>(inst, bounds, FID2,
+                                                    /*add=*/2);
+
   Runtime::get_runtime().shutdown(Event::NO_EVENT, 0 /*success*/);
 }
 
@@ -95,11 +124,6 @@ int main(int argc, const char **argv)
 
   Processor::register_task_by_kind(p.kind(), false /*!global*/, TOP_LEVEL_TASK,
                                    CodeDescriptor(top_level_task),
-                                   ProfilingRequestSet())
-      .external_wait();
-
-  Processor::register_task_by_kind(p.kind(), false /*!global*/, CREATE_REGION_TASK,
-                                   CodeDescriptor(create_region_task),
                                    ProfilingRequestSet())
       .external_wait();
 
