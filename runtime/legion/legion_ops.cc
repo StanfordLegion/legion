87825ac415cfc7f44675743e5a046aaf744fcac2
Mike <mebauer@cs.stanford.edu>
2023-02-16 16:49:25 -0800

    legion: make sure we save the right privilege fields for external resource objects

--- runtime/legion/legion_ops.cc
+++ runtime/legion/legion_ops.cc
@@ -18807,7 +18807,7 @@ namespace Legion {
       // Create the result and the point attach operations
       ExternalResourcesImpl *result = new ExternalResourcesImpl(ctx,
           indexes.size(), upper_bound, launch_bounds, launcher.parent,
-          launcher.privilege_fields);
+          requirement.privilege_fields);
       points.resize(indexes.size());
       for (unsigned idx = 0; idx < indexes.size(); idx++)
       {
