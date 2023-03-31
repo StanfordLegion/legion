be2588f111d1b5a20f7ede78640dc96389f4a571
apriakhin <apriakhin@nvidia.com>
2023-03-30 17:19:14 -0700

    Reformat instance.h to work with doxygen.

--- runtime/realm/instance.h
+++ runtime/realm/instance.h
@@ -38,6 +38,11 @@
 typedef ptrdiff_t intptr_t;
 #endif
 
+/**
+ * \file instance.h
+ * This file provides a C++ interface for Realm region instances.
+ */
+
 namespace Realm {
 
   typedef int FieldID;
@@ -54,6 +59,11 @@ namespace Realm {
     struct Instruction;
   };
 
+  /**
+   * \class RegionInstance
+   * A RegionInstance is a handle to a region of memory that
+   * that stores persistent application data.
+   */
   class REALM_PUBLIC_API RegionInstance {
   public:
     typedef ::realm_id_t id_t;
@@ -71,7 +81,14 @@ namespace Realm {
     //const LinearizedIndexSpaceIntfc& get_lis(void) const;
     const InstanceLayoutGeneric *get_layout(void) const;
 
-    // gets a compiled piece lookup program for a given field
+    ///@{
+    /**
+     * Return a compiled piece lookup program for a given field.
+     * @param field_id The field ID to look up.
+     * @param allowed_mask A mask of allowed piece types.
+     * @param field_offset The offset of the field within the piece.
+     * @return A pointer to the compiled piece lookup program.
+     */
     template <int N, typename T>
     REALM_INTERNAL_API_EXTERNAL_LINKAGE
     const PieceLookup::Instruction *get_lookup_program(FieldID field_id,
@@ -83,22 +100,40 @@ namespace Realm {
 						       const Rect<N,T>& subrect,
 						       unsigned allowed_mask,
 						       uintptr_t& field_offset);
+    ///@}
 
-    // these methods may be used to access the contents of an instance, but
-    //  users are encouraged to use various accessors which make repeated
-    //  accesses much more efficient
+    ///@{
+    /**
+     * Read data from an instance.
+     * Users are encouraged to use the various accessors which make repeated
+     * accesses much more efficient.
+     * @param offset The offset into the instance to read from.
+     * @param data A pointer to the data to read into.
+     * @param datalen The number of bytes to read.
+     */
+    void read_untyped(size_t offset, void* data, size_t datalen) const;
+    ///@}
 
-    void read_untyped(size_t offset, void *data, size_t datalen) const;
     void write_untyped(size_t offset, const void *data, size_t datalen) const;
+
     void reduce_apply_untyped(size_t offset, ReductionOpID redop_id,
 			      const void *data, size_t datalen,
 			      bool exclusive = false) const;
     void reduce_fold_untyped(size_t offset, ReductionOpID redop_id,
 			     const void *data, size_t datalen,
 			     bool exclusive = false) const;
-    // returns a null pointer if the instance storage cannot be directly
-    //  accessed via load/store instructions
+
+    ///@{
+    /**
+     * Return a pointer to the instance data.
+     * Returns a null pointer if the instance storage cannot be directly
+     * accessed via load/store instructions.
+     * @param offset The offset into the instance to read from.
+     * @param datalen The number of bytes to read.
+     * @return A pointer to the instance data.
+     */
     void *pointer_untyped(size_t offset, size_t datalen) const;
+    ///@}
 
     // typed template helpers of the above
     template <typename T>
@@ -114,31 +149,66 @@ namespace Realm {
     template <typename T>
     T *pointer(size_t offset) const;
 
-    // calls to create_instance return immediately with a handle, but also
-    //  return an event that must be used as a precondition for any use (or
-    //  destruction) of the instance
+    ///@{
+    /**
+     * Create a new region instance. Calls to create_instance return immediately
+     * with a handle, but also return an event that must be used as a
+     * precondition for any use (or destruction) of the instance.
+     * @param inst The handle to the new instance.
+     * @param memory The memory to create the instance in.
+     * @param ilg The layout of the instance.
+     * @param prs The profiling requests for the instance.
+     * @param wait_on The event to wait on before creating the instance.
+     * @return The event to wait on before using the instance.
+     */
     static Event create_instance(RegionInstance& inst,
 				 Memory memory,
 				 InstanceLayoutGeneric *ilg,
 				 const ProfilingRequestSet& prs,
 				 Event wait_on = Event::NO_EVENT);
+    ///@}
 
-    // creates an instance that is backed by an external resource - Realm
-    //  performs no allocation, but allows access and copies as with normal
-    //  instances
+    ///@{
+    /**
+     * Create a new region instance backed by an external resource.
+     * Realm performs no allocation, but allows access and copies as with
+     * normal instances.
+     * @param inst The handle to the new instance.
+     * @param memory The memory to create the instance in.
+     * @param ilg The layout of the instance.
+     * @param resource The external resource to back the instance.
+     * @param prs The profiling requests for the instance.
+     * @param wait_on The event to wait on before creating the instance.
+     * @return The event to wait on before using the instance.
+     */
     static Event create_external_instance(RegionInstance& inst,
 					  Memory memory,
 					  InstanceLayoutGeneric *ilg,
 					  const ExternalInstanceResource& resource,
 					  const ProfilingRequestSet& prs,
 					  Event wait_on = Event::NO_EVENT);
+    ///@}
 
+    ///@{
+    /**
+     * Create a new region instance based on an index space.
+     * block_size=0 means SOA, block_size=1 means AOS, block_size>1 means
+     * hybrid (block_size fields per block).
+     * @param inst The handle to the new instance.
+     * @param memory The memory to create the instance in.
+     * @param space The index space to create the instance for.
+     * @param field_sizes The size of each field in the instance.
+     * @param block_size The block size to use for the instance.
+     * @param prs The profiling requests for the instance.
+     * @param wait_on The event to wait on before creating the instance.
+     * @return The event to wait on before using the instance.
+     */
     template <int N, typename T>
     static Event create_instance(RegionInstance& inst,
 				 Memory memory,
 				 const IndexSpace<N,T>& space,
 				 const std::vector<size_t>& field_sizes,
-				 size_t block_size, // 0=SOA, 1=AOS, 2+=hybrid
+				 size_t block_size,
 				 const ProfilingRequestSet& prs,
 				 Event wait_on = Event::NO_EVENT);
 
@@ -147,9 +217,10 @@ namespace Realm {
 				 Memory memory,
 				 const IndexSpace<N,T>& space,
 				 const std::map<FieldID, size_t>& field_sizes,
-				 size_t block_size, // 0=SOA, 1=AOS, 2+=hybrid
+				 size_t block_size,
 				 const ProfilingRequestSet& prs,
 				 Event wait_on = Event::NO_EVENT);
+    ///@}
 
     // we'd like the methods above to accept a Rect<N,T> in place of the
     //  IndexSpace<N,T>, but that doesn't work unless the method template
@@ -216,13 +287,20 @@ namespace Realm {
 
     AddressSpace address_space(void) const;
 
-    // before you can get an instance's index space or construct an accessor for
-    //  a given processor, the necessary metadata for the instance must be
-    //  available on to that processor
-    // this can require network communication and/or completion of the actual
-    //  allocation, so an event is returned and (as always) the application
-    //  must decide when/where to handle this precondition
+    ///@{
+    /**
+     * Fetch the metadata for an instance on a given processor. Before
+     * you can get an instance's index space or construct an accessor for
+     * a given processor, the necessary metadata for the instance must be
+     * available on to that processor. This can require network communication
+     * and/or completion of the actual allocation, so an event is returned
+     * and (as always) the application must decide when/where to handle this
+     * precondition.
+     * @param target The processor to fetch the metadata for.
+     * @return The event to wait on before using the instance.
+     */
     Event fetch_metadata(Processor target) const;
+    ///@}
 
     // apparently we can't use default template parameters on methods without C++11, but we
     //  can provide templates of two different arities...
@@ -244,7 +322,22 @@ namespace Realm {
     //  (which must be deleted by the caller) that corresponds to this instance
     //  but may return a null pointer for instances that do not support
     //  re-registration
-    ExternalInstanceResource *generate_resource_info(bool read_only) const;
+    //
+
+    /**
+     * Generate an ExternalInstanceResource object for this instance.
+     *
+     * This function creates an ExternalInstanceResource object that represents
+     * this instance and can be used to register it as an "external" instance,
+     * which can provide a different view on the same bits. The returned object
+     * should be deleted by the caller when no longer needed.
+     *
+     * @param read_only A flag indicating whether the instance will be used
+     * read-only.
+     * @return An ExternalInstanceResource object for this instance, or null if
+     *         the instance does not support re-registration.
+     */
+    ExternalInstanceResource* generate_resource_info(bool read_only) const;
 
     // a version of the above that allows limiting the described memory to just
     //  a subset of the original instance (NOTE: this will accept any
@@ -252,6 +345,7 @@ namespace Realm {
     ExternalInstanceResource *generate_resource_info(const IndexSpaceGeneric& space,
 						     span<const FieldID> fields,
 						     bool read_only) const;
+    ///@}
 
     struct DestroyedField {
     public:
@@ -281,6 +375,10 @@ namespace Realm {
   std::ostream& operator<<(std::ostream& os, RegionInstance r);
 
 
+  /**
+   * \class ExternalInstanceResource
+   * A class that represents an external instance resource.
+   */
   class REALM_PUBLIC_API ExternalInstanceResource {
   protected:
     // only subclasses can be constructed
@@ -308,6 +406,9 @@ namespace Realm {
   bool serialize(S& serializer, const ExternalInstanceResource& res);
 
 
+  /**
+   * \class ExternalMemoryResource
+   */
   class REALM_PUBLIC_API ExternalMemoryResource : public ExternalInstanceResource {
   public:
     ExternalMemoryResource(uintptr_t _base, size_t _size_in_bytes, bool _read_only);
@@ -338,6 +439,9 @@ namespace Realm {
     bool read_only;
   };
 
+  /**
+   * \class ExternalFileResource
+   */
   class REALM_PUBLIC_API ExternalFileResource : public ExternalInstanceResource {
   public:
     ExternalFileResource(const std::string& _filename, realm_file_mode_t _mode,
