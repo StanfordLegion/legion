ff549c63cbebfde561f94751e700b3bdd07d4f72
Mike <mebauer@cs.stanford.edu>
2023-05-15 16:52:14 -0700

    legion: mark replicated inline mappings as being a kind of collective write

--- runtime/legion/legion_replication.cc
+++ runtime/legion/legion_replication.cc
@@ -6531,6 +6531,8 @@ namespace Legion {
     void ReplMapOp::initialize_replication(ReplicateContext *ctx) 
     //--------------------------------------------------------------------------
     {
+      // Mark that this is collective
+      requirement.privilege |= LEGION_COLLECTIVE_MASK;
       if (!remap_region && !runtime->unsafe_mapper)
       {
         mapping_check = ctx->get_next_collective_index(COLLECTIVE_LOC_74);
