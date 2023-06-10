d98e71c3ab1f1ca2dcebe456bbd07d6b912749de
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-06-09 21:49:18 -0700

    legion_prof_rs: Changes to match prof-viewer.

--- tools/legion_prof_rs/src/backend/viewer.rs
+++ tools/legion_prof_rs/src/backend/viewer.rs
@@ -5,9 +5,7 @@ use crate::state::State;
 
 pub fn start(state: State) {
     app::start(
-        Box::new(DeferredDataSourceWrapper::new(Box::new(
-            StateDataSource::new(state),
-        ))),
+        Box::new(DeferredDataSourceWrapper::new(StateDataSource::new(state))),
         None,
     );
 }
