6873bcbdfe556bb65d97d4a62590575273f6faa6
Wei Wu <eddy16112@gmail.com>
2023-06-12 22:09:22 +0000

    CMake: move the CUDA_GENCODE into the top level CMakeLists.txt

--- runtime/runtime.mk
+++ runtime/runtime.mk
@@ -653,10 +653,14 @@ ifeq ($(strip $(GPU_ARCH)),ampere)
 override GPU_ARCH = 80
 NVCC_FLAGS	+= -DAMPERE_ARCH
 endif
+ifeq ($(strip $(GPU_ARCH)),hopper)
+override GPU_ARCH = 90
+NVCC_FLAGS	+= -DHOPPER_ARCH
+endif
 
 ifeq ($(strip $(GPU_ARCH)),auto)
   # detect based on what nvcc supports
-  ALL_ARCHES = 20 30 32 35 37 50 52 53 60 61 62 70 72 75 80
+  ALL_ARCHES = 20 30 32 35 37 50 52 53 60 61 62 70 72 75 80 90
   override GPU_ARCH = $(shell for X in $(ALL_ARCHES) ; do \
     $(NVCC) -gencode arch=compute_$$X,code=sm_$$X -cuda -x c++ /dev/null -o /dev/null 2> /dev/null && echo $$X; \
   done)
