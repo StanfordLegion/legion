7eaf1e4664b038bc28d6ff4809726a32f26bfcf1
Mike <mebauer@cs.stanford.edu>
2024-02-02 01:35:05 -0800

    legion: fix for detecting postconditions of a trace including cases when there are reduction-only postconditions

--- runtime/legion/legion_trace.cc
+++ runtime/legion/legion_trace.cc
@@ -4509,19 +4509,9 @@ namespace Legion {
 #endif
           if (ready.exists() && !ready.has_triggered())
             ready.wait();
-          // Query the view for the events that it needs
-          // Note that if we're not performing actual fence elision
-          // we switch the usage to full read-write privileges so 
-          // that we can capture all dependences for the end of the trace
-          if (!trace->perform_fence_elision)
-          {
-            const RegionUsage usage(LEGION_READ_WRITE, LEGION_EXCLUSIVE, 0);
-            finder->second->find_last_users(manager, result.events, usage,
-                uit->mask, uit->expr, frontier_events);
-          }
-          else
-            finder->second->find_last_users(manager, result.events,
-                uit->usage, uit->mask, uit->expr, frontier_events);
+          const RegionUsage usage(LEGION_READ_WRITE, LEGION_EXCLUSIVE, 0);
+          finder->second->find_last_users(manager, result.events, usage,
+              uit->mask, uit->expr, frontier_events);
         }
       }
     }
