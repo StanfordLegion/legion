0c9ef99ce320bf424b582df74d6f4bf4048958b8
Elliott Slaughter <slaughter@cs.stanford.edu>
2024-02-27 23:20:08 -0800

    Bump copyright year.

--- test/performance/realm/event_ubench/event_ubench.cc
+++ test/performance/realm/event_ubench/event_ubench.cc
@@ -1,5 +1,5 @@
-/* Copyright 2023 Stanford University
- * Copyright 2023 NVIDIA Corp
+/* Copyright 2024 Stanford University
+ * Copyright 2024 NVIDIA Corp
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
