282603dffb16f4a0352436a8f3819e14852435db
apriakhin <apriakhin@nvidia.com>
2024-04-26 11:33:57 -0700

    realm: refactor refcount_image_test

--- test/realm/refcount_image_test.cc
+++ test/realm/refcount_image_test.cc
@@ -25,31 +25,34 @@ Logger log_app("app");
 enum
 {
   MAIN_TASK = Processor::TASK_ID_FIRST_AVAILABLE + 0,
-  NODE_TASK_0,
+  NODE_TASK,
 };
+
+constexpr int NUM_INSTS = 1;
 
 struct TaskArgs {
-  IndexSpace<1> ss_inst1[1];
-  FieldDataDescriptor<IndexSpace<1>, Point<1>> fd_ptrs1[1];
-  IndexSpace<1> root2;
+  IndexSpace<1> sources[NUM_INSTS];
+  FieldDataDescriptor<IndexSpace<1>, Point<1>> ptr_data[NUM_INSTS];
+  IndexSpace<1> parent;
 };
-
 
 namespace TestConfig {
   bool remote_create = false;
 };
 
-void node_task_0(const void *args, size_t arglen, const void *userdata, size_t userlen,
-                 Processor p)
+void node_task(const void *args, size_t arglen, const void *userdata, size_t userlen,
+               Processor p)
 {
   TaskArgs &task_args = *(TaskArgs *)args;
-  std::vector<IndexSpace<1>> ss_images;
-  Event e2 = task_args.root2.create_subspaces_by_image(
-      std::vector<FieldDataDescriptor<IndexSpace<1>, Point<1>>>{task_args.fd_ptrs1[0]},
-      std::vector<IndexSpace<1>>{task_args.ss_inst1[0]}, ss_images,
-      ProfilingRequestSet());
-  for(IndexSpace<1> image : ss_images) {
-    image.sparsity.destroy(e2);
+  std::vector<IndexSpace<1>> images;
+  Event e2 = task_args.parent.create_subspaces_by_image(
+      std::vector<FieldDataDescriptor<IndexSpace<1>, Point<1>>>{
+          std::begin(task_args.ptr_data), std::end(task_args.ptr_data)},
+      std::vector<IndexSpace<1>>{std::begin(task_args.sources),
+                                 std::end(task_args.sources)},
+      images, ProfilingRequestSet());
+  for(size_t i = 0; i < images.size(); i++) {
+    images[i].sparsity.destroy(e2);
   }
 }
 
@@ -60,73 +63,68 @@ void main_task(const void *args, size_t arglen, const void *userdata, size_t use
   rects.push_back(Rect<1>(Point<1>(0), Point<1>(5)));
   rects.push_back(Rect<1>(Point<1>(10), Point<1>(15)));
 
-  UserEvent done = UserEvent::create_user_event();
-
   Machine machine = Machine::get_machine();
   std::vector<Memory> memories;
   for(Machine::MemoryQuery::iterator it = Machine::MemoryQuery(machine).begin(); it;
       ++it) {
     Memory m = *it;
-    if(m.kind() != Memory::SYSTEM_MEM)
-      continue;
-    memories.push_back(m);
+    if(m.kind() == Memory::SYSTEM_MEM) {
+      memories.push_back(m);
+    }
   }
 
-  IndexSpace<1> root1(rects);
-  IndexSpace<1> root2(rects);
+  IndexSpace<1, int> root1(rects[0]);
+  IndexSpace<1> parent(rects[0]);
 
-  size_t num_insts = 1;
-
-  std::vector<IndexSpace<1>> ss_inst1;
-  root1.create_equal_subspaces(num_insts, 1, ss_inst1, Realm::ProfilingRequestSet())
+  std::vector<IndexSpace<1>> sources;
+  root1.create_equal_subspaces(NUM_INSTS, 1, sources, Realm::ProfilingRequestSet())
       .wait();
 
   std::vector<size_t> field_sizes;
   field_sizes.push_back(sizeof(int));
   field_sizes.push_back(sizeof(Point<1>));
 
-  std::vector<FieldDataDescriptor<IndexSpace<1>, int>> fd_vals1(num_insts);
-  std::vector<FieldDataDescriptor<IndexSpace<1>, Point<1>>> fd_ptrs1(num_insts);
+  std::vector<FieldDataDescriptor<IndexSpace<1>, Point<1>>> ptr_data(NUM_INSTS);
 
-  for(size_t i = 0; i < num_insts; i++) {
-    size_t mem_idx = i % memories.size();
+  for(int i = 0; i < NUM_INSTS; i++) {
+    int mem_idx = i % memories.size();
     RegionInstance ri;
-    RegionInstance::create_instance(ri, memories[mem_idx], ss_inst1[i], field_sizes, 0,
+    RegionInstance::create_instance(ri, memories[mem_idx], sources[i], field_sizes, 0,
                                     Realm::ProfilingRequestSet())
         .wait();
-
-    fd_ptrs1[i].index_space = ss_inst1[i];
-    fd_ptrs1[i].inst = ri;
-    fd_ptrs1[i].field_offset = 0 + sizeof(int);
+    ptr_data[i].index_space = sources[i];
+    ptr_data[i].inst = ri;
+    ptr_data[i].field_offset = 0;
+    AffineAccessor<int, 1> a_vals(ri, 0);
+    for(PointInRectIterator<1, int> pir(root1.bounds); pir.valid; pir.step()) {
+      a_vals.write(pir.p, pir.p[0]);
+    }
   }
 
   std::vector<Event> events;
   for(std::vector<Memory>::const_iterator it = memories.begin(); it != memories.end();
       ++it) {
-    Memory m = *it;
-
     Processor proc = *Machine::ProcessorQuery(machine)
                           .only_kind(Processor::LOC_PROC)
-                          .same_address_space_as(m)
+                          .same_address_space_as(*it)
                           .begin();
 
     if((Network::max_node_id > 1 && TestConfig::remote_create) &&
-       NodeID(ID(m).memory_owner_node()) ==
-           NodeID(ID(fd_ptrs1[0].inst).instance_owner_node())) {
+       NodeID(ID(*it).memory_owner_node()) ==
+           NodeID(ID(ptr_data[0].inst).instance_owner_node())) {
       continue;
     }
 
-    {
-      TaskArgs args;
-      args.ss_inst1[0] = ss_inst1[0];
-      args.fd_ptrs1[0] = fd_ptrs1[0];
-      args.root2 = root2;
-      Event e = proc.spawn(NODE_TASK_0, &args, sizeof(args));
-      events.push_back(e);
+    TaskArgs args;
+    for(int i = 0; i < NUM_INSTS; i++) {
+      args.sources[0] = sources[0];
+      args.ptr_data[0] = ptr_data[0];
     }
+    args.parent = parent;
+    Event e = proc.spawn(NODE_TASK, &args, sizeof(args));
+    events.push_back(e);
   }
 
-  done.trigger();
   Event::merge_events(events).wait();
   usleep(100000);
   Runtime::get_runtime().shutdown(Processor::get_current_finish_event(), 0);
@@ -142,12 +140,9 @@ int main(int argc, char **argv)
   assert(ok);
 
   rt.init(&argc, &argv);
-
   rt.register_task(MAIN_TASK, main_task);
-
-  Processor::register_task_by_kind(Processor::LOC_PROC, false /*!global*/, NODE_TASK_0,
-                                   CodeDescriptor(node_task_0), ProfilingRequestSet(), 0,
-                                   0)
+  Processor::register_task_by_kind(Processor::LOC_PROC, /*!global=*/false, NODE_TASK,
+                                   CodeDescriptor(node_task), ProfilingRequestSet(), 0, 0)
       .wait();
 
   Processor p = Machine::ProcessorQuery(Machine::get_machine())
