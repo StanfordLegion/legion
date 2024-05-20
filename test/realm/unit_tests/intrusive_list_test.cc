32ff1f0cbde323e47d12ecabe3416e2417941a1a
apriakhin <apriakhin@nvidia.com>
2024-05-20 16:53:50 -0700

    realm: refactor tests to follow aaa pattern

--- test/realm/unit_tests/intrusive_list_test.cc
+++ test/realm/unit_tests/intrusive_list_test.cc
@@ -15,6 +15,7 @@ struct TestEntry {
   TestEntry(int _value = 0)
     : value(_value)
   {}
+
   int value;
   IntrusiveListLink<TestEntry> ew_list_link;
   REALM_PMTA_DEFN(TestEntry, IntrusiveListLink<TestEntry>, ew_list_link);
@@ -25,149 +26,106 @@ struct TestEntry {
 TEST(IntrusiveListTest, CheckEmptyList)
 {
   TestEntry::TestEntryList list;
-  EXPECT_TRUE(list.empty());
+
   TestEntry *entry = list.pop_front();
+
   EXPECT_EQ(entry, nullptr);
+  EXPECT_TRUE(list.empty());
+}
+
+TEST(IntrusiveListTest, CheckNonEmptyList)
+{
+  TestEntry::TestEntryList list;
+  TestEntry object(42);
+
+  list.push_back(&object);
+
+  EXPECT_FALSE(list.empty());
 }
 
 TEST(IntrusiveListTest, SingleEntryPushBackPopFront)
 {
   TestEntry::TestEntryList list;
   TestEntry object(42);
-  list.push_back(&object);
-  EXPECT_FALSE(list.empty());
-  {
-    TestEntry *entry = list.pop_front();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_EQ(entry->value, object.value);
-  }
-  EXPECT_TRUE(list.empty());
 
-  {
-    TestEntry *entry = list.pop_front();
-    EXPECT_EQ(entry, nullptr);
-  }
+  list.push_back(&object);
+  TestEntry *entry = list.pop_front();
+
+  EXPECT_EQ(entry->value, object.value);
 }
 
 TEST(IntrusiveListTest, SingleEntryPushFrontPopFront)
 {
   TestEntry::TestEntryList list;
   TestEntry object(42);
-  list.push_front(&object);
-  EXPECT_FALSE(list.empty());
-  {
-    TestEntry *entry = list.pop_front();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_EQ(entry->value, object.value);
-  }
-  EXPECT_TRUE(list.empty());
 
-  {
-    TestEntry *entry = list.pop_front();
-    EXPECT_EQ(entry, nullptr);
-  }
+  list.push_front(&object);
+  TestEntry *entry = list.pop_front();
+
+  EXPECT_EQ(entry->value, object.value);
 }
 
 TEST(IntrusiveListTest, PushFrontSameEntry)
 {
   TestEntry::TestEntryList list;
   TestEntry object(42);
+
   list.push_back(&object);
   list.push_back(&object);
+  TestEntry *entry1 = list.pop_front();
+  TestEntry *entry2 = list.pop_front();
 
-  EXPECT_FALSE(list.empty());
-
-  {
-    TestEntry *entry = list.pop_front();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_EQ(entry->value, object.value);
-  }
-
-  EXPECT_FALSE(list.empty());
-
-  {
-    TestEntry *entry = list.pop_front();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_EQ(entry->value, object.value);
-  }
-
-  EXPECT_TRUE(list.empty());
+  EXPECT_EQ(entry1->value, object.value);
+  EXPECT_EQ(entry2->value, object.value);
 }
 
 TEST(IntrusiveListTest, EraseEntry)
 {
   TestEntry::TestEntryList list;
   TestEntry object(42);
-  list.push_front(&object);
-  EXPECT_FALSE(list.empty());
 
+  list.push_front(&object);
   list.erase(&object);
-  EXPECT_TRUE(list.empty());
-  // erase same object again
-  list.erase(&object);
+
   EXPECT_TRUE(list.empty());
 }
 
 TEST(IntrusiveListTest, SwapLists)
 {
   TestEntry::TestEntryList list;
-  TestEntry object1(42);
-  list.push_back(&object1);
-  TestEntry object2(43);
-  list.push_back(&object2);
-  EXPECT_FALSE(list.empty());
-
   TestEntry::TestEntryList new_list;
+  TestEntry object1(42);
+  TestEntry object2(43);
   TestEntry object3(44);
+
+  list.push_back(&object1);
+  list.push_back(&object2);
   new_list.push_back(&object3);
-
   new_list.swap(list);
-  EXPECT_FALSE(new_list.empty());
-  EXPECT_FALSE(list.empty());
+  TestEntry *entry1 = new_list.pop_front();
+  TestEntry *entry2 = new_list.pop_front();
+  TestEntry *entry3 = list.pop_front();
 
-  {
-    TestEntry *entry = new_list.pop_front();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_EQ(entry->value, object1.value);
-  }
-
-  {
-    TestEntry *entry = new_list.pop_front();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_EQ(entry->value, object2.value);
-  }
-
-  {
-    TestEntry *entry = list.pop_front();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_EQ(entry->value, object3.value);
-  }
+  EXPECT_EQ(entry1->value, object1.value);
+  EXPECT_EQ(entry2->value, object2.value);
+  EXPECT_EQ(entry3->value, object3.value);
 }
 
 TEST(IntrusiveListTest, AbsortAppend)
 {
   TestEntry::TestEntryList list;
-  TestEntry object1(42);
-  list.push_back(&object1);
-  TestEntry object2(43);
-  list.push_back(&object2);
-  EXPECT_FALSE(list.empty());
-
   TestEntry::TestEntryList new_list;
+
+  TestEntry object1(42);
+  TestEntry object2(43);
+
+  list.push_back(&object1);
+  list.push_back(&object2);
   new_list.absorb_append(list);
+  TestEntry *entry1 = new_list.pop_front();
+  TestEntry *entry2 = new_list.pop_front();
 
-  EXPECT_FALSE(new_list.empty());
+  EXPECT_EQ(entry1->value, object1.value);
+  EXPECT_EQ(entry2->value, object2.value);
   EXPECT_TRUE(list.empty());
-
-  {
-    TestEntry *entry = new_list.pop_front();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_EQ(entry->value, object1.value);
-  }
-
-  {
-    TestEntry *entry = new_list.pop_front();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_EQ(entry->value, object2.value);
-  }
 }
