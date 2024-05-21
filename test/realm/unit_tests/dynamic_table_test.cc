b74e622977161961b67f088d0e561a51a6d12d84
apriakhin <apriakhin@nvidia.com>
2024-05-21 06:48:21 -0700

    realm: refactor tests to follow aaa pattern

--- test/realm/unit_tests/dynamic_table_test.cc
+++ test/realm/unit_tests/dynamic_table_test.cc
@@ -19,14 +19,24 @@ struct Dummy {
   Dummy *next_free = nullptr;
 };
 
+TEST(DynamicTableTest, CheckEmptyDynamicTable)
+{
+  const int leaf_bits = 4;
+  DynamicTable<DynamicTableAllocator<Dummy, 1, leaf_bits>> dtable;
+  int id = 0;
+
+  EXPECT_FALSE(dtable.has_entry(id));
+  EXPECT_EQ(dtable.max_entries(), 0);
+}
+
 TEST(DynamicTableTest, LookupSingleEntry)
 {
   const int leaf_bits = 4;
   DynamicTable<DynamicTableAllocator<Dummy, 1, leaf_bits>> dtable;
   int id = 0;
-  EXPECT_FALSE(dtable.has_entry(id));
-  EXPECT_EQ(dtable.max_entries(), 0);
+
   dtable.lookup_entry(id, 0);
+
   EXPECT_TRUE(dtable.has_entry(id));
   EXPECT_EQ(dtable.max_entries(), 16);
 }
@@ -35,31 +45,19 @@ TEST(DynamicTableTest, LookupMultipleEntries)
 {
   const int leaf_bits = 4;
   DynamicTable<DynamicTableAllocator<Dummy, 1, leaf_bits>> dtable;
+  std::vector<int> ids { 0, 15, 16, 33 };
+  int id0 = 0, id1 = 15, id2 = 16, id3 = 33;
 
-  int id0 = 0;
-  EXPECT_NE(dtable.lookup_entry(id0, 0), nullptr);
-  EXPECT_TRUE(dtable.has_entry(id0));
-
-  EXPECT_EQ(dtable.max_entries(), 16);
-
-  int id1 = 15;
-  EXPECT_TRUE(dtable.has_entry(id1));
-
-  int id2 = 16;
-  EXPECT_FALSE(dtable.has_entry(id2));
-  // create new leaf node
-  EXPECT_NE(dtable.lookup_entry(id2, 0), nullptr);
-  EXPECT_TRUE(dtable.has_entry(id2));
-
-  EXPECT_EQ(dtable.max_entries(), 32);
-
-  int id3 = 33;
-  EXPECT_FALSE(dtable.has_entry(id3));
-  // create new leaf node
-  EXPECT_NE(dtable.lookup_entry(id3, 0), nullptr);
-  EXPECT_TRUE(dtable.has_entry(id3));
+  std::vector<Dummy *> entries(ids.size());
+  for(int i = 0; i < ids.size(); i++) {
+    entries[i] = dtable.lookup_entry(ids[i], 0);
+  }
 
   EXPECT_EQ(dtable.max_entries(), 64);
+  for(int i = 0; i < ids.size(); i++) {
+    EXPECT_TRUE(dtable.has_entry(ids[i]));
+    EXPECT_NE(entries[i], nullptr);
+  }
 }
 
 TEST(DynamicTableTest, LookupTheLimit)
@@ -68,11 +66,16 @@ TEST(DynamicTableTest, LookupTheLimit)
   const int max_levels = 7;
   DynamicTable<DynamicTableAllocator<Dummy, 1, leaf_bits>> dtable;
   int index = 0;
+
+  std::vector<Dummy *> entries(((1 << leaf_bits) << max_levels));
   for(int i = 0; i < ((1 << leaf_bits) << max_levels); i++) {
-    EXPECT_NE(dtable.lookup_entry(index, 0), nullptr);
-    EXPECT_TRUE(dtable.has_entry(index++));
+    entries[i] = dtable.lookup_entry(index++, 0);
   }
+
   EXPECT_EQ(dtable.max_entries(), index);
+  for(int i = 0; i < entries.size(); i++) {
+    EXPECT_NE(entries[i], nullptr);
+  }
 }
 
 TEST(DynamicTableTest, FreeListSingleAlloc)
@@ -82,13 +85,24 @@ TEST(DynamicTableTest, FreeListSingleAlloc)
   DynamicTableAllocator<Dummy, 1, leaf_bits>::FreeList free_list(dtable, 0);
 
   Dummy *entry = free_list.alloc_entry();
+
   EXPECT_NE(entry, nullptr);
   EXPECT_EQ(entry->next_free, nullptr);
   EXPECT_TRUE(dtable.has_entry(entry->me.id));
+}
 
+TEST(DynamicTableTest, FreeListSingleAllocAndFree)
+{
+  const int leaf_bits = 4;
+  DynamicTable<DynamicTableAllocator<Dummy, 1, leaf_bits>> dtable;
+  DynamicTableAllocator<Dummy, 1, leaf_bits>::FreeList free_list(dtable, 0);
+
+  Dummy *entry = free_list.alloc_entry();
   free_list.free_entry(entry);
+
+  EXPECT_NE(entry, nullptr);
   EXPECT_NE(entry->next_free, nullptr);
-  EXPECT_TRUE(free_list.table.has_entry(entry->me.id)); //??
+  EXPECT_TRUE(dtable.has_entry(entry->me.id));
 }
 
 TEST(DynamicTableTest, FreeListUpMaxAlloc)
@@ -97,10 +111,14 @@ TEST(DynamicTableTest, FreeListUpMaxAlloc)
   DynamicTable<DynamicTableAllocator<Dummy, 1, leaf_bits>> dtable;
   DynamicTableAllocator<Dummy, 1, leaf_bits>::FreeList free_list(dtable, 0);
 
+  std::vector<Dummy *> entries(255);
   for(int i = 0; i < 255; i++) {
-    Dummy *entry = free_list.alloc_entry();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_TRUE(dtable.has_entry(entry->me.id));
+    entries[i] = free_list.alloc_entry();
+  }
+
+  for(int i = 0; i < 255; i++) {
+    EXPECT_NE(entries[i], nullptr);
+    EXPECT_TRUE(dtable.has_entry(entries[i]->me.id));
   }
 }
 
@@ -110,10 +128,14 @@ TEST(DynamicTableTest, FreeListOverMaxAlloc)
   DynamicTable<DynamicTableAllocator<Dummy, 1, leaf_bits>> dtable;
   DynamicTableAllocator<Dummy, 1, leaf_bits>::FreeList free_list(dtable, 0);
 
+  std::vector<Dummy *> entries(256);
   for(int i = 0; i < 256; i++) {
-    Dummy *entry = free_list.alloc_entry();
-    EXPECT_NE(entry, nullptr);
-    EXPECT_TRUE(dtable.has_entry(entry->me.id));
-    free_list.free_entry(entry);
+    entries[i] = free_list.alloc_entry();
+    free_list.free_entry(entries[i]);
+  }
+
+  for(int i = 0; i < 256; i++) {
+    EXPECT_NE(entries[i], nullptr);
+    EXPECT_TRUE(dtable.has_entry(entries[i]->me.id));
   }
 }
