fd449c29d0649103c68ecbd4f092dba3845bd688
Mike <mebauer@cs.stanford.edu>
2024-06-21 10:57:24 -0700

    realm: more format fixes

--- test/realm/deppart.cc
+++ test/realm/deppart.cc
@@ -1607,7 +1607,7 @@ public:
 
           a_side_mapsp1.write(ps0, pp0);
           a_side_mapsp1.write(ps1, pp1);
-	  a_side_mapsp1.write(ps2, pp2);
+          a_side_mapsp1.write(ps2, pp2);
 	  a_side_mapsp1.write(ps3, pp3);
 
 	  a_side_ok.write(ps0, true);
