452fb4c70bc2248aa548dd25e97c9da67373fd67
apriakhin <apriakhin@nvidia.com>
2024-07-22 11:55:12 -0700

    realm: add data-driven test for find_fastest_path

--- test/realm/unit_tests/lowlevel_dma_test.cc
+++ test/realm/unit_tests/lowlevel_dma_test.cc
@@ -157,221 +157,187 @@ TEST(FindBestChannelTest, BestChannelForMemPairNoSrcPath)
   EXPECT_EQ(best_channel, dst_channel);
 }
 
-class FFPTest : public ::testing::Test {
-protected:
-  virtual void SetUp() {}
-
-  virtual void TearDown() {}
-
-  void mock_supports_path(MockChannel *channel, Memory src, Memory dst,
-                          std::vector<size_t> *src_frags, std::vector<size_t> *dst_frags,
-                          uint64_t cost, bool direct = true, size_t total_bytes = 16)
+class FFPMockChannel : public Channel {
+public:
+  typedef std::map<std::pair<Memory, Memory>, size_t> PathMap;
+  PathMap paths;
+  // Node *node;
+  FFPMockChannel(XferDesKind _kind, NodeID _node, const PathMap &_paths)
+    : Channel(_kind)
+    , paths(_paths)
   {
-    ChannelCopyInfo info{src, dst};
-    info.is_direct = direct;
-    EXPECT_CALL(*channel, supports_path(info, /*src_serdez_id=*/0, /*dst_serdez_id*/ 0,
-                                        /*redop_id=*/0, total_bytes, src_frags, dst_frags,
-                                        testing::_, nullptr, nullptr))
-        .Times(1)
-        .WillRepeatedly(::testing::Return(cost));
+    this->node = _node;
   }
 
+  uint64_t supports_path(ChannelCopyInfo channel_copy_info, CustomSerdezID src_serdez_id,
+                         CustomSerdezID dst_serdez_id, ReductionOpID redop_id,
+                         size_t total_bytes, const std::vector<size_t> *src_frags,
+                         const std::vector<size_t> *dst_frags, XferDesKind *kind_ret,
+                         unsigned *bw_ret, unsigned *lat_ret)
+  {
+    PathMap::iterator it =
+        paths.find(std::make_pair(channel_copy_info.src_mem, channel_copy_info.dst_mem));
+    *kind_ret = this->kind;
+    return it != paths.end() ? it->second : 0;
+  }
+
+  MOCK_METHOD(Memory, suggest_ib_memories, (Memory mem), (const));
+  MOCK_METHOD(long, available, (), ());
+  MOCK_METHOD(long, submit, (Request * *requests, long nr), ());
+  MOCK_METHOD(void, pull, (), ());
+  MOCK_METHOD(void, wakeup_xd, (XferDes * xd), ());
+  MOCK_METHOD(void, enqueue_ready_xd, (XferDes * xd), ());
+  MOCK_METHOD(XferDesFactory *, get_factory, (), ());
+};
+
+struct TestCase {
+  Memory src;
+  Memory dst;
+  // TODO: ind_mem
+  std::vector<FFPMockChannel *> channels;
+  std::vector<IBMemory *> ib_mems;
+  size_t total_bytes;
+  MemPathInfo info;
+  bool status;
+};
+
+class FFPTest : public ::testing::TestWithParam<TestCase> {
+protected:
+  std::vector<size_t> src_frags;
+  std::vector<size_t> dst_frags;
   PathCache path_cache;
 };
 
-// TODO(apryakhin): Add tests with path cache
-
-TEST_F(FFPTest, FFPNoChannels)
+TEST_P(FFPTest, Base)
 {
-  std::vector<Node> nodes(1);
-  ChannelCopyInfo channel_info{ID::make_memory(0, 0).convert<Memory>(),
-                               ID::make_memory(0, 1).convert<Memory>()};
+  TestCase test_case = GetParam();
+
+  NodeID max_node_id = 0;
+  for(auto *ch : test_case.channels) {
+    max_node_id = std::max(max_node_id, ch->node);
+  }
+
+  std::vector<Node> nodes(max_node_id + 1);
+  for(auto *ch : test_case.channels) {
+    nodes[ch->node].dma_channels.push_back(ch);
+  }
+
+  for(auto *ib_mem : test_case.ib_mems) {
+    NodeID node = NodeID(ID(ib_mem->me).memory_owner_node());
+    nodes[node].ib_memories.push_back(ib_mem);
+  }
+
+  ChannelCopyInfo channel_info{test_case.src, test_case.dst};
   std::vector<size_t> src_frags;
   std::vector<size_t> dst_frags;
-  size_t total_bytes = 16;
 
   MemPathInfo info;
-  bool ok = find_fastest_path(nodes.data(), path_cache, channel_info, /*serdez_id=*/0,
-                              /*redop_id=*/0, total_bytes, &src_frags, &dst_frags, info);
-
-  EXPECT_FALSE(ok);
-  EXPECT_TRUE(info.xd_channels.empty());
-  EXPECT_TRUE(info.path.empty());
+  EXPECT_EQ(find_fastest_path(nodes.data(), path_cache, channel_info, /*serdez_id=*/0,
+                              /*redop_id=*/0, test_case.total_bytes, &src_frags,
+                              &dst_frags, info),
+            test_case.status);
+  for(size_t i = 0; i < test_case.info.path.size(); i++) {
+    EXPECT_EQ(info.path[i], test_case.info.path[i]);
+  }
+  // EXPECT_EQ(info.xd_channels.size(), test_case.info.xd_channels.size());
 }
 
-TEST_F(FFPTest, FFPSrcDstDirectNoPath)
+static inline Memory make_mem(int idx, int node_id)
 {
-  const uint64_t exp_cost_a = 0;
-  const uint64_t exp_cost_b = 0;
-  std::vector<Node> nodes(1);
-  MockChannel *channel_a = new MockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/0);
-  MockChannel *channel_b = new MockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/1);
-  nodes[0].dma_channels.push_back(channel_a);
-  nodes[0].dma_channels.push_back(channel_b);
-  Memory src_mem = ID::make_memory(0, 0).convert<Memory>();
-  Memory dst_mem = ID::make_memory(0, 1).convert<Memory>();
-
-  ChannelCopyInfo channel_info{src_mem, dst_mem};
-  std::vector<size_t> src_frags;
-  std::vector<size_t> dst_frags;
-  size_t total_bytes = 16;
-
-  mock_supports_path(channel_a, src_mem, dst_mem, &src_frags, &dst_frags, exp_cost_a);
-  mock_supports_path(channel_b, src_mem, dst_mem, &src_frags, &dst_frags, exp_cost_b);
-
-  MemPathInfo info;
-  bool ok = find_fastest_path(nodes.data(), path_cache, channel_info, /*serdez_id=*/0,
-                              /*redop_id=*/0, total_bytes, &src_frags, &dst_frags, info);
-
-  EXPECT_FALSE(ok);
-  EXPECT_TRUE(info.xd_channels.empty());
-  EXPECT_TRUE(info.path.empty());
+  return ID::make_memory(idx, node_id).convert<Memory>();
 }
 
-// TODO(aprakhin): There are more channel/cost combinations we can
-// test. Consider making parameterized tests in a future.
+INSTANTIATE_TEST_SUITE_P(
+    FindFastestPath, FFPTest,
+    testing::Values(
 
-TEST_F(FFPTest, FFPSrcDstDirect)
-{
-  const uint64_t exp_cost_a = 7;
-  const uint64_t exp_cost_b = 5;
-  std::vector<Node> nodes(1);
-  MockChannel *channel_a = new MockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/0);
-  MockChannel *channel_b = new MockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/1);
-  nodes[0].dma_channels.push_back(channel_a);
-  nodes[0].dma_channels.push_back(channel_b);
-  Memory src_mem = ID::make_memory(0, 0).convert<Memory>();
-  Memory dst_mem = ID::make_memory(0, 1).convert<Memory>();
+        // Case 0: no path available
+        TestCase{.src = make_mem(0, 0),
+                 .dst = make_mem(1, 1),
+                 .channels{new FFPMockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/0,
+                                              {// src->dst
+                                               {{make_mem(0, 0), make_mem(1, 1)},
+                                                /*cost=*/0}}),
+                           new FFPMockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/1,
+                                              {{{make_mem(0, 0), make_mem(1, 1)},
+                                                /*cost=*/0}})},
+                 .total_bytes = 16,
+                 .status = 0},
 
-  ChannelCopyInfo channel_info{src_mem, dst_mem};
-  std::vector<size_t> src_frags;
-  std::vector<size_t> dst_frags;
-  size_t total_bytes = 16;
+        // Case 1: src(0) --> dst(1)
+        TestCase{.src = make_mem(0, 0),
+                 .dst = make_mem(1, 1),
+                 .channels{new FFPMockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/0,
+                                              {// src->dst
+                                               {{make_mem(0, 0), make_mem(1, 1)},
+                                                /*cost=*/12}}),
+                           new FFPMockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/1,
+                                              {{{make_mem(0, 0), make_mem(1, 1)},
+                                                /*cost=*/6}})},
+                 .total_bytes = 16,
+                 .info =
+                     {
+                         .path = {make_mem(0, 0), make_mem(1, 1)},
+                     },
+                 .status = 1},
 
-  mock_supports_path(channel_a, src_mem, dst_mem, &src_frags, &dst_frags, exp_cost_a);
-  mock_supports_path(channel_b, src_mem, dst_mem, &src_frags, &dst_frags, exp_cost_b);
+        // Case 2: src(0) -> src_ib(0) --> dst(1)
+        TestCase{.src = make_mem(0, 0),
+                 .dst = make_mem(1, 1),
+                 .channels{new FFPMockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/0,
+                                              {// src->dst
+                                               {{make_mem(0, 0), make_mem(1, 1)},
+                                                /*cost=*/12},
+                                               // src->src_ib
+                                               {{make_mem(0, 0), make_mem(0, 3)},
+                                                /*cost=*/2},
+                                               // src_ib->dst
+                                               {{make_mem(0, 3), make_mem(1, 1)},
+                                                /*cost=*/4}}),
+                           new FFPMockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/1,
+                                              {{{make_mem(0, 0), make_mem(1, 1)},
+                                                /*cost=*/12}})},
+                 .ib_mems{new IBMemory(make_mem(0, 3),
+                                       /*size=*/16, MemoryImpl::MKIND_SYSMEM,
+                                       Memory::SYSTEM_MEM,
+                                       /*prealloc_base=*/0, /*_segment=*/0)},
+                 .total_bytes = 16,
+                 .info =
+                     {
+                         .path = {make_mem(0, 0), make_mem(0, 3), make_mem(1, 1)},
+                     },
+                 .status = 1},
 
-  MemPathInfo info;
-  bool ok = find_fastest_path(nodes.data(), path_cache, channel_info, /*serdez_id=*/0,
-                              /*redop_id=*/0, total_bytes, &src_frags, &dst_frags, info);
+        // Case 3: src(0) -> src_ib(0) --> dst_ib(1) -> dst
+        TestCase{
+            .src = make_mem(0, 0),
+            .dst = make_mem(1, 1),
+            .channels{new FFPMockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/0,
+                                         {// src->src_ib
+                                          {{make_mem(0, 0), make_mem(0, 3)},
+                                           /*cost=*/2},
+                                          // src_ib->dst_ib
+                                          {{make_mem(0, 3), make_mem(1, 3)},
+                                           /*cost=*/2}}),
+                      new FFPMockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/1,
+                                         {// dst_ib->dst
+                                          {{make_mem(1, 3), make_mem(1, 1)},
+                                           /*cost=*/2}})},
+            .ib_mems{
+                new IBMemory(make_mem(0, 3),
+                             /*size=*/16, MemoryImpl::MKIND_SYSMEM, Memory::SYSTEM_MEM,
+                             /*prealloc_base=*/0,
+                             /*_segment=*/0),
+                new IBMemory(make_mem(1, 3),
+                             /*size=*/16, MemoryImpl::MKIND_SYSMEM, Memory::SYSTEM_MEM,
+                             /*prealloc_base=*/0,
+                             /*_segment=*/0),
+            },
+            .total_bytes = 16,
+            .info =
+                {
+                    .path = {make_mem(0, 0), make_mem(0, 3), make_mem(1, 3),
+                             make_mem(1, 1)},
+                },
+            .status = 1}));
 
-  EXPECT_TRUE(ok);
-  EXPECT_EQ(info.xd_channels.size(), 1);
-  EXPECT_EQ(info.xd_channels[0], channel_b);
-  EXPECT_EQ(info.path.size(), 2);
-  EXPECT_EQ(info.path[0], src_mem);
-  EXPECT_EQ(info.path[1], dst_mem);
-}
-
-TEST_F(FFPTest, FFPSrcIBDst)
-{
-  const uint64_t exp_cost_a = 12;
-  const uint64_t exp_cost_b = 12;
-  std::vector<Node> nodes(2);
-  MockChannel *channel_a = new MockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/0);
-  MockChannel *channel_b = new MockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/1);
-  nodes[0].dma_channels.push_back(channel_a);
-  nodes[1].dma_channels.push_back(channel_b);
-  Memory src_mem = ID::make_memory(0, 0).convert<Memory>();
-  Memory dst_mem = ID::make_memory(1, 1).convert<Memory>();
-  IBMemory *src_ib_mem =
-      new IBMemory(Memory(ID::make_memory(0, 3).convert<Memory>()),
-                   /*size=*/16, MemoryImpl::MKIND_SYSMEM, Memory::SYSTEM_MEM,
-                   /*prealloc_base=*/0, /*_segment=*/0);
-  nodes[0].ib_memories.push_back(src_ib_mem);
-
-  ChannelCopyInfo channel_info{src_mem, dst_mem};
-  channel_info.is_direct = true;
-  std::vector<size_t> src_frags;
-  std::vector<size_t> dst_frags;
-  size_t total_bytes = 16;
-
-  // Channels setup - direct path
-  mock_supports_path(channel_a, src_mem, dst_mem, &src_frags, &dst_frags, exp_cost_a);
-  mock_supports_path(channel_b, src_mem, dst_mem, &src_frags, &dst_frags, exp_cost_b);
-
-  // Channels setup - path via single edge
-  mock_supports_path(channel_a, src_mem, src_ib_mem->me, &src_frags, nullptr, 2, false);
-  mock_supports_path(channel_a, src_ib_mem->me, dst_mem, nullptr, &dst_frags, 4, false);
-  mock_supports_path(channel_b, src_ib_mem->me, dst_mem, nullptr, &dst_frags, 4, false);
-
-  MemPathInfo info;
-  bool ok = find_fastest_path(nodes.data(), path_cache, channel_info, /*serdez_id=*/0,
-                              /*redop_id=*/0, total_bytes, &src_frags, &dst_frags, info);
-
-  EXPECT_TRUE(ok);
-  EXPECT_EQ(info.xd_channels.size(), 2);
-  EXPECT_EQ(info.xd_channels[0], channel_a);
-  EXPECT_EQ(info.xd_channels[1], channel_a);
-  EXPECT_EQ(info.path.size(), 3);
-  EXPECT_EQ(info.path[0], src_mem);
-  EXPECT_EQ(info.path[1], src_ib_mem->me);
-  EXPECT_EQ(info.path[2], dst_mem);
-}
-
-TEST_F(FFPTest, FFPSrcIBToIBDst)
-{
-  const uint64_t exp_cost_a = 12;
-  const uint64_t exp_cost_b = 12;
-  std::vector<Node> nodes(2);
-  MockChannel *channel_a = new MockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/0);
-  MockChannel *channel_b = new MockChannel(XferDesKind::XFER_MEM_CPY, /*node=*/1);
-  nodes[0].dma_channels.push_back(channel_a);
-  nodes[1].dma_channels.push_back(channel_b);
-  Memory src_mem = ID::make_memory(0, 0).convert<Memory>();
-  Memory dst_mem = ID::make_memory(1, 3).convert<Memory>();
-  IBMemory *src_ib_mem =
-      new IBMemory(Memory(ID::make_memory(0, 1).convert<Memory>()),
-                   /*size=*/16, MemoryImpl::MKIND_SYSMEM, Memory::SYSTEM_MEM,
-                   /*prealloc_base=*/0, /*_segment=*/0);
-  IBMemory *dst_ib_mem =
-      new IBMemory(Memory(ID::make_memory(1, 2).convert<Memory>()),
-                   /*size=*/16, MemoryImpl::MKIND_SYSMEM, Memory::SYSTEM_MEM,
-                   /*prealloc_base=*/0, /*_segment=*/0);
-  nodes[0].ib_memories.push_back(src_ib_mem);
-  nodes[1].ib_memories.push_back(dst_ib_mem);
-
-  ChannelCopyInfo channel_info{src_mem, dst_mem};
-  channel_info.is_direct = true;
-  std::vector<size_t> src_frags;
-  std::vector<size_t> dst_frags;
-  size_t total_bytes = 16;
-
-  // TODO(apryakhin): Consider doing a smarter setup and name
-  // variables.
-
-  // Channels setup - direct path
-  mock_supports_path(channel_a, src_mem, dst_mem, &src_frags, &dst_frags, exp_cost_a);
-  mock_supports_path(channel_b, src_mem, dst_mem, &src_frags, &dst_frags, exp_cost_b);
-
-  // Channels setup - first edge
-  mock_supports_path(channel_a, src_mem, src_ib_mem->me, &src_frags, nullptr, 2, false);
-  mock_supports_path(channel_a, src_mem, dst_ib_mem->me, &src_frags, nullptr, 0, false);
-  mock_supports_path(channel_b, src_mem, dst_ib_mem->me, &src_frags, nullptr, 0, false);
-
-  // Channels setup - intermediate edges
-  mock_supports_path(channel_a, src_ib_mem->me, dst_ib_mem->me, nullptr, nullptr, 2,
-                     false);
-  mock_supports_path(channel_b, src_ib_mem->me, dst_ib_mem->me, nullptr, nullptr, 2,
-                     false);
-
-  // Channels setup - final step
-  mock_supports_path(channel_a, src_ib_mem->me, dst_mem, nullptr, &dst_frags, 0, false);
-  mock_supports_path(channel_b, src_ib_mem->me, dst_mem, nullptr, &dst_frags, 8, false);
-  mock_supports_path(channel_b, dst_ib_mem->me, dst_mem, nullptr, &dst_frags, 4, false);
-
-  MemPathInfo info;
-  bool ok = find_fastest_path(nodes.data(), path_cache, channel_info, /*serdez_id=*/0,
-                              /*redop_id=*/0, total_bytes, &src_frags, &dst_frags, info);
-
-  EXPECT_TRUE(ok);
-  EXPECT_EQ(info.xd_channels.size(), 3);
-  EXPECT_EQ(info.path.size(), 4);
-  EXPECT_EQ(info.xd_channels[0], channel_a);
-  EXPECT_EQ(info.xd_channels[1], channel_a);
-  EXPECT_EQ(info.xd_channels[2], channel_b);
-  EXPECT_EQ(info.path[0], src_mem);
-  EXPECT_EQ(info.path[1], src_ib_mem->me);
-  EXPECT_EQ(info.path[2], dst_ib_mem->me);
-  EXPECT_EQ(info.path[3], dst_mem);
-}
