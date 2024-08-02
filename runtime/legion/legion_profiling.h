b074f3e52c8696b60e649efd86e2f1b0ce581c30
Mike <mebauer@cs.stanford.edu>
2024-08-01 18:55:57 -0700

    Revert "legion: debugging commit to figure out profiling failure mode"
    This reverts commit 8ee3ab4803ffa64a8b9475f27846fe5d32f5df36.

--- runtime/legion/legion_profiling.h
+++ runtime/legion/legion_profiling.h
@@ -669,7 +669,6 @@ namespace Legion {
       std::vector<ProcID> proc_ids;
     private:
       std::deque<ProfTaskInfo> prof_task_infos;
-      std::set<LgEvent> recorded_events;
     };
 
     class LegionProfiler : public ProfilingResponseHandler {
