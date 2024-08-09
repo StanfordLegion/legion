b759925a9e9af28fb98d7c3e164217320d3b4170
apriakhin <apriakhin@nvidia.com>
2024-08-08 18:02:52 -0700

    realm: fix sparsity_destroy

--- test/realm/sparsity_destroy.cc
+++ test/realm/sparsity_destroy.cc
@@ -32,6 +32,7 @@ void node_task_0(const void *args, size_t arglen, const void *userdata, size_t u
     SparsityMap<1> local_sparsity =
         SparsityMap<1>::construct({bounds}, /*always_create=*/true, /*disjoint=*/false);
     IndexSpace<1> is(bounds, local_sparsity);
+    is.sparsity.add_references();
     is.destroy();
   }
 
@@ -39,12 +40,14 @@ void node_task_0(const void *args, size_t arglen, const void *userdata, size_t u
     std::vector<Rect<1>> bounds{Rect<1>(Point<1>(0), Point<1>(20000)),
                                 Rect<1>(Point<1>(30000), Point<1>(50000))};
     IndexSpace<1> is(bounds);
+    is.sparsity.add_references();
     is.destroy(task_args.wait_on);
   }
 
   {
     std::vector<Point<1>> points{Point<1>(0), Point<1>(20000), Point<1>(30000)};
     IndexSpace<1> is(points);
+    is.sparsity.add_references();
     is.destroy();
   }
 }
