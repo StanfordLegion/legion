9be38b70420a70158baf69ff7adee2d80f5ef0dd
Mike <mebauer@cs.stanford.edu>
2024-08-15 15:23:24 -0700

    legion: add support for -lg:werror flag to promote warnings to errors

--- runtime/legion.h
+++ runtime/legion.h
@@ -9371,6 +9371,7 @@ namespace Legion {
        * ------------- 
        * -lg:warn     Enable all verbose runtime warnings
        * -lg:warn_backtrace Print a backtrace for each warning
+       * -lg:werror   Promote all legion warnings into errors
        * -lg:leaks    Report information about resource leaks
        * -lg:ldb <replay_file> Replay the execution of the application
        *              with the associated replay file generted by LegionSpy. 
