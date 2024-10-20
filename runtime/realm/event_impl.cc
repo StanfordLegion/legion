444a319515d537e2a006a63711609ea0b970fe7f
Mike <mebauer@cs.stanford.edu>
2024-10-20 01:03:03 -0700

    realm: handle self-cancellation correctly for Event::cancel_operation

--- runtime/realm/event_impl.cc
+++ runtime/realm/event_impl.cc
@@ -340,9 +340,16 @@ namespace Realm {
 
     op = e->get_trigger_op(gen);
     if (op != nullptr) {
-      // Fast path, triggering operation is local!
-      op->attempt_cancellation(Realm::Faults::ERROR_CANCELLED, reason_data, reason_len);
-      op->remove_reference();
+      // Check for self-cancellation
+      Thread *thread = Thread::self();
+      if ((thread != nullptr) && (op == thread->get_operation())) {
+        op->remove_reference();
+        Processor::report_execution_fault(Realm::Faults::ERROR_CANCELLED, reason_data, reason_len);
+      } else {
+        // Fast path, triggering operation is local!
+        op->attempt_cancellation(Realm::Faults::ERROR_CANCELLED, reason_data, reason_len);
+        op->remove_reference();
+      }
     }
     else {
       // Slow path, triggering operation is remote
