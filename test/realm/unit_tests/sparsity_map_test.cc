4adf97aa2fa2a80c1705dff9e0f88d89dbb257da
Artem <pryakhin.artem@gmail.com>
2025-01-27 08:28:17 -0800

    realm: remove TEST_POINT_TYPES

--- test/realm/unit_tests/sparsity_map_test.cc
+++ test/realm/unit_tests/sparsity_map_test.cc
@@ -431,8 +431,6 @@ REGISTER_TYPED_TEST_SUITE_P(SparsityMapImplTest, AddRemoteWaiter, RemoteDataRepl
                             ComputeCoveringForNRect, ComputeOverlapPassApprox,
                             ComputeOverlapFail);
 
-#define TEST_POINT_TYPES(T) GeneratePointTypesForAllDims<T>()
-
 template <typename T, int... Ns>
 auto GeneratePointTypes(std::integer_sequence<int, Ns...>)
 {
@@ -446,7 +444,7 @@ auto GeneratePointTypesForAllDims()
 }
 
 #define INSTANTIATE_TEST_TYPES(BASE_TYPE, SUFFIX, FIXTURE)                               \
-  using N##SUFFIX = decltype(TEST_POINT_TYPES(BASE_TYPE));                               \
+  using N##SUFFIX = decltype(GeneratePointTypesForAllDims<BASE_TYPE>());                 \
   INSTANTIATE_TYPED_TEST_SUITE_P(SUFFIX##Type, FIXTURE, N##SUFFIX)
 
 INSTANTIATE_TEST_TYPES(int, Int, SparsityMapImplTest);
