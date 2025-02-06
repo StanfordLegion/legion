ab34d9066fdfa293774079abc05af9b5571871e4
apriakhin <apriakhin@nvidia.com>
2025-02-06 12:49:17 -0800

    realm: add all test cases for ::compute_covering

--- test/realm/unit_tests/sparsity_map_test.cc
+++ test/realm/unit_tests/sparsity_map_test.cc
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "realm/deppart/sparsity_impl.h"
+#include "realm/inst_impl.h"
 
 using namespace Realm;
 
@@ -294,68 +295,6 @@ TYPED_TEST_P(SparsityMapImplTest, ContributeDenseDisjointRects)
   }
 }
 
-TYPED_TEST_P(SparsityMapImplTest, ComputeCoveringForOneRect)
-{
-  using T = typename TestFixture::T;
-  constexpr int N = TestFixture::N;
-  constexpr int num_rects = 2, gap = 3, max_overhead = 1000;
-  NodeSet subscribers;
-  SparsityMap<N, T> handle = (ID::make_sparsity(0, 0, 0)).convert<SparsityMap<N, T>>();
-  std::vector<Rect<N, T>> rect_list;
-  std::unique_ptr<SparsityMapImpl<N, T>> impl =
-      std::make_unique<SparsityMapImpl<N, T>>(handle, subscribers, this->sparsity_comm);
-  SparsityMapPublicImpl<N, T> *public_impl = impl.get();
-  Rect<N, T> bounds(TypeParam(0), TypeParam(10));
-  std::vector<Rect<N, T>> covering;
-
-  int offset = 0;
-  for(int i = 0; i < num_rects; i++) {
-    TypeParam lo_point = TypeParam(offset);
-    TypeParam hi_point = TypeParam(offset + 1);
-    rect_list.emplace_back(Rect<N, T>(lo_point, hi_point));
-    offset += gap;
-  }
-
-  impl->contribute_dense_rect_list(rect_list, /*disjoint=*/true);
-  impl->set_contributor_count(1);
-  bool ok =
-      public_impl->compute_covering(bounds, /*max_rects=*/1, max_overhead, covering);
-
-  ASSERT_TRUE(ok);
-  ASSERT_EQ(covering.size(), 1);
-  ASSERT_EQ(covering.front().lo, TypeParam(0));
-  ASSERT_EQ(covering.front().hi, TypeParam(offset - gap + 1));
-}
-
-// TODO(apryakhin@): There are possible inputs for ::compute_covering, so consider making
-// a standlone paratemerie test if needed.
-TYPED_TEST_P(SparsityMapImplTest, ComputeCoveringForNRect)
-{
-  using T = typename TestFixture::T;
-  constexpr int N = TestFixture::N;
-  constexpr size_t num_rects = 4, max_rects = 3, gap = 3, max_overhead = 1000;
-  std::vector<Rect<N, T>> rect_list = create_rects<N, T>(num_rects, gap);
-  SparsityMap<N, T> handle = (ID::make_sparsity(0, 0, 0)).convert<SparsityMap<N, T>>();
-  NodeSet subscribers;
-  std::unique_ptr<SparsityMapImpl<N, T>> impl =
-      std::make_unique<SparsityMapImpl<N, T>>(handle, subscribers, this->sparsity_comm);
-  SparsityMapPublicImpl<N, T> *public_impl = impl.get();
-  Rect<N, T> bounds(TypeParam(0), TypeParam(10));
-  std::vector<Rect<N, T>> covering;
-
-  impl->contribute_dense_rect_list(rect_list, /*disjoint=*/true);
-  impl->set_contributor_count(1);
-  impl->contribute_nothing();
-  bool ok = public_impl->compute_covering(bounds, max_rects, max_overhead, covering);
-
-  ASSERT_TRUE(ok);
-  ASSERT_EQ(covering.size(), max_rects);
-  ASSERT_EQ(covering[0].lo, TypeParam(0));
-  ASSERT_EQ(covering[0].hi, TypeParam(1));
-  ASSERT_EQ(covering[1].lo, TypeParam(3));
-  ASSERT_EQ(covering[1].hi, TypeParam(4));
-}
-
 TYPED_TEST_P(SparsityMapImplTest, ComputeOverlapPassApprox)
 {
   constexpr int N = TestFixture::N;
@@ -431,8 +370,7 @@ TYPED_TEST_P(SparsityMapImplTest, ComputeOverlapFail)
 
 REGISTER_TYPED_TEST_SUITE_P(SparsityMapImplTest, RemoteDataReply,
                             ContributeDenseRectListRemote, ContributeDenseNotDisjoint,
-                            ContributeDenseDisjointRects, ComputeCoveringForOneRect,
-                            ComputeCoveringForNRect, ComputeOverlapPassApprox,
+                            ContributeDenseDisjointRects, ComputeOverlapPassApprox,
                             ComputeOverlapFail);
 
 template <typename T, int... Ns>
@@ -453,3 +391,250 @@ auto GeneratePointTypesForAllDims()
 
 INSTANTIATE_TEST_TYPES(int, Int, SparsityMapImplTest);
 INSTANTIATE_TEST_TYPES(long long, LongLong, SparsityMapImplTest);
+
+template <int N, typename T>
+static InstanceLayout<N, T> *create_layout(Rect<N, T> bounds,
+                                           const std::vector<int> &dim_order,
+                                           size_t bytes_per_element = 8)
+{
+  InstanceLayout<N, T> *inst_layout = new InstanceLayout<N, T>();
+  InstanceLayoutGeneric::FieldLayout field_layout;
+  field_layout.list_idx = 0;
+  field_layout.rel_offset = 0;
+  field_layout.size_in_bytes = bytes_per_element;
+
+  AffineLayoutPiece<N, T> *affine_piece = new AffineLayoutPiece<N, T>();
+  affine_piece->bounds = bounds;
+  affine_piece->offset = 0;
+  affine_piece->strides[dim_order[0]] = bytes_per_element;
+  size_t mult = affine_piece->strides[dim_order[0]];
+  for(int i = 1; i < N; i++) {
+    int d = dim_order[i];
+    affine_piece->strides[d] = (bounds.hi[i - 1] - bounds.lo[i - 1] + 1) * mult;
+    mult *= (bounds.hi[i - 1] - bounds.lo[i - 1] + 1);
+  }
+
+  inst_layout->space = bounds;
+  inst_layout->fields[0] = field_layout;
+  inst_layout->piece_lists.resize(1);
+  inst_layout->piece_lists[0].pieces.push_back(affine_piece);
+
+  return inst_layout;
+}
+
+// TODO(apryakhin@): Move to utils
+static inline RegionInstance make_inst(int owner = 0, int creator = 0, int mem_idx = 0,
+                                       int inst_idx = 0)
+{
+  return ID::make_instance(owner, creator, mem_idx, inst_idx).convert<RegionInstance>();
+}
+
+template <int N, typename T>
+static RegionInstanceImpl *
+create_inst(Rect<N, T> bounds, const std::vector<int> &dim_order,
+            size_t bytes_per_element = 8, RegionInstance inst = make_inst())
+{
+  InstanceLayout<N, T> *inst_layout = create_layout(bounds, dim_order, bytes_per_element);
+  RegionInstanceImpl *impl = new RegionInstanceImpl(inst, inst.get_location());
+  impl->metadata.layout = inst_layout;
+  impl->metadata.inst_offset = 0;
+  NodeSet ns;
+  impl->metadata.mark_valid(ns);
+  return impl;
+}
+
+template <int DIM>
+struct TypeWrapper {
+  static constexpr int value = DIM;
+};
+
+template <typename TypeWrapper>
+class ComputeCoveringTest : public ::testing::Test {
+public:
+  static constexpr int DIM = TypeWrapper::value;
+};
+
+template <int DIM>
+struct ComputeCoveringCase {
+  Rect<DIM> bounds;
+  std::vector<Rect<DIM>> rects;
+  bool disjoint;
+  size_t max_rects;
+  int max_overhead;
+  std::vector<Rect<DIM>> expected;
+  bool status = true;
+};
+
+template <int DIM>
+std::vector<ComputeCoveringCase<DIM>> ComputeCoveringTestCases()
+{
+  if constexpr(DIM == 1) {
+    return {
+        {// Case: Empty input (no rects)
+         /*bounds=*/Rect<DIM>{{1}, {0}},
+         /*rects=*/{},
+         /*disjoint=*/true,
+         /*max_rects=*/2,
+         /*max_overhead=*/10,
+         /*expected=*/{}, true},
+
+        {// Case: Max overhead = -1 (Unlimited overhead, should always return a valid
+         // covering)
+         /*bounds=*/Rect<DIM>{{0}, {5}},
+         /*rects=*/{Rect<DIM>{{1}, {2}}, Rect<DIM>{{3}, {4}}},
+         /*disjoint=*/true,
+         /*max_rects=*/2,
+         /*max_overhead=*/-1,
+         /*expected=*/{Rect<DIM>{{1}, {4}}}},
+
+        {// Case : compute covering zero max overhead
+         /*bounds=*/Rect<DIM>{{0}, {6}},
+         /*rects=*/{Rect<DIM>{{0}, {0}}, Rect<DIM>{{2}, {2}}, Rect<DIM>{{4}, {4}}},
+         /*disjoint=*/true,
+         /*max_rects=*/2,
+         /*max_overhead=*/0,
+         /*expected=*/{},
+         /*status=*/false},
+
+        {// Case: All rects exactly fit within bounds (disjoint)
+         /*bounds=*/Rect<DIM>{{0}, {5}},
+         /*rects=*/{Rect<DIM>{{0}, {0}}, Rect<DIM>{{3}, {3}}, Rect<DIM>{{5}, {5}}},
+         /*disjoint=*/true,
+         /*max_rects=*/3,
+         /*max_overhead=*/10,
+         /*expected=*/{Rect<DIM>{{0}, {0}}, Rect<DIM>{{3}, {3}}, Rect<DIM>{{5}, {5}}}},
+
+        {// Case: Single rect much larger than bounds (Should be cropped correctly)
+         /*bounds=*/Rect<DIM>{{1}, {3}},
+         /*rects=*/{Rect<DIM>{{0}, {5}}},
+         /*disjoint=*/true,
+         /*max_rects=*/1,
+         /*max_overhead=*/10,
+         /*expected=*/{Rect<DIM>{{1}, {3}}}},
+
+        {// Case: Two rects touching (hi[j] + 1 == lo[j+1], should merge)
+         /*bounds=*/Rect<DIM>{{0}, {5}},
+         /*rects=*/{Rect<DIM>{{0}, {2}}, Rect<DIM>{{3}, {5}}},
+         /*disjoint=*/true,
+         /*max_rects=*/1,
+         /*max_overhead=*/10,
+         /*expected=*/{Rect<DIM>{{0}, {5}}}},
+    };
+  } else if constexpr(DIM == 2) {
+    return {
+        {// Case: Empty input (no rects)
+         /*bounds=*/Rect<DIM>{{1, 1}, {0, 0}},
+         /*rects=*/{},
+         /*disjoint=*/true,
+         /*max_rects=*/2,
+         /*max_overhead=*/10,
+         /*expected=*/{}},
+
+        {// Case: Max overhead = -1 (Should always return a valid covering)
+         /*bounds=*/Rect<DIM>{{0, 0}, {4, 4}},
+         /*rects=*/{Rect<DIM>{{0, 0}, {1, 1}}, Rect<DIM>{{2, 2}, {3, 3}}},
+         /*disjoint=*/true,
+         /*max_rects=*/2,
+         /*max_overhead=*/-1,
+         /*expected=*/{Rect<DIM>{{0, 0}, {1, 1}}, Rect<DIM>{{2, 2}, {3, 3}}}},
+
+        {// Case: Rectangles exactly fit bounds
+         /*bounds=*/Rect<DIM>{{0, 0}, {4, 4}},
+         /*rects=*/{Rect<DIM>{{0, 0}, {2, 2}}, Rect<DIM>{{3, 3}, {4, 4}}},
+         /*disjoint=*/true,
+         /*max_rects=*/2,
+         /*max_overhead=*/10,
+         /*expected=*/{Rect<DIM>{{0, 0}, {2, 2}}, Rect<DIM>{{3, 3}, {4, 4}}}},
+
+        {// Case: Large rectangle covering bounds
+         /*bounds=*/Rect<DIM>{{1, 1}, {3, 3}},
+         /*rects=*/{Rect<DIM>{{0, 0}, {4, 4}}},
+         /*disjoint=*/true,
+         /*max_rects=*/1,
+         /*max_overhead=*/10,
+         /*expected=*/{Rect<DIM>{{1, 1}, {3, 3}}}},
+    };
+  } else if constexpr(DIM == 3) {
+    return {
+        {// Case: Empty input (no rects)
+         /*bounds=*/Rect<DIM>{{1, 1, 1}, {0, 0, 0}},
+         /*rects=*/{},
+         /*disjoint=*/true,
+         /*max_rects=*/2,
+         /*max_overhead=*/10,
+         /*expected=*/{}},
+
+        {// Case: Max overhead = -1 (Should always return a valid covering)
+         /*bounds=*/Rect<DIM>{{0, 0, 0}, {4, 4, 4}},
+         /*rects=*/{Rect<DIM>{{0, 0, 0}, {1, 1, 1}}, Rect<DIM>{{2, 2, 2}, {3, 3, 3}}},
+         /*disjoint=*/true,
+         /*max_rects=*/2,
+         /*max_overhead=*/-1,
+         /*expected=*/{Rect<DIM>{{0, 0, 0}, {1, 1, 1}}, Rect<DIM>{{2, 2, 2}, {3, 3, 3}}}},
+
+        {// Case: Rectangles exactly fit bounds
+         /*bounds=*/Rect<DIM>{{0, 0, 0}, {4, 4, 4}},
+         /*rects=*/{Rect<DIM>{{0, 0, 0}, {2, 2, 2}}, Rect<DIM>{{3, 3, 3}, {4, 4, 4}}},
+         /*disjoint=*/true,
+         /*max_rects=*/2,
+         /*max_overhead=*/10,
+         /*expected=*/{Rect<DIM>{{0, 0, 0}, {2, 2, 2}}, Rect<DIM>{{3, 3, 3}, {4, 4, 4}}}},
+
+        {// Case: Large rectangle covering bounds
+         /*bounds=*/Rect<DIM>{{1, 1, 1}, {3, 3, 3}},
+         /*rects=*/{Rect<DIM>{{0, 0, 0}, {4, 4, 4}}},
+         /*disjoint=*/true,
+         /*max_rects=*/1,
+         /*max_overhead=*/10,
+         /*expected=*/{Rect<DIM>{{1, 1, 1}, {3, 3, 3}}}},
+    };
+  }
+  return {};
+}
+
+TYPED_TEST_SUITE_P(ComputeCoveringTest);
+
+TYPED_TEST_P(ComputeCoveringTest, Base)
+{
+  using T = int;
+  constexpr int N = TypeParam::value;
+
+  auto test_cases = ComputeCoveringTestCases<N>();
+  for(const auto &test_case : test_cases) {
+    MockSparsityMapCommunicator<N, T> *sparsity_comm =
+        new MockSparsityMapCommunicator<N, T>();
+    SparsityMap<N, T> handle = (ID::make_sparsity(0, 0, 0)).convert<SparsityMap<N, T>>();
+    NodeSet subscribers;
+    std::unique_ptr<SparsityMapImpl<N, T>> impl =
+        std::make_unique<SparsityMapImpl<N, T>>(handle, subscribers, sparsity_comm);
+    SparsityMapPublicImpl<N, T> *public_impl = impl.get();
+
+    std::vector<Rect<N, T>> covering;
+
+    impl->contribute_dense_rect_list(test_case.rects, test_case.disjoint);
+    impl->set_contributor_count(1);
+    impl->contribute_nothing();
+    bool ok = public_impl->compute_covering(test_case.bounds, test_case.max_rects,
+                                            test_case.max_overhead, covering);
+    EXPECT_EQ(ok, test_case.status);
+    EXPECT_EQ(covering.size(), test_case.expected.size());
+    for(size_t i = 0; i < test_case.expected.size(); i++) {
+      EXPECT_EQ(covering[i].lo, test_case.expected[i].lo);
+      EXPECT_EQ(covering[i].hi, test_case.expected[i].hi);
+    }
+  }
+}
+
+REGISTER_TYPED_TEST_SUITE_P(ComputeCoveringTest, Base);
+
+template <typename Seq>
+struct WrapTypes;
+
+template <std::size_t... Ns>
+struct WrapTypes<std::index_sequence<Ns...>> {
+  using type = ::testing::Types<TypeWrapper<Ns + 1>...>;
+};
+
+using TestTypes = typename WrapTypes<std::make_index_sequence<REALM_MAX_DIM>>::type;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(AllDimensions, ComputeCoveringTest, TestTypes);
